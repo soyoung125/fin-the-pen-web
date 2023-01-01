4ca7cd8bed2e6ac31fd3eb9c5ab890d0c04a73c3
soyoung125 <sypark041754@gmail.com>
2023-01-01 23:52:02 +0900

    feat: 홈화면 달력 기능 추가 - 정기 출금 표시

--- src/components/calender/Calender.js
+++ src/components/calender/Calender.js
@@ -1,12 +1,44 @@
-import TextField from '@mui/material/TextField';
+/* eslint-disable react/jsx-props-no-spreading */
+import { Box } from '@mui/material';
 // import moment from 'moment';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay/PickersDay';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import React from 'react';
 
+/**
+ * --일정 스키마--
+ * 고유 아이디 id
+ * 유저 아이디 user_id
+ * 날짜 date
+ * 일정 시작 start_time
+ * 일정 끝 end_time
+ * 이벤트 이름 event_name
+ * 타입(출금,입금) type
+ * 금액 expected_spending
+ * 예산 제외 exclusion
+ * 카테고리 categories
+ * 일정중요도 importance
+ * + 정기 출금은??
+ */
+
 function Calender() {
   const [value, setValue] = React.useState(new Date());
+  // eslint-disable-next-line no-unused-vars
+  const [fixedWithdrawal, setFixedWithdrawal] = React.useState(['2023/01/24']);
+
+  const renderDayInPicker = (day, _value, DayComponentProps) => {
+    if (fixedWithdrawal.includes(day.format('YYYY/MM/DD'))) {
+      return (
+        <Box component="span" sx={{ border: 1, borderRadius: 2, borderColor: 'warning.main' }}>
+          <PickersDay {...DayComponentProps} />
+        </Box>
+      );
+    }
+
+    return <PickersDay {...DayComponentProps} />;
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
@@ -17,7 +49,7 @@ function Calender() {
           setValue(newValue);
         }}
         // eslint-disable-next-line react/jsx-props-no-spreading
-        renderInput={(params) => <TextField {...params} />}
+        renderDay={renderDayInPicker}
       />
     </LocalizationProvider>
   );
