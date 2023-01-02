508027a298213ace3d618e7a4f99f27d55c02b0d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-02 21:43:43 +0900

    fix: 파일 임시 삭제

--- src/utils/constants/Common.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-const NOTHING_IS_HERE_YET = '아직 아무것도 없음';
-const NOTHING_IS_AVAILABLE_BELOW_HERE = '여기를 기준으로 하단 기능은 아직 동작하지 않습니다.';
-export { NOTHING_IS_HERE_YET, NOTHING_IS_AVAILABLE_BELOW_HERE };
--- src/utils/constants/Path.js
+++
@@ -1,8 +0,0 @@
-const PATH = Object.freeze({
-  home: '/',
-  analysis: '/analysis',
-  signIn: '/sign-in',
-  test: '/test',
-});
-
-export default PATH;
