940b0a971758048568fdb7a94cf7c32b7322a799
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-03 02:13:19 +0900

    feat: 로그인 페이지 뼈대 구현

--- src/containers/mypage/MyPageContainer.js
+++ src/containers/mypage/MyPageContainer.js
@@ -1,8 +1,12 @@
+import { Box } from '@mui/material';
+import SignInContainer from '../sign/SignInContainer';
+
 function MyPageContainer() {
   return (
-    <>
-      개인 페이지
-    </>
+    <Box>
+      로그인 검사 후 MyPage를 띄울 지, SignInContainer를 띄어줄 지 결정하는 로직이 필요함
+      <SignInContainer />
+    </Box>
   );
 }
 export default MyPageContainer;
