18899e0cfba359498f3ba55fe4d2d35ec5725fee
soyoung125 <sypark041754@gmail.com>
2023-01-04 14:51:59 +0900

    refactor: 헤더 디자인 수정 z-index로 달력이 헤더 위로 올라가는 오류 수정

--- src/components/layouts/TopBar.js
+++ src/components/layouts/TopBar.js
@@ -6,7 +6,7 @@ function TopBar() {
   return (
     <Paper
       sx={{
-        position: 'fixed', top: 0, left: 0, right: 0,
+        position: 'fixed', top: 0, left: 0, right: 0, zIndex: 100,
       }}
       elevation={3}
     >
