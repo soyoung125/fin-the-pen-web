2ab7fe32c5302dc76762a3b3b8f827aecd4a869a
soyoung125 <sypark041754@gmail.com>
2023-01-05 17:33:22 +0900

    feat: 분석 파이 차트 추가 - 데이터 연동 필요 - 카테고리별 색상 필요

--- src/components/analysis/AnalysisGraph.js
+++ src/components/analysis/AnalysisGraph.js
@@ -1,8 +1,76 @@
+import { Box } from '@mui/material';
+import { ResponsivePie } from '@nivo/pie';
+
 function AnalysisGraph() {
+  const data = [
+    {
+      id: 'rust',
+      label: 'rust',
+      value: 600,
+      color: 'hsl(275, 70%, 50%)',
+    },
+    {
+      id: 'haskell',
+      label: 'haskell',
+      value: 106,
+      color: 'hsl(358, 70%, 50%)',
+    },
+    {
+      id: 'c',
+      label: 'c',
+      value: 50,
+      color: 'hsl(52, 70%, 50%)',
+    },
+    {
+      id: 'go',
+      label: 'go',
+      value: 13,
+      color: 'hsl(19, 70%, 50%)',
+    },
+    {
+      id: 'hack',
+      label: 'hack',
+      value: 18,
+      color: 'hsl(300, 70%, 50%)',
+    },
+  ];
+
   return (
-    <>
-      r그래프당
-    </>
+    <Box sx={{ width: '100vw', height: '100vw' }}>
+      <ResponsivePie
+        data={data}
+        margin={{
+          top: 10, right: 60, bottom: 10, left: 60,
+        }}
+        sortByValue
+        colors={{ scheme: 'nivo' }}
+        borderColor={{
+          from: 'color',
+          modifiers: [
+            [
+              'darker',
+              '0',
+            ],
+          ],
+        }}
+        enableArcLinkLabels={false}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: 'color' }}
+        arcLabelsSkipAngle={7}
+        arcLabelsTextColor={{
+          from: 'color',
+          modifiers: [
+            [
+              'darker',
+              '2',
+            ],
+          ],
+        }}
+        legends={[]}
+      />
+    </Box>
   );
 }
 
