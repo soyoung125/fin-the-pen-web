da1a40c0e31497e15bbe38b1f7f562e8e03a1d7e
soyoung125 <sypark041754@gmail.com>
2023-01-05 15:30:55 +0900

    feat: 분석 페이지 헤더 추가

--- src/components/analysis/AnalysisHeader.js
+++ src/components/analysis/AnalysisHeader.js
@@ -1,8 +1,24 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import { IconButton, Stack, Typography } from '@mui/material';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import moment from 'moment';
+
 function AnalysisHeader() {
+  const today = moment(new Date()).format('YYYY년MM월DD일');
   return (
-    <>
-      header
-    </>
+    <Stack direction="row" alignItems="center" justifyContent="space-between" px={2} my={2} sx={{ height: '30px' }}>
+      <IconButton aria-label="delete" sx={{ padding: '5px', marginRight: '-3px' }}>
+        <KeyboardArrowLeftIcon />
+      </IconButton>
+      <Stack alignItems="center">
+        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>카테고리별 소비 현황</Typography>
+        <Typography variant="caption">{today} 기준</Typography>
+      </Stack>
+      <IconButton aria-label="delete" sx={{ padding: '5px', marginLeft: '-3px' }}>
+        <KeyboardArrowRightIcon />
+      </IconButton>
+    </Stack>
   );
 }
 
--- src/containers/analysis/AnalysisContainer.js
+++ src/containers/analysis/AnalysisContainer.js
@@ -1,14 +1,15 @@
+import { Box } from '@mui/material';
 import AnalysisGraph from '../../components/analysis/AnalysisGraph';
 import AnalysisHeader from '../../components/analysis/AnalysisHeader';
 import AnalysisList from '../../components/analysis/AnalysisList';
 
 function AnalysisContainer() {
   return (
-    <>
+    <Box>
       <AnalysisHeader />
       <AnalysisGraph />
       <AnalysisList />
-    </>
+    </Box>
   );
 }
 export default AnalysisContainer;
