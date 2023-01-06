bcc1b8279fe420b595fb4be9d831d1b5ddaf54a1
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-07 02:39:18 +0900

    feat: 로그인 상황에 따른 마이페이지/로그인 버튼 전환

--- src/components/layouts/TopBar.js
+++ src/components/layouts/TopBar.js
@@ -11,74 +11,89 @@ import { useSelector } from 'react-redux';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import PersonIcon from '@mui/icons-material/Person';
 import { useNavigate } from 'react-router-dom';
+import LoginIcon from '@mui/icons-material/Login';
 import { selectHeaderOpen } from '../../utils/redux/common/commonSlice';
 import FullScreenDialog from './FullScreenDialog';
 import RoundedButton from '../common/RoundedButton';
 import PATH from '../../utils/constants/path';
+import { selectUser } from '../../utils/redux/user/userSlice';
 
 function TopBar() {
   const headerOpen = useSelector(selectHeaderOpen);
   const [fullScreenModalOpen, setFullScreenModalOpen] = useState(false);
   const navigate = useNavigate();
+  const user = useSelector(selectUser);
   return (
     <>
       <Box>
         {
-        headerOpen
-        && (
-          <Paper
-            elevation={10}
-            sx={{
-              backgroundColor: '#7c4dff',
-              height: 100,
-              borderBottomLeftRadius: '30px',
-              borderBottomRightRadius: '30px',
-            }}
-          >
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              alignItems="stretch"
-              mx={1}
-              sx={{ height: 100 }}
+          headerOpen
+          && (
+            <Paper
+              elevation={10}
+              sx={{
+                backgroundColor: '#7c4dff',
+                height: 100,
+                borderBottomLeftRadius: '30px',
+                borderBottomRightRadius: '30px',
+              }}
             >
               <Stack
-                justifyContent="flex-start"
-                alignItems="center"
-                mt={1}
+                direction="row"
+                justifyContent="space-between"
+                alignItems="stretch"
+                mx={1}
+                sx={{ height: 100 }}
               >
-                <RoundedButton onClick={() => navigate(PATH.notification)}>
-                  <NotificationsIcon />
-                </RoundedButton>
-              </Stack>
-              <Stack
-                justifyContent="flex-end"
-                alignItems="center"
-                mb={1}
-              >
-                {/* <Typography sx={{ fontWeight: 'bolder' }}>
+                <Stack
+                  justifyContent="flex-start"
+                  alignItems="center"
+                  mt={1}
+                >
+                  <RoundedButton onClick={() => navigate(PATH.notification)}>
+                    <NotificationsIcon />
+                  </RoundedButton>
+                </Stack>
+                <Stack
+                  justifyContent="flex-end"
+                  alignItems="center"
+                  mb={1}
+                >
+                  {/* <Typography sx={{ fontWeight: 'bolder' }}>
                   중앙
                 </Typography>
                 <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
                   표시영역
                 </Typography> */}
-                {/* <Button variant="contained" onClick={() => setFullScreenModalOpen(true)}>
+                  {/* <Button variant="contained" onClick={() => setFullScreenModalOpen(true)}>
                   임시로 위치한 버튼 (빠른 시간 내에 복구 예정)
                 </Button> */}
+                </Stack>
+                <Stack
+                  justifyContent="flex-start"
+                  alignItems="center"
+                  mt={1}
+                >
+                  {
+                    user === null
+                      ? (
+                        <RoundedButton onClick={() => navigate(PATH.signIn)}>
+                          <LoginIcon />
+                        </RoundedButton>
+                      )
+
+                      : (
+                        <RoundedButton onClick={() => navigate(PATH.mypage)}>
+                          <PersonIcon />
+                        </RoundedButton>
+                      )
+
+                  }
+                </Stack>
               </Stack>
-              <Stack
-                justifyContent="flex-start"
-                alignItems="center"
-                mt={1}
-              >
-                <RoundedButton onClick={() => navigate(PATH.mypage)}>
-                  <PersonIcon />
-                </RoundedButton>
-              </Stack>
-            </Stack>
-          </Paper>
-        )
-      }
+            </Paper>
+          )
+        }
       </Box>
       <FullScreenDialog open={fullScreenModalOpen} setOpen={setFullScreenModalOpen} />
     </>
