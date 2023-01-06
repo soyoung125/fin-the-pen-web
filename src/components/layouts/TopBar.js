e00830f563f799b954612e115cf7e6ddf9bc2606
soyoung125 <sypark041754@gmail.com>
2023-01-06 17:56:02 +0900

    refactor: 로그인 페이지 헤더 숨기기 - 헤더 디자인 변경 후 헤더가 사라지지 않은 오류 수정

--- src/components/layouts/TopBar.js
+++ src/components/layouts/TopBar.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Paper, Stack, Typography,
 } from '@mui/material';
 import { useSelector } from 'react-redux';
@@ -7,67 +8,68 @@ import { selectHeaderOpen } from '../../utils/redux/common/commonSlice';
 function TopBar() {
   const headerOpen = useSelector(selectHeaderOpen);
   return (
-    <Paper
-      elevation={10}
-      sx={{
-        backgroundColor: '#7c4dff',
-        height: 100,
-        borderBottomLeftRadius: '30px',
-        borderBottomRightRadius: '30px',
-      }}
-    >
+    <Box>
       {
         headerOpen
         && (
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="stretch"
-          mx={1}
-          sx={{ height: 100 }}
-        >
-          <Stack
-            justifyContent="flex-start"
-            alignItems="center"
-            mt={1}
+          <Paper
+            elevation={10}
+            sx={{
+              backgroundColor: '#7c4dff',
+              height: 100,
+              borderBottomLeftRadius: '30px',
+              borderBottomRightRadius: '30px',
+            }}
           >
-            <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
-              좌측
-            </Typography>
-            <Typography variant="caption" sx={{ fontWeight: 'bold', color: 'white' }}>
-              표시영역
-            </Typography>
-          </Stack>
-          <Stack
-            justifyContent="flex-end"
-            alignItems="center"
-            mb={1}
-          >
-            <Typography sx={{ fontWeight: 'bolder' }}>
-              중앙
-            </Typography>
-            <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
-              표시영역
-            </Typography>
-          </Stack>
-          <Stack
-            justifyContent="flex-start"
-            alignItems="center"
-            mt={1}
-          >
-            <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
-              우측
-            </Typography>
-            <Typography variant="caption" sx={{ fontWeight: 'bold', color: 'white' }}>
-              표시영역
-            </Typography>
-          </Stack>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="stretch"
+              mx={1}
+              sx={{ height: 100 }}
+            >
+              <Stack
+                justifyContent="flex-start"
+                alignItems="center"
+                mt={1}
+              >
+                <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
+                  좌측
+                </Typography>
+                <Typography variant="caption" sx={{ fontWeight: 'bold', color: 'white' }}>
+                  표시영역
+                </Typography>
+              </Stack>
+              <Stack
+                justifyContent="flex-end"
+                alignItems="center"
+                mb={1}
+              >
+                <Typography sx={{ fontWeight: 'bolder' }}>
+                  중앙
+                </Typography>
+                <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
+                  표시영역
+                </Typography>
+              </Stack>
+              <Stack
+                justifyContent="flex-start"
+                alignItems="center"
+                mt={1}
+              >
+                <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
+                  우측
+                </Typography>
+                <Typography variant="caption" sx={{ fontWeight: 'bold', color: 'white' }}>
+                  표시영역
+                </Typography>
+              </Stack>
 
-        </Stack>
+            </Stack>
+          </Paper>
         )
       }
-
-    </Paper>
+    </Box>
   );
 }
 export default TopBar;
