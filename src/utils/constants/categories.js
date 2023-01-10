e850bf3655fc691e7d09d8617da105a5d932872f
soyoung125 <sypark041754@gmail.com>
2023-01-10 22:50:51 +0900

    refactor: category 구조 변경

--- src/utils/constants/categories.js
+++ src/utils/constants/categories.js
@@ -6,13 +6,14 @@ const FIXED = {
     {
       type: '입금',
       categories: ['급여'],
+      color: '#FF0000',
     },
     {
       type: '출금',
       categories: ['구독비', '교통비/자동차세', '(주거)관리비/월세', '핸드폰요금', '적금', '보험비', '카드이체료'],
+      color: '#0000FF',
     },
   ],
-  color: '#FFE400',
 };
 
 const INCOME = {
@@ -21,13 +22,14 @@ const INCOME = {
     {
       type: '용돈',
       categories: ['용돈'],
+      color: '#800080',
     },
     {
       type: '수익',
       categories: ['금융', '사업', '기타'],
+      color: '#008000',
     },
   ],
-  color: '#1DDB16',
 };
 
 const EXPENDITURE = {
@@ -36,25 +38,29 @@ const EXPENDITURE = {
     {
       type: '음식',
       categories: ['식비', '술', '카페'],
+      color: '#87CEEB',
     },
     {
       type: '생활',
       categories: ['생필품', '주거', '통신', '자녀/육아', '반려동물', '교육/학습', '의료/건강'],
+      color: '#0000FF',
     },
     {
       type: '교통',
       categories: ['자동차', '대중교통'],
+      color: '#808080',
     },
     {
       type: '문화',
       categories: ['온라인쇼핑', '패션', '미용', '영화/전시', '여행/숙박'],
+      color: '#FFC0CB',
     },
     {
       type: '금융',
       categories: ['저축', '투자'],
+      color: '#A52A2A',
     },
   ],
-  color: '#FF0000',
 };
 
 const categoryFlatter = (obj) => {
@@ -64,9 +70,9 @@ const categoryFlatter = (obj) => {
     title,
     color,
   }));
-  const { type, color } = obj;
+  const { type } = obj;
   return obj.nested
-    .map((cat) => flatNestedCategories(type, cat.type, cat.categories, color))
+    .map((cat) => flatNestedCategories(type, cat.type, cat.categories, cat.color))
     .flat(Infinity);
 };
 
