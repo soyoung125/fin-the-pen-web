25fafffd4cf1911d5f0f3ce40d6f1386a548f724
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-10 22:44:20 +0900

    feat: 일정 설정 뼈대 추가

--- src/containers/settings/SettingsContainer.js
+++ src/containers/settings/SettingsContainer.js
@@ -3,16 +3,25 @@ import ListSubheader from '@mui/material/ListSubheader';
 import AppLocker from './display/AppLocker';
 import Budget from './display/Budget';
 import ThemeMode from './display/ThemeMode';
+import Schedule from './schedule/Schedule';
 
 export default function SettingsContainer() {
   return (
-    <List
-      sx={{ width: '100%', bgcolor: 'background.paper' }}
-      subheader={<ListSubheader>화면 설정</ListSubheader>}
-    >
-      <AppLocker />
-      <ThemeMode />
-      <Budget />
-    </List>
+    <>
+      <List
+        sx={{ width: '100%', bgcolor: 'background.paper' }}
+        subheader={<ListSubheader>화면 설정</ListSubheader>}
+      >
+        <AppLocker />
+        <ThemeMode />
+        <Budget />
+      </List>
+      <List
+        sx={{ width: '100%', bgcolor: 'background.paper' }}
+        subheader={<ListSubheader>일정</ListSubheader>}
+      >
+        <Schedule />
+      </List>
+    </>
   );
 }
