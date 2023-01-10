076da80b6fefba7fd4bdd3c613a2780379d7ffc3
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-10 17:15:17 +0900

    fix: macos 버그 수정

--- src/containers/settings/SettingsContainer.js
+++ src/containers/settings/SettingsContainer.js
@@ -1,7 +1,9 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import { List } from '@mui/material';
 import ListSubheader from '@mui/material/ListSubheader';
-import AppLocker from './\bdisplay/AppLocker';
-import ThemeMode from './\bdisplay/ThemeMode';
+import AppLocker from './display/AppLocker';
+import ThemeMode from './display/ThemeMode';
 
 export default function SettingsContainer() {
   return (
