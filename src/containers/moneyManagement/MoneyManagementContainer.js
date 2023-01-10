dec638e7e3d2587cdc840e21816d8e2c623690ec
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-10 15:28:37 +0900

    refactor: category 구조 개선
    - 기존의 하드 코딩의 비효율성 제거

--- src/containers/moneyManagement/MoneyManagementContainer.js
+++ src/containers/moneyManagement/MoneyManagementContainer.js
@@ -1,7 +1,12 @@
+import CATEGORIES from '../../utils/constants/categories';
+
 function MoneyManagementContainer() {
   return (
     <>
       자산 관리
+      {
+        CATEGORIES.map((cat) => <div>{JSON.stringify(cat)}</div>)
+      }
     </>
   );
 }
--- src/utils/constants/categories.js
+++ src/utils/constants/categories.js
@@ -1,44 +1,73 @@
-const INCOME = '수입';
-const TRANSFER = '이체';
-const EXPENDITURE = '지출';
+/* eslint-disable no-unused-vars */
 
-const CATEGORIES = Object.freeze([
-  {
-    title: '급여',
-    type: INCOME,
-  },
-  {
-    title: '용돈',
-    type: INCOME,
-  },
-  {
-    title: '금융수입',
-    type: INCOME,
-  },
-  {
-    title: '내 계좌',
-    type: TRANSFER,
-  },
-  {
-    title: '이체',
-    type: TRANSFER,
-  },
-  {
-    title: '카드대금',
-    type: TRANSFER,
-  },
-  {
-    title: '식비',
-    type: EXPENDITURE,
-  },
-  {
-    title: '카페/간식',
-    type: EXPENDITURE,
-  },
-  {
-    title: '술',
-    type: EXPENDITURE,
-  },
-]);
+const FIXED = {
+  type: '고정 입출금',
+  nested: [
+    {
+      type: '입금',
+      categories: ['급여'],
+    },
+    {
+      type: '출금',
+      categories: ['구독비', '교통비/자동차세', '(주거)관리비/월세', '핸드폰요금', '적금', '보험비', '카드이체료'],
+    },
+  ],
+};
+
+const INCOME = {
+  type: '수입',
+  nested: [
+    {
+      type: '용돈',
+      categories: ['용돈'],
+    },
+    {
+      type: '수익',
+      categories: ['금융', '사업', '기타'],
+    },
+  ],
+};
+
+const EXPENDITURE = {
+  type: '지출',
+  nested: [
+    {
+      type: '음식',
+      categories: ['식비', '술', '카페'],
+    },
+    {
+      type: '생활',
+      categories: ['생필품', '주거', '통신', '자녀/육아', '반려동물', '교육/학습', '의료/건강'],
+    },
+    {
+      type: '교통',
+      categories: ['자동차', '대중교통'],
+    },
+    {
+      type: '문화',
+      categories: ['온라인쇼핑', '패션', '미용', '영화/전시', '여행/숙박'],
+    },
+    {
+      type: '금융',
+      categories: ['저축', '투자'],
+    },
+  ],
+};
+
+const categoryFlatter = (obj) => {
+  const flatNestedCategories = (type, nestedType, categories) => categories.map((title) => ({
+    type,
+    nestedType,
+    title,
+  }));
+  const { type } = obj;
+  return obj.nested
+    .map((cat) => flatNestedCategories(type, cat.type, cat.categories))
+    .flat(Infinity);
+};
+
+const CATEGORIES = Object.freeze(
+  [...categoryFlatter(FIXED), ...categoryFlatter(INCOME), ...categoryFlatter(EXPENDITURE)],
+);
 
 export default CATEGORIES;
