e80a5e7f8ca8128a7b17fcacb79ab56721e6095e
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-10 23:38:34 +0900

    fix: 임시 페이지에서 원래 페이지로 돌아갈 수 없던 문제를 수정

--- src/containers/test/TestContainer.js
+++ src/containers/test/TestContainer.js
@@ -1,7 +1,13 @@
+import { Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
 function TestContainer() {
+  const navigate = useNavigate();
   return (
     <>
-      테스트
+      <Typography variant="h4">테스트</Typography>
+      <Typography>이 페이지는 아직 어떤 페이지가 구현되지 않았을 때 임시로 이동하게 하는 페이지 입니다.</Typography>
+      <Button variant="contained" color="error" onClick={() => navigate(-1)}>Go Back</Button>
     </>
   );
 }
