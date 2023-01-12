dc9848853dd6ef0c20471cd3a95f8284ce5fb9dd
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-13 01:21:11 +0900

    feat: 게스트 계정 전용 메뉴 뼈대 추가

--- src/containers/mypage/MyPageContainer.js
+++ src/containers/mypage/MyPageContainer.js
@@ -1,4 +1,8 @@
-import { Box, Button, Typography } from '@mui/material';
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-alert */
+import {
+  Box, Button, Stack, Typography,
+} from '@mui/material';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -11,8 +15,10 @@ function MyPageContainer() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userLogOut = () => {
-    dispatch(logOut());
-    // 로그아웃 하면서 추가로 해제해야 할 행동을 여기에서 해줘야 함
+    if (confirm('게스트 계정은 로그아웃 하는 경우 지금까지 작업한 내용이 저장되지 않습니다. 중요한 자료는 미리 백업해주세요. (확인 시 모든 정보 날라감)')) {
+      dispatch(logOut());
+      // 로그아웃 하면서 추가로 해제해야 할 행동을 여기에서 해줘야 함
+    }
   };
   useEffect(() => {
     // 로그인 안된 계정은 로그인 페이지로 강제연결
@@ -26,13 +32,35 @@ function MyPageContainer() {
       <Typography>
         {JSON.stringify(user)}
       </Typography>
-      <Button
-        variant="contained"
-        color="error"
-        onClick={() => userLogOut()}
-      >
-        로그아웃
-      </Button>
+      <Stack spacing={2} m={2} border={1} p={2}>
+        <Typography variant="h5">게스트 계정 전용 메뉴 (준비중)</Typography>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => alert('준비 중인 메뉴')}
+        >
+          세이브 데이터 복구하기
+        </Button>
+
+        <Button
+          variant="contained"
+          color="info"
+          onClick={() => alert('준비 중인 메뉴')}
+        >
+          지금까지 작업한 데이터 백업
+        </Button>
+
+      </Stack>
+      <Box m={2}>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => userLogOut()}
+        >
+          로그아웃
+        </Button>
+      </Box>
+
     </Box>
   );
 }
