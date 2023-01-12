95d48c98e6d3a5e4ea5c094fbbd6de8522f035c2
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-12 20:30:53 +0900

    refactor: 더이상 사용하지 않는 파일을 다른 용도로 변경
    - 로그인 유도 모달용으로 재활용

--- src/components/calender/scheduleList/ScheduleList.js
+++ src/components/calender/scheduleList/ScheduleList.js
@@ -2,18 +2,13 @@ import {
   Box, List, Stack, Typography,
 } from '@mui/material';
 import moment from 'moment';
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectDate, selectSchedules } from '../../../utils/redux/schedule/scheduleSlice';
-import ModalStaticBackdrop from '../../layouts/ModalStaticBackdrop';
 import ScheduleCard from './ScheduleCard';
-import ScheduleModal from './ScheduleModal';
 
 function ScheduleList() {
   const schedules = useSelector(selectSchedules);
   const date = moment(useSelector(selectDate)).format('YYYY-MM-DD');
-  const [selectedSchedule, setSelectedSchedule] = useState(null);
-  const [scheduleModalOpen, setScheduleModalOpen] = useState(false);
 
   return (
     <>
@@ -46,78 +41,10 @@ function ScheduleList() {
               <ScheduleCard
                 schedule={el}
                 key={Math.random()}
-                setScheduleModalOpen={setScheduleModalOpen}
-                setSelectedSchedule={setSelectedSchedule}
               />
             ))
         }
       </List>
-      <ModalStaticBackdrop
-        keepMounted
-        width="xl"
-        open={scheduleModalOpen}
-        component={(
-          <ScheduleModal
-            selectedSchedule={selectedSchedule}
-            setScheduleModalOpen={setScheduleModalOpen}
-          />
-          // <Box p={1}>
-          //   {
-          //   selectedSchedule
-          //   && (
-          //     <>
-          //       <Typography>
-          //         {selectedSchedule.event_name}
-          //       </Typography>
-          //       <Typography>
-          //         {selectedSchedule.date}
-          //       </Typography>
-          //       <Typography>
-          //         {selectedSchedule.start_time}
-          //       </Typography>
-          //       <Typography>
-          //         {selectedSchedule.end_time}
-          //       </Typography>
-          //       <Typography>
-          //         {JSON.stringify(selectedSchedule.categories)}
-          //       </Typography>
-          //       <Typography>
-          //         {selectedSchedule.type}
-          //       </Typography>
-          //       <Typography>
-          //         {selectedSchedule.expected_spending}
-          //       </Typography>
-          //       <Typography>
-          //         {selectedSchedule.importance}
-          //       </Typography>
-          //       <Typography>
-          //         {JSON.stringify(selectedSchedule.exclusion)}
-          //       </Typography>
-
-          //     </>
-          //   )
-          //   }
-          //   <Stack direction="row" spacing={1}>
-          //     <Button
-          //       variant="contained"
-          //       color="error"
-          //       fullWidth
-          //       onClick={() => setScheduleModalOpen(false)}
-          //     >
-          //       삭제
-          //     </Button>
-          //     <Button
-          //       variant="contained"
-          //       fullWidth
-          //       onClick={() => setScheduleModalOpen(false)}
-          //     >
-          //       확인
-          //     </Button>
-          //   </Stack>
-
-          // </Box>
-        )}
-      />
     </>
   );
 }
