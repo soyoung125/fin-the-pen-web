535871f1cfc20703daf81433f9691d5b8af75b67
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-13 13:58:53 +0900

    feat: 일정 목록에 마우스 올리면 hover 효과 적용

--- src/components/calender/scheduleList/ScheduleCard.js
+++ src/components/calender/scheduleList/ScheduleCard.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import {
-  Box, ButtonBase, Divider, Stack, Typography,
+  Box, CardActionArea, Divider, Stack, Typography,
 } from '@mui/material';
 import { useState } from 'react';
 
@@ -21,19 +21,21 @@ function ScheduleCard({ schedule, setScheduleModalOpen, setSelectedSchedule }) {
 
   return (
     <>
-      <ButtonBase sx={{ width: '100vw', justifyContent: 'start' }}>
-        <Box px={3} py={2} onClick={() => handleModal()}>
-          <Stack direction="row">
-            <Box
-              sx={{
-                width: '15px', height: '15px', border: '4px solid', borderRadius: 3, borderColor: schedule.category.color, marginY: 'auto', marginRight: 2,
-              }}
-            />
-            <Typography variant="caption" fullwidth>{`${schedule.start_time} - ${schedule.end_time}`}</Typography>
-          </Stack>
-          <Typography variant="h6" pl={1} pt={1}>{schedule.event_name}</Typography>
+      <CardActionArea>
+        <Box sx={{ width: '100vw', justifyContent: 'start' }}>
+          <Box px={3} py={2} onClick={() => handleModal()}>
+            <Stack direction="row">
+              <Box
+                sx={{
+                  width: '15px', height: '15px', border: '4px solid', borderRadius: 3, borderColor: schedule.category.color, marginY: 'auto', marginRight: 2,
+                }}
+              />
+              <Typography variant="caption" fullwidth>{`${schedule.start_time} - ${schedule.end_time}`}</Typography>
+            </Stack>
+            <Typography variant="h6" pl={1} pt={1}>{schedule.event_name}</Typography>
+          </Box>
         </Box>
-      </ButtonBase>
+      </CardActionArea>
       <Divider />
     </>
 
