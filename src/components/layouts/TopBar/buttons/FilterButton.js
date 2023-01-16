a1d2c0b5298f9e47fd755ed06c24cbfbecfb78e1
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-16 15:36:18 +0900

    feat: 필터 헤더 구현

--- src/components/layouts/TopBar/buttons/FilterButton.js
+++ src/components/layouts/TopBar/buttons/FilterButton.js
@@ -1,4 +1,6 @@
-import { Drawer, Stack, Typography } from '@mui/material';
+import {
+  Button, Drawer, Stack, Typography,
+} from '@mui/material';
 import { useState } from 'react';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import RoundedButton from '../../../common/RoundedButton';
@@ -15,7 +17,16 @@ function FilterButton() {
         anchor="bottom"
         onClose={() => setBottomDrawerOpen(false)}
       >
-        <Stack>
+        <Stack
+          justifyContent="space-between"
+          spacing={2}
+          m={1}
+        >
+          <Stack direction="row" alignItems="center" justifyContent="space-between">
+            <Button onClick={() => alert('취소')}>취소</Button>
+            <Typography variant="h5" sx={{ fontWeight: 'bold' }}>필터 설정</Typography>
+            <Button onClick={() => alert('확인')}>확인</Button>
+          </Stack>
           <Typography>
             필터
           </Typography>
