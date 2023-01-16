6fa45e786a1da1ac73b44de78532b04ada058838
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-17 03:19:33 +0900

    feat: filter modal에 categories 렌더링

--- src/components/layouts/TopBar/buttons/FilterButton.js
+++ src/components/layouts/TopBar/buttons/FilterButton.js
@@ -1,13 +1,21 @@
+/* eslint-disable no-unused-vars */
 import {
-  Button, Drawer, Stack, Typography,
+  Box,
+  Button, Chip, Drawer, ListItem, Stack, Typography,
 } from '@mui/material';
 import { useState } from 'react';
-// import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
 import RoundedButton from '../../../common/RoundedButton';
+import Accordion from '../../../common/accordions/Accordion';
+import AccordionSummary from '../../../common/accordions/AccordionSummary';
+import AccordionDetails from '../../../common/accordions/AccordionDetails';
+import { EXPENDITURE, FIXED, INCOME } from '../../../../utils/constants/categories';
 
 function FilterButton() {
   const [bottomDrawerOpen, setBottomDrawerOpen] = useState(false);
+  const handleDelete = () => {
+    console.info('You clicked the Chip.');
+  };
   return (
     <>
       <RoundedButton value="user" onClick={() => setBottomDrawerOpen(true)}>
@@ -29,9 +37,30 @@ function FilterButton() {
             <Typography variant="h5" sx={{ fontWeight: 'bold' }}>필터 설정</Typography>
             <Button onClick={() => alert('확인')}>확인</Button>
           </Stack>
-          <Typography>
-            필터
-          </Typography>
+          <Stack>
+            {
+              [FIXED, INCOME, EXPENDITURE].map((obj) => (
+                <Accordion>
+                  <AccordionSummary
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography>{obj.type}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    {obj.nested.map((el) => (
+                      <>
+                        <Typography>{el.type}</Typography>
+                        {el.categories.map((cat) => (
+                          <Chip label={cat} variant="outlined" onDelete={handleDelete} sx={{ mr: 1, mb: 1 }} />
+                        ))}
+                      </>
+                    ))}
+                  </AccordionDetails>
+                </Accordion>
+              ))
+            }
+          </Stack>
         </Stack>
       </Drawer>
     </>
