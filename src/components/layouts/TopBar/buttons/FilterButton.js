c733f431799d6ce10cfafd3fca07c8c069b0417e
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-18 23:13:31 +0900

    feat: 필터링의 기간 설정 렌더링

--- src/components/layouts/TopBar/buttons/FilterButton.js
+++ src/components/layouts/TopBar/buttons/FilterButton.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import {
   Box,
-  Button, Chip, Drawer, ListItem, Stack, Typography,
+  Button, Chip, Drawer, ListItem, Stack, TextField, Typography,
 } from '@mui/material';
 import { useState } from 'react';
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
@@ -33,8 +33,8 @@ function FilterButton() {
           m={1}
         >
           <Stack direction="row" alignItems="center" justifyContent="space-between">
-            <Button onClick={() => alert('취소')}>취소</Button>
-            <Typography variant="h5" sx={{ fontWeight: 'bold' }}>필터 설정</Typography>
+            <Button onClick={() => setBottomDrawerOpen(false)}>닫기</Button>
+            <Typography variant="h5" sx={{ fontWeight: 'bold' }}>필터 설정(제작중)</Typography>
             <Button onClick={() => alert('확인')}>확인</Button>
           </Stack>
           <Stack>
@@ -60,6 +60,46 @@ function FilterButton() {
                 </Accordion>
               ))
             }
+          </Stack>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            spacing={2}
+            sx={{ width: '100%' }}
+          >
+            <TextField
+              id="start_date"
+              label="시작일"
+              type="date"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+              // value={schedule.date}
+              // onChange={updateSchedule}
+              size="small"
+            />
+            <Typography>~</Typography>
+            <TextField
+              id="date"
+              label="종료일"
+              type="date"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+              // value={schedule.date}
+              // onChange={updateSchedule}
+              size="small"
+            />
+          </Stack>
+          <Stack>
+            <Button
+              variant="contained"
+            >
+              필터 초기화
+            </Button>
           </Stack>
         </Stack>
       </Drawer>
