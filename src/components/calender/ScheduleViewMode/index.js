a6ecdc51adc4bc02955b999d712893e3299462b8
soyoung125 <sypark041754@gmail.com>
2023-01-20 22:56:51 +0900

    feat: mode 스위치되도록 구현

--- src/components/calender/ScheduleViewMode/index.js
+++ src/components/calender/ScheduleViewMode/index.js
@@ -1,12 +1,26 @@
 import { Box, Button } from '@mui/material';
 import { grey } from '@mui/material/colors';
+import { useState } from 'react';
 
 function ScheduleViewMode() {
+  const [viewMode, setViewMode] = useState('schedule');
   return (
     <Box display="flex" justifyContent="center" alignItems="center">
       <Box sx={{ borderRadius: 4, backgroundColor: grey[200] }}>
-        <Button sx={{ borderRadius: 4 }} variant="text">자산</Button>
-        <Button sx={{ borderRadius: 4 }} variant="contained">일정</Button>
+        <Button
+          sx={{ borderRadius: 4 }}
+          variant={viewMode === 'asset' ? 'contained' : 'text'}
+          onClick={() => setViewMode('asset')}
+        >
+          자산
+        </Button>
+        <Button
+          sx={{ borderRadius: 4 }}
+          variant={viewMode === 'schedule' ? 'contained' : 'text'}
+          onClick={() => setViewMode('schedule')}
+        >
+          일정
+        </Button>
       </Box>
     </Box>
   );
