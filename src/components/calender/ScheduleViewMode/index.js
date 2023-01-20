d09f1cc043d9076affcf1ddec3ce5322f30839c6
soyoung125 <sypark041754@gmail.com>
2023-01-20 22:29:10 +0900

    feat: 기본 디자인 구현

--- src/components/calender/ScheduleViewMode/index.js
+++ src/components/calender/ScheduleViewMode/index.js
@@ -1,12 +1,13 @@
-import { Box, Button, ButtonGroup } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import { grey } from '@mui/material/colors';
 
 function ScheduleViewMode() {
   return (
     <Box display="flex" justifyContent="center" alignItems="center">
-      <ButtonGroup variant="outlined" sx={{ marginX: 'auto', justifyContent: 'center' }}>
-        <Button>자산</Button>
-        <Button>일정</Button>
-      </ButtonGroup>
+      <Box sx={{ borderRadius: 4, backgroundColor: grey[200] }}>
+        <Button sx={{ borderRadius: 4 }} variant="text">자산</Button>
+        <Button sx={{ borderRadius: 4 }} variant="contained">일정</Button>
+      </Box>
     </Box>
   );
 }
