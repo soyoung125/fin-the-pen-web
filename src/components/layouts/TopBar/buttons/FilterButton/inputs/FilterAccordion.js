0df2c5310cf32b21c5400d7f492b9de6cca89b73
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-21 16:28:44 +0900

    fix: 카테고리 전체 필터가 독립적으로 행동하지 않던 문제 해결

--- src/components/layouts/TopBar/buttons/FilterButton/inputs/FilterAccordion.js
+++ src/components/layouts/TopBar/buttons/FilterButton/inputs/FilterAccordion.js
@@ -1,26 +1,10 @@
-import {
-  Box, Chip, Stack, Typography,
-} from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import Checkbox from '@mui/material/Checkbox';
-import { useState } from 'react';
+import { Typography } from '@mui/material';
 import Accordion from '../../../../../common/accordions/Accordion';
 import AccordionSummary from '../../../../../common/accordions/AccordionSummary';
 import AccordionDetails from '../../../../../common/accordions/AccordionDetails';
-import { selectFiltered, updateFilter } from '../../../../../../utils/redux/schedule/scheduleSlice';
+import FilterChips from './FilterChips';
 
 function FilterAccordion({ tag }) {
-  const dispatch = useDispatch();
-  const filtered = useSelector(selectFiltered);
-  const handleClick = (state) => {
-    console.info(state.target.innerText);
-    dispatch(updateFilter(state.target.innerText));
-  };
-  const [checked, setChecked] = useState(true);
-
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
-  };
   return (
     <Accordion>
       <AccordionSummary
@@ -31,25 +15,7 @@ function FilterAccordion({ tag }) {
       </AccordionSummary>
       <AccordionDetails>
         {tag.nested.map((el) => (
-          <Box key={el.type}>
-            <Stack direction="row" alignItems="center">
-              <Typography>{el.type}</Typography>
-              <Checkbox
-                checked={checked}
-                onChange={handleChange}
-                inputProps={{ 'aria-label': 'controlled' }}
-              />
-            </Stack>
-            {el.categories.map((cat) => (
-              <Chip
-                key={cat}
-                label={cat}
-                variant={filtered.findIndex((word) => word === cat) === -1 ? 'outlined' : 'contained'}
-                onClick={handleClick}
-                sx={{ mr: 1, mb: 1, color: filtered.findIndex((word) => word === cat) === -1 ? el.color : 'white' }}
-              />
-            ))}
-          </Box>
+          <FilterChips key={el.type} nested={el} />
         ))}
       </AccordionDetails>
     </Accordion>
---
+++ src/components/layouts/TopBar/buttons/FilterButton/inputs/FilterChips.js
@@ -0,0 +1,51 @@
+import Checkbox from '@mui/material/Checkbox';
+import {
+  Box, Chip, Stack, Typography,
+} from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { selectFiltered, updateFilter } from '../../../../../../utils/redux/schedule/scheduleSlice';
+
+function FilterChips({ nested }) {
+  const dispatch = useDispatch();
+  const filtered = useSelector(selectFiltered);
+  const [checked, setChecked] = useState(true);
+
+  const handleClick = (state) => {
+    console.info(state.target.innerText);
+    dispatch(updateFilter(state.target.innerText));
+  };
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  const isFiltered = (cat) => filtered.findIndex((word) => word === cat) === -1;
+
+  return (
+    <Box key={nested.type}>
+      <Stack direction="row" alignItems="center">
+        <Typography>{nested.type}</Typography>
+        <Checkbox
+          checked={checked}
+          onChange={handleChange}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+      </Stack>
+      {nested.categories.map((cat) => (
+        <Chip
+          key={cat}
+          label={cat}
+          variant={isFiltered(cat) ? 'outlined' : 'contained'}
+          onClick={handleClick}
+          sx={{
+            mr: 1,
+            mb: 1,
+            color: isFiltered(cat) ? nested.color : 'white',
+          }}
+        />
+      ))}
+    </Box>
+  );
+}
+export default FilterChips;
