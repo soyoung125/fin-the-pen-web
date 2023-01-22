7b94eb8d44407f5eaaf7b285bf00782a5bc0340a
soyoung125 <sypark041754@gmail.com>
2023-01-22 15:17:34 +0900

    feat: 지출 내역 카드 디자인

--- src/components/analysis/detailCard/AnalysisDetailCard.js
+++ src/components/analysis/detailCard/AnalysisDetailCard.js
@@ -1,9 +1,12 @@
 /* eslint-disable max-len */
 import {
   Alert,
-  Card, CardContent, CardHeader, IconButton, List, ListItem, ListItemText, Stack, Typography,
+  Button,
+  Card, CardContent, CardHeader, IconButton, Paper, Stack, Typography,
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import moment from 'moment';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import AssetManagement from './AssetManagement';
 import ALERTS from '../../../utils/constants/alerts';
 
@@ -27,16 +30,34 @@ function AnalysisDetailCard({ closeDetailCard, selectedItem }) {
           <Typography variant="h4">{`${selectedItem.label} 지출 내역`}</Typography>
           <Typography>{`총 ${selectedItem.history.length}건`}</Typography>
         </Stack>
-        <List>
+        <Stack sx={{ borderRadius: 3, marginBottom: 2 }}>
           {selectedItem.history.map((s) => (
-            <ListItem>
-              <ListItemText
-                key={Math.random()}
-                primary={s.event_name}
-              />
-            </ListItem>
+            <Paper sx={{ marginY: 1, padding: 1 }}>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>{moment(s.date).format('MM월 DD일')}</Typography>
+                <Typography>{s.event_name}</Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="space-between">
+                <Typography>
+                  <AccessTimeIcon fontSize="small" />
+                  {s.start_time}
+                </Typography>
+                <Typography>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{
+                      borderRadius: 5, minWidth: 0, width: '20px', height: '20px',
+                    }}
+                  >
+                    {s.type}
+                  </Button>
+                  {s.expected_spending}
+                </Typography>
+              </Stack>
+            </Paper>
           ))}
-        </List>
+        </Stack>
         <AssetManagement />
         <Alert color={ALERTS[random].color} sx={{ width: '100%' }} icon={ALERTS[random].icon}>
           {ALERTS[random].message}
