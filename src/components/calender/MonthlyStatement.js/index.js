8f8e79d6b443eeae8b5a77fb659795f3bae60c34
soyoung125 <sypark041754@gmail.com>
2023-01-24 22:32:54 +0900

    design: 메인 페이지 하단 수입 지출 카드 디자인 수정

--- src/components/calender/MonthlyStatement.js/index.js
+++ src/components/calender/MonthlyStatement.js/index.js
@@ -2,24 +2,35 @@ import {
   Box,
   Grid, Paper, Stack, Typography,
 } from '@mui/material';
+import { lightBlue, pink } from '@mui/material/colors';
 
 function MonthlyStatement() {
   return (
     <Box mx={2}>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Paper elevation={3}>
+          <Paper
+            elevation={3}
+            sx={{
+              height: 90, borderRadius: 5, padding: 2, backgroundColor: pink[100], color: 'white',
+            }}
+          >
             <Stack>
-              <Typography>수입</Typography>
-              <Typography>1000000원</Typography>
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>수입</Typography>
+              <Box sx={{ textAlign: 'end', fontWeight: 'bold' }}>1000000원</Box>
             </Stack>
           </Paper>
         </Grid>
         <Grid item xs={6}>
-          <Paper elevation={3}>
+          <Paper
+            elevation={3}
+            sx={{
+              height: 90, borderRadius: 5, padding: 2, backgroundColor: lightBlue[200], color: 'white',
+            }}
+          >
             <Stack>
-              <Typography>지출</Typography>
-              <Typography>30000원</Typography>
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>지출</Typography>
+              <Box sx={{ textAlign: 'end', fontWeight: 'bold' }}>1000000원</Box>
             </Stack>
           </Paper>
         </Grid>
