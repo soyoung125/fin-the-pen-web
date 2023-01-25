c36584dc0fe54c66fd62e7f291e6ebe7d3108ad1
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-26 00:30:05 +0900

    fix: mockAPI의 fetchMockLogin()이 현재 백엔드 스키마와 일치하도록 수정

--- src/utils/redux/mockAPI.js
+++ src/utils/redux/mockAPI.js
@@ -4,28 +4,29 @@
 export function fetchMockLogin() {
   return new Promise((resolve) => setTimeout(() => resolve({
     data: {
-      id: 'guest',
-      user_id: 'guest',
+      id: 0,
+      user_id: 'guest@finthepen.com',
       name: 'guest',
-      password: '',
       bday: '2000-01-01',
-      account_number: '123456789',
-      goals: [
-        {
-          id: 0,
-          question: '일년 동안의 저축 목표 금액은 얼마인가요?',
-          label: '저축 목표 금액',
-          answer: null,
-          skip: true,
-        },
-        {
-          id: 1,
-          question: '월 기준 소비 목표 금액은 얼마인가요?',
-          label: '소비 목표 금액',
-          answer: null,
-          skip: true,
-        },
-      ],
+      registerDate: '2023-01-25T14:57:08.023+00:00',
+      phone_number: '010-4413-5698',
+      // account_number: '123456789', // 백엔드에서 먼저 추가 필요(?)
+      // goals: [
+      //   {
+      //     id: 0,
+      //     question: '일년 동안의 저축 목표 금액은 얼마인가요?',
+      //     label: '저축 목표 금액',
+      //     answer: null,
+      //     skip: true,
+      //   },
+      //   {
+      //     id: 1,
+      //     question: '월 기준 소비 목표 금액은 얼마인가요?',
+      //     label: '소비 목표 금액',
+      //     answer: null,
+      //     skip: true,
+      //   },
+      // ],
     },
   }), 2000));
 }
