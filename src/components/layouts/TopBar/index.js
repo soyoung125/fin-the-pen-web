a1d7398e283c56a39fdfb264b882d40faedf80fa
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-27 14:53:26 +0900

    feat: guest mode가 활성화 된 경우, 헤더를 통해 알리는 기능 추가

--- src/components/layouts/TopBar/index.js
+++ src/components/layouts/TopBar/index.js
@@ -3,12 +3,14 @@ import {
   Box, Button, Paper, Stack, Typography,
 } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 // import NotificationsIcon from '@mui/icons-material/Notifications';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import { selectGuestMode, selectHeaderMode, selectHeaderOpen } from '../../../utils/redux/common/commonSlice';
+import {
+  selectGuestMode, selectHeaderMode, selectHeaderOpen, setGuestModeFalse, setGuestModeTrue,
+} from '../../../utils/redux/common/commonSlice';
 import FullScreenDialog from '../FullScreenDialog';
 import RoundedButton from '../../common/RoundedButton';
 import PATH from '../../../utils/constants/path';
@@ -24,7 +26,7 @@ function LogoButton({ navigate }) {
       <img src={logo} alt="" width="26px" height="26px" />
       {
         guestMode && (
-          <Typography>GUEST MODE</Typography>
+          <Typography ml={1}>GUEST MODE</Typography>
         )
       }
     </RoundedButton>
@@ -33,17 +35,18 @@ function LogoButton({ navigate }) {
 
 function TopBar() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const headerOpen = useSelector(selectHeaderOpen);
   const headerMode = useSelector(selectHeaderMode);
   const [fullScreenModalOpen, setFullScreenModalOpen] = useState(false);
-  const [ask, setAsk] = useState({
-    id: 0,
-    question: '',
-    label: '',
-    answer: null,
-    skip: false,
-  });
+  // const [ask, setAsk] = useState({
+  //   id: 0,
+  //   question: '',
+  //   label: '',
+  //   answer: null,
+  //   skip: false,
+  // });
 
   // useEffect(() => {
   //   if (user !== null) {
@@ -57,6 +60,16 @@ function TopBar() {
   //     }
   //   }
   // }, [user]);
+
+  useEffect(() => {
+    // 옵셔널 체이닝 사용하면 eslint에서 오류 발생
+    if (user && user.name === 'guest') {
+      console.warn('게스트 모드로 동작합니다. 게스트 모드에서는 데이터가 저장되지 않습니다.');
+      dispatch(setGuestModeTrue());
+    } else {
+      dispatch(setGuestModeFalse());
+    }
+  }, [user]);
 
   return (
     <>
@@ -132,7 +145,11 @@ function TopBar() {
           )
         }
       </Box>
-      <FullScreenDialog open={fullScreenModalOpen} setOpen={setFullScreenModalOpen} ask={ask} />
+      {/* <FullScreenDialog
+        open={fullScreenModalOpen}
+        setOpen={setFullScreenModalOpen}
+        ask={ask}
+      /> */}
     </>
 
   );
