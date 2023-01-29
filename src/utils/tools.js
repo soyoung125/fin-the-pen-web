9b2ffc672d6e0766340569435c1af58c21122ffe
soyoung125 <sypark041754@gmail.com>
2023-01-30 01:13:38 +0900

    refactor: 매개변수 이름 변경

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -16,22 +16,22 @@ export const isObjectValuesEmpty = (obj) => (
 
 /**
   * 수입, 지출액을 계산하기 위한 함수
-  * @param {Moment} renderDay 수입, 지출액을 계산할 기준일
+  * @param {Moment} baseDate 수입, 지출액을 계산할 기준일
   * @param {String} unit 'day', 'week', 'month' 같은 날/주/월을 확인하기 위한 단위
   * @param {Sting} type '-', '+' 수입/지출을 확인하기 위한 매개변수
   * @returns 일/주/월별 수입/지출 액
   */
-export const calculateIncomeExpenditure = (schedules, renderDay, unit, type) => {
+export const calculateIncomeExpenditure = (schedules, baseDate, unit, type) => {
   let result = 0;
   if (type === '-') {
-    result = schedules.filter((s) => renderDay.isSame(s.date, unit))
+    result = schedules.filter((s) => baseDate.isSame(s.date, unit))
       .reduce((sum, current) => (
         current.type === type
           ? sum - parseInt(current.expected_spending, 10)
           : sum
       ), result);
   } else {
-    result = schedules.filter((s) => renderDay.isSame(s.date, unit))
+    result = schedules.filter((s) => baseDate.isSame(s.date, unit))
       .reduce((sum, current) => (
         current.type === type
           ? sum + parseInt(current.expected_spending, 10)
