ed6f24207a0dc96a314d2fc7161afbe86f64f95a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-30 00:44:15 +0900

    feat: 서버로부터 일정 데이터 수신하는 기능 구현

--- src/components/calender/scheduleList/ScheduleList.js
+++ src/components/calender/scheduleList/ScheduleList.js
@@ -2,11 +2,14 @@ import {
   Box, Drawer, Stack, Typography,
 } from '@mui/material';
 import moment from 'moment';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { CATEGORIES } from '../../../utils/constants/categories';
 import { SCHEDULE_DRAWER_MODE } from '../../../utils/constants/schedule';
+import { fetchSchedules } from '../../../utils/redux/API';
+import { selectGuestMode } from '../../../utils/redux/common/commonSlice';
 import { selectDate, selectSchedules } from '../../../utils/redux/schedule/scheduleSlice';
+import { selectUser } from '../../../utils/redux/user/userSlice';
 import ScheduleDrawer from '../ScheduleDrawer';
 import ScheduleCard from './ScheduleCard';
 
@@ -15,11 +18,26 @@ function ScheduleList() {
   const [selectedSchedule, setSelectedSchedule] = useState(null);
   const schedules = useSelector(selectSchedules);
   const date = moment(useSelector(selectDate)).format('YYYY-MM-DD');
+  const user = useSelector(selectUser);
+  const guestMode = useSelector(selectGuestMode);
 
   const handleModal = (schedule) => {
     setSelectedSchedule(schedule);
     setBottomDrawerOpen(true);
   };
+
+  const getSchedules = async () => {
+    console.log('전체 데이터를 수신할 위치');
+    const result = await fetchSchedules(user.user_id);
+    console.log(result);
+  };
+
+  useEffect(() => {
+    // 게스트 모드가 아닌 경우에만 서버에 데이터를 요청
+    if (user && !guestMode) {
+      getSchedules();
+    }
+  }, [date]);
 
   return (
     <>
--- src/utils/redux/API.js
+++ src/utils/redux/API.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable consistent-return */
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
@@ -27,5 +28,17 @@ export const fetchCreateSchedule = async (schedule) => {
     return response.data;
   } catch (err) {
     alert(err);
+  }
+};
+
+export const fetchSchedules = async (user_id) => {
+  try {
+    const response = await axios.post('/getAllSchedules', user_id);
+    // alert(JSON.stringify(response));
+    return response.data;
+  } catch (err) {
+    // 나중에 alert로 복구 예정
+    // alert(err);
+    console.log(err);
   }
 };
