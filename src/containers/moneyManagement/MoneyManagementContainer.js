afbf006dd600ceb1af328fbbddd507e29a9ba4a3
soyoung125 <sypark041754@gmail.com>
2023-01-31 23:20:34 +0900

    feat: 자산페이지 스케줄현황 카드 추가

--- src/containers/moneyManagement/MoneyManagementContainer.js
+++ src/containers/moneyManagement/MoneyManagementContainer.js
@@ -1,12 +1,22 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
-import { Box, Grid, Paper } from '@mui/material';
+import {
+  Box, Grid, Paper, Stack,
+} from '@mui/material';
 import {
   lightBlue, pink, teal, yellow,
 } from '@mui/material/colors';
+import moment from 'moment/moment';
+import { useSelector } from 'react-redux';
 import { CATEGORIES } from '../../utils/constants/categories';
+import { selectSchedules } from '../../utils/redux/schedule/scheduleSlice';
 
 function MoneyManagementContainer() {
+  const schedules = useSelector(selectSchedules);
+  const today = moment();
+
+  console.log(schedules.filter((s) => today.isSame(s.date, 'month')));
+
   return (
     <Box sx={{ m: 3 }}>
       {/* 자산 관리
@@ -55,6 +65,37 @@ function MoneyManagementContainer() {
           </Grid>
         </Grid>
       </Paper>
+
+      <Box sx={{ typography: 'h4', fontWeight: 'bold', marginTop: 5 }}>
+        My 스케줄 현황
+      </Box>
+
+      <Box sx={{
+        marginTop: 3, border: '2px solid', borderRadius: 2, borderColor: 'primary.main',
+      }}
+      >
+        <Stack direction="row">
+          <Stack width="50%" sx={{ textAlign: 'center', paddingY: 2 }}>
+            <Box>{`${today.format('M월')} 남은 일정`}</Box>
+            <Box sx={{
+              typography: 'h4', fontWeight: 'bold', color: 'primary.main', marginTop: 2,
+            }}
+            >
+              {`${schedules.filter((s) => today.isSame(s.date, 'month') && today.isSameOrBefore(s.date, 'day')).length}개`}
+            </Box>
+          </Stack>
+
+          <Stack width="50%" sx={{ textAlign: 'center', paddingY: 2 }}>
+            <Box>추천 소비 금액</Box>
+            <Box sx={{
+              typography: 'h4', fontWeight: 'bold', color: 'primary.main', marginTop: 2,
+            }}
+            >
+              xxxxx원
+            </Box>
+          </Stack>
+        </Stack>
+      </Box>
     </Box>
   );
 }
