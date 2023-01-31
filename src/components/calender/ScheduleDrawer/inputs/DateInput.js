aebc09ee87232e740c82a41b6d71f65d261aa921
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-31 23:49:32 +0900

    feat: 시각 선택기 제작을 위한 모달 뼈대 구축

--- src/components/calender/ScheduleDrawer/inputs/DateInput.js
+++ src/components/calender/ScheduleDrawer/inputs/DateInput.js
@@ -1,14 +1,20 @@
 import {
+  Box,
+  Button,
+  Divider,
   Stack, TextField,
 } from '@mui/material';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { SCHEDULE_DRAWER } from '../../../../utils/constants/schedule';
 import { selectSchedule } from '../../../../utils/redux/schedule/scheduleSlice';
+import ModalStaticBackdrop from '../../../layouts/ModalStaticBackdrop';
 import { updateSchedule } from '../utils/schedule';
 
 function DateInput() {
   const dispatch = useDispatch();
   const schedule = useSelector(selectSchedule);
+  const [modalOpen, setModalOpen] = useState(false);
   const changeSchedule = (state) => {
     updateSchedule(dispatch, schedule, state);
   };
@@ -64,6 +70,62 @@ function DateInput() {
           size="small"
         />
       </Stack>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={2}
+        sx={{ width: '100%' }}
+      >
+        <TextField
+          id="start_time"
+          label={`${SCHEDULE_DRAWER.start_time} (동작x 구현중)`}
+          fullWidth
+          value={schedule.start_time}
+          onClick={() => setModalOpen(!modalOpen)}
+          size="small"
+        />
+        <TextField
+          id="end_time"
+          label={`${SCHEDULE_DRAWER.end_time} (동작x 구현중)`}
+          fullWidth
+          value={schedule.end_time}
+          onClick={() => setModalOpen(!modalOpen)}
+          size="small"
+        />
+      </Stack>
+      <ModalStaticBackdrop
+        keepMounted
+        width="xl"
+        open={modalOpen}
+        component={(
+          <Box>
+            <Stack p={2}>
+              <Stack direction="row" justifyContent="center">
+                <Button>오전</Button>
+                <Button>오후</Button>
+              </Stack>
+              <Box my={3}>
+                <Divider />
+              </Box>
+              <Box>
+                {
+                  Array.from({ length: 12 }, (_, i) => i + 1).map((n) => <Button>{n}</Button>)
+                }
+              </Box>
+              <Box my={3}>
+                <Divider />
+              </Box>
+              <Stack direction="row" justifyContent="center">
+                <Button>00</Button>
+                <Button>30</Button>
+              </Stack>
+            </Stack>
+
+            <Button fullWidth variant="contained" onClick={() => setModalOpen(false)}>닫기</Button>
+          </Box>
+        )}
+      />
     </>
 
   );
