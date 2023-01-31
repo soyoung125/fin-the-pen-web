34547315619d03fb8ac41f4e110fcb3fdb04c3cc
soyoung125 <sypark041754@gmail.com>
2023-01-31 23:45:44 +0900

    refactor: 자산 페이지 설정 메뉴 카드 컴포넌트 분리

--- src/components/momeyManagement/SettingsPaper/index.js
+++ src/components/momeyManagement/SettingsPaper/index.js
@@ -1,7 +1,6 @@
-import { Box, Grid, Paper } from '@mui/material';
-import {
-  lightBlue, pink, teal, yellow,
-} from '@mui/material/colors';
+import { Grid, Paper } from '@mui/material';
+import momeyManagementSettings from '../../../utils/constants/settings';
+import SettingCard from './SettingCard';
 
 function SettingsPaper() {
   return (
@@ -10,38 +9,9 @@ function SettingsPaper() {
     }}
     >
       <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Box sx={{
-            backgroundColor: pink[200], paddingY: 5, borderRadius: 4,
-          }}
-          >
-            저축 목표 금액 설정
-          </Box>
-        </Grid>
-        <Grid item xs={6}>
-          <Box sx={{
-            backgroundColor: yellow[200], paddingY: 5, borderRadius: 4,
-          }}
-          >
-            정기 입출금액 설정
-          </Box>
-        </Grid>
-        <Grid item xs={6}>
-          <Box sx={{
-            backgroundColor: lightBlue[200], paddingY: 5, borderRadius: 4,
-          }}
-          >
-            카테고리별 자산 설정
-          </Box>
-        </Grid>
-        <Grid item xs={6}>
-          <Box sx={{
-            backgroundColor: teal[200], paddingY: 5, borderRadius: 4,
-          }}
-          >
-            일정 관리
-          </Box>
-        </Grid>
+        {momeyManagementSettings.map((s) => (
+          <SettingCard setting={s} />
+        ))}
       </Grid>
     </Paper>
   );
---
+++ src/utils/constants/settings.js
@@ -0,0 +1,24 @@
+import {
+  lightBlue, pink, teal, yellow,
+} from '@mui/material/colors';
+
+const momeyManagementSettings = Object.freeze([
+  {
+    title: '저축 목표 금액 설정',
+    color: pink[200],
+  },
+  {
+    title: '정기 입출금액 설정',
+    color: yellow[200],
+  },
+  {
+    title: '카테고리별 자산 설정',
+    color: lightBlue[200],
+  },
+  {
+    title: '일정 관리',
+    color: teal[200],
+  },
+]);
+
+export default momeyManagementSettings;
