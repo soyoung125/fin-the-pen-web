b946a75c7e8f5bd5d2f510e8868602df20f70e06
soyoung125 <sypark041754@gmail.com>
2023-02-01 16:30:23 +0900

    feat: 자산관리 페이지 비로그인시 알람띄우기

--- src/containers/moneyManagement/MoneyManagementContainer.js
+++ src/containers/moneyManagement/MoneyManagementContainer.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 import {
+  Alert,
   Box, Grid, Paper, Stack,
 } from '@mui/material';
 import {
@@ -12,29 +13,33 @@ import ScheduleStatusCard from '../../components/momeyManagement/ScheduleStatusC
 import SettingsPaper from '../../components/momeyManagement/SettingsPaper';
 import { CATEGORIES } from '../../utils/constants/categories';
 import { selectSchedules } from '../../utils/redux/schedule/scheduleSlice';
+import { selectUser } from '../../utils/redux/user/userSlice';
 
 function MoneyManagementContainer() {
   const schedules = useSelector(selectSchedules);
+  const user = useSelector(selectUser);
   const today = moment();
 
   return (
     <Box sx={{ m: 3 }}>
-      <Box sx={{ typography: 'h4', fontWeight: 'bold' }}>
-        핀더팬과 함께 &quot;OOO&quot; 님의
-        <br />
-        자산과 일정을 관리하세요
-      </Box>
+      {user
+        ? (
+          <>
+            <Box sx={{ typography: 'h4', fontWeight: 'bold' }}>
+              <Box>{`핀더팬과 함께 "${user.name}" 님의`}</Box>
+              <Box>자산과 일정을 관리하세요</Box>
+            </Box>
+            <SettingsPaper />
+            <Box sx={{ typography: 'h4', fontWeight: 'bold' }}>
+              My 스케줄 현황
+            </Box>
+            <ScheduleStatusCard
+              month={today.format('M월')}
+              numberOfSchedule={schedules.filter((s) => today.isSame(s.date, 'month') && today.isSameOrBefore(s.date, 'day')).length}
+            />
 
-      <SettingsPaper />
-
-      <Box sx={{ typography: 'h4', fontWeight: 'bold' }}>
-        My 스케줄 현황
-      </Box>
-
-      <ScheduleStatusCard
-        month={today.format('M월')}
-        numberOfSchedule={schedules.filter((s) => today.isSame(s.date, 'month') && today.isSameOrBefore(s.date, 'day')).length}
-      />
+          </>
+        ) : <Alert severity="error">로그인이 필요한 페이지입니다.</Alert>}
     </Box>
   );
 }
