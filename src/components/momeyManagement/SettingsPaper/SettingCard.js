337349584ee7aeba05a5cf596e2b289071cd1e87
soyoung125 <sypark041754@gmail.com>
2023-02-01 16:41:58 +0900

    design: 자산관리페이지 설정카드 디자인 수정

--- src/components/momeyManagement/SettingsPaper/SettingCard.js
+++ src/components/momeyManagement/SettingsPaper/SettingCard.js
@@ -1,21 +1,22 @@
-import { Box, Grid } from '@mui/material';
+import { Box, CardActionArea, Grid } from '@mui/material';
 
 function SettingCard({ setting }) {
   return (
     <Grid item xs={6}>
-      <Box sx={{
-        backgroundColor: setting.color,
-        paddingY: 5,
-        paddingX: 1,
-        borderRadius: 4,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100%',
-      }}
-      >
-        {setting.title}
-      </Box>
+      <CardActionArea>
+        <Box sx={{
+          height: '120px',
+          backgroundColor: setting.color,
+          paddingX: 1,
+          borderRadius: 4,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        >
+          {setting.title}
+        </Box>
+      </CardActionArea>
     </Grid>
   );
 }
