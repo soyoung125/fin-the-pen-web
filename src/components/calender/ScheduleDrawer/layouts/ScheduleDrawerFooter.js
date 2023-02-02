806913deb2956f340fb6f04b35529d82eeee8c77
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-02 19:44:29 +0900

    refactor: 일정 등록 및 수정 버튼 통합
    - 게스트 모드와 일반 모드를 외관 상 구분짓지 못하도록 개선
    - 현재 상태가 게스트 모드인지를 자동으로 판별하여 수행

--- src/components/calender/ScheduleDrawer/layouts/ScheduleDrawerFooter.js
+++ src/components/calender/ScheduleDrawer/layouts/ScheduleDrawerFooter.js
@@ -1,23 +1,61 @@
 /* eslint-disable no-unused-vars */
 import { Button, Stack } from '@mui/material';
-import { useSelector } from 'react-redux';
+import moment from 'moment';
+import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { NEED_SIGN_IN, NOT_AVAILABLE } from '../../../../utils/constants/common';
-import { NEED_TITLE, SCHEDULE_DRAWER } from '../../../../utils/constants/schedule';
+import {
+  NEED_TITLE, REPEAT_CYCLE, SCHEDULE_DRAWER, SCHEDULE_DRAWER_MODE,
+} from '../../../../utils/constants/schedule';
 import { fetchCreateSchedule } from '../../../../utils/redux/API';
 import { selectGuestMode } from '../../../../utils/redux/common/commonSlice';
-import { selectSchedule } from '../../../../utils/redux/schedule/scheduleSlice';
+import { addSchedule, modifySchedule, selectSchedule } from '../../../../utils/redux/schedule/scheduleSlice';
 import { selectUser } from '../../../../utils/redux/user/userSlice';
 import { executeFunctionByGuestMode } from '../../../../utils/tools';
 import DeprecatedButton from './DeprecatedButton';
+
+/**
+ * 각종 로직들 모듈로 이전 예정
+ */
 
 function ScheduleDrawerFooter({ mode, setBottomDrawerOpen }) {
   const user = useSelector(selectUser);
   const guestMode = useSelector(selectGuestMode);
   const schedule = useSelector(selectSchedule);
+  const dispatch = useDispatch();
+
   // guest mode start
+
+  const addNewSchedule = () => {
+    // 반복 일정 추가
+    if ((schedule.repeating_cycle !== '없음') && (schedule.repeat_deadline !== '없음')) {
+      let repeatDate = moment(schedule.date).add(1, REPEAT_CYCLE[schedule.repeating_cycle]);
+      while (moment(schedule.repeat_endDate).isSameOrAfter(repeatDate)) {
+        dispatch(addSchedule({ ...schedule, id: uuidv4(), date: repeatDate.format('YYYY-MM-DD') }));
+        repeatDate = moment(repeatDate).add(1, REPEAT_CYCLE[schedule.repeating_cycle]);
+      }
+    }
+    // 원래 일정 추가
+    dispatch(addSchedule({ ...schedule, id: uuidv4() }));
+    setBottomDrawerOpen(false);
+  };
+
+  const modifySelectedSchedule = () => {
+    dispatch(modifySchedule(schedule));
+    setBottomDrawerOpen(false);
+  };
+
   const guestHandler = () => {
-    alert(`[${mode}] 게스트 모드는 아직 기능을 연결하지 않았습니다. 기존 버튼을 이용해주세요`);
+    alert(`[${mode}] 게스트 모드에서 동작!`);
+    if (schedule.event_name.length === 0) {
+      alert(NEED_TITLE);
+      return;
+    }
+    if (mode === SCHEDULE_DRAWER_MODE.생성) {
+      addNewSchedule();
+    } else {
+      modifySelectedSchedule();
+    }
   };
   // guest mode end
 
@@ -60,21 +98,21 @@ function ScheduleDrawerFooter({ mode, setBottomDrawerOpen }) {
       direction="row"
       spacing={1}
     >
-      <DeprecatedButton
+      {/* <DeprecatedButton
         mode={mode}
         setBottomDrawerOpen={setBottomDrawerOpen}
-      />
+      /> */}
 
       <Button
         variant="contained"
-        color="warning"
+        // color="warning"
         fullWidth
         disabled={user === null}
         onClick={() => handleSubmit()}
       >
         {user === null
           ? NEED_SIGN_IN
-          : `${SCHEDULE_DRAWER.add_schedule[mode]} (fetch)`}
+          : `${SCHEDULE_DRAWER.add_schedule[mode]}`}
       </Button>
     </Stack>
   );
