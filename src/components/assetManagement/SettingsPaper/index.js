ae0baf5745e2250aebb7baf5261d0a71d2cf876d
soyoung125 <sypark041754@gmail.com>
2023-02-06 00:55:17 +0900

    refactor: 자산관리 페이지 roundedPaper 적용

--- src/components/assetManagement/SettingsPaper/index.js
+++ src/components/assetManagement/SettingsPaper/index.js
@@ -1,21 +1,17 @@
-import { Grid, Paper } from '@mui/material';
+import { Grid } from '@mui/material';
 import assetManagements from '../../../utils/constants/managements';
+import RoundedPaper from '../../common/RoundedPaper';
 import SettingCard from './SettingCard';
 
 function SettingsPaper() {
   return (
-    <Paper
-      elevation={8}
-      sx={{
-        marginTop: 2, marginBottom: 5, padding: 2, textAlign: 'center', borderRadius: 3,
-      }}
-    >
+    <RoundedPaper>
       <Grid container spacing={2} alignItems="stretch">
         {assetManagements.map((s) => (
           <SettingCard setting={s} key={Math.random()} />
         ))}
       </Grid>
-    </Paper>
+    </RoundedPaper>
   );
 }
 
