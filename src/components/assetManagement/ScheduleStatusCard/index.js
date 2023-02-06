fc5c4428105fa836246d1ee06e4533b59116e765
soyoung125 <sypark041754@gmail.com>
2023-02-06 22:00:38 +0900

    refactor: 스케줄 현환에 roundedBorderBox 컴포넌트 적용

--- src/components/assetManagement/ScheduleStatusCard/index.js
+++ src/components/assetManagement/ScheduleStatusCard/index.js
@@ -1,16 +1,14 @@
 import { Box, Stack } from '@mui/material';
+import RoundedBorderBox from '../../common/RoundedBorderBox';
 import StatusStack from './StatusStack';
 
 function ScheduleStatusCard({ month, numberOfSchedule }) {
   return (
     <>
-      <Box sx={{ typography: 'h6', fontWeight: 'bold' }}>
+      <Box sx={{ typography: 'h6', fontWeight: 'bold', mb: 1 }}>
         My 스케줄 현황
       </Box>
-      <Box sx={{
-        marginTop: 1, border: '2px solid', borderRadius: 2, borderColor: 'primary.main',
-      }}
-      >
+      <RoundedBorderBox>
         <Stack direction="row">
           <StatusStack
             title={`${month} 남은 일정`}
@@ -22,7 +20,7 @@ function ScheduleStatusCard({ month, numberOfSchedule }) {
             content="xxxxx원"
           />
         </Stack>
-      </Box>
+      </RoundedBorderBox>
 
     </>
   );
