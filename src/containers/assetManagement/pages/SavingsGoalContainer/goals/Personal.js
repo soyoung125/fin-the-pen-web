d56c21f4b0c096ac9a263d3c19316aadadc4b4a5
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-06 22:56:37 +0900

    feat: Personal Goal 모달 구현
    - redux로 관리하는 기능은 아직 넣지 않음

--- src/containers/assetManagement/pages/SavingsGoalContainer/goals/Personal.js
+++ src/containers/assetManagement/pages/SavingsGoalContainer/goals/Personal.js
@@ -1,13 +1,46 @@
+/* eslint-disable no-unused-vars */
 import {
-  Box, Grid, IconButton, Stack, Typography,
+  Box, Button, Divider, FormControl, Grid, IconButton, InputAdornment,
+  OutlinedInput, Stack, Switch, TextField, Typography,
 } from '@mui/material';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import ClearIcon from '@mui/icons-material/Clear';
 import ModalStaticBackdrop from '../../../../../components/layouts/ModalStaticBackdrop';
 import RoundedBorderBox from '../../../../../components/common/RoundedBorderBox';
+import { SOMETHING_IS_WRONG } from '../../../../../utils/constants/common';
 
 function Personal() {
   const [personalGoalModalOpen, setPersonalGoalModalOpen] = useState(false);
+
+  const init = {
+    name: 'i-Mac',
+    money: 2027100,
+    deadline: '2024-01-01',
+    type: 'month', // day||month
+    autoSaving: true,
+  };
+  const [personalGoal, setPersonalGoal] = useState(init);
+
+  const changePersonalGoal = (state) => {
+    setPersonalGoal({ ...personalGoal, [state.target.id]: state.target.value });
+  };
+
+  const divisionByType = (type, money) => {
+    switch (type) {
+      case 'day':
+        return money / 365;
+      case 'month':
+        return money / 12;
+      default:
+        return SOMETHING_IS_WRONG;
+    }
+  };
+
+  useEffect(() => {
+    console.log(personalGoal);
+  }, [personalGoal]);
 
   return (
     <>
@@ -59,9 +92,124 @@ function Personal() {
         width="xl"
         open={personalGoalModalOpen}
         component={(
-          <Typography>
-            개인 목표
-          </Typography>
+          <Stack p={2} spacing={1}>
+            <Stack direction="row" alignItems="center" justifyContent="space-between">
+              <IconButton onClick={() => setPersonalGoalModalOpen(false)}>
+                <ClearIcon />
+              </IconButton>
+              <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+                Personal Goal
+              </Typography>
+              <IconButton onClick={() => setPersonalGoal(init)} color="error">
+                <DeleteForeverIcon />
+              </IconButton>
+            </Stack>
+            <Box my={1}>
+              <Divider />
+            </Box>
+
+            <Stack spacing={1}>
+
+              {/* 목표 */}
+              <FormControl fullWidth>
+                <OutlinedInput
+                  id="name"
+                  startAdornment={<InputAdornment position="start">목표</InputAdornment>}
+                  value={personalGoal.name}
+                  onChange={changePersonalGoal}
+                  size="small"
+                />
+              </FormControl>
+
+              {/* 금액 */}
+              <FormControl fullWidth>
+                <OutlinedInput
+                  id="money"
+                  startAdornment={<InputAdornment position="start">금액</InputAdornment>}
+                  value={personalGoal.money}
+                  onChange={changePersonalGoal}
+                  size="small"
+                />
+              </FormControl>
+
+              {/* 기한 */}
+              <TextField
+                id="deadline"
+                type="date"
+                fullWidth
+                InputProps={{
+                  startAdornment: <InputAdornment position="start">기한</InputAdornment>,
+                }}
+                value={personalGoal.deadline}
+                onChange={changePersonalGoal}
+                size="small"
+              />
+
+              {/* 적금 단위 */}
+              <Stack direction="row" spacing={1}>
+                <Button
+                  fullWidth
+                  id="type"
+                  variant={personalGoal.type === 'day' ? 'contained' : 'outlined'}
+                  onClick={() => changePersonalGoal({
+                    target: {
+                      id: 'type',
+                      value: 'day',
+                    },
+                  })}
+                >
+                  하루 기준
+                </Button>
+                <Button
+                  fullWidth
+                  id="type"
+                  variant={personalGoal.type === 'month' ? 'contained' : 'outlined'}
+                  onClick={() => changePersonalGoal({
+                    target: {
+                      id: 'type',
+                      value: 'month',
+                    },
+                  })}
+                >
+                  한달 기준
+                </Button>
+              </Stack>
+
+              {/* 필요 적금 액 */}
+              <FormControl fullWidth>
+                <OutlinedInput
+                  startAdornment={<InputAdornment position="start">필요 적금액</InputAdornment>}
+                  value={divisionByType(personalGoal.type, personalGoal.money)}
+                  size="small"
+                />
+              </FormControl>
+
+              {/* 자동 적금 */}
+              <FormControl fullWidth>
+                <OutlinedInput
+                  startAdornment={<InputAdornment position="start">자동 적금</InputAdornment>}
+                  endAdornment={(
+                    <Switch
+                      checked={personalGoal.autoSaving}
+                      onChange={() => changePersonalGoal({
+                        target: {
+                          id: 'autoSaving',
+                          value: !personalGoal.autoSaving,
+                        },
+                      })}
+                      inputProps={{ 'aria-label': 'controlled' }}
+                    />
+)}
+                  size="small"
+                  readOnly
+                />
+              </FormControl>
+
+            </Stack>
+            <Button fullWidth variant="contained">
+              나만의 목표 설정하기
+            </Button>
+          </Stack>
         )}
       />
     </>
