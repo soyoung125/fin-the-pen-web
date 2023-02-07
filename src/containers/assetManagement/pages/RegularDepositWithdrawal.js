a96e0b4b70eb0a69f58d7ad1ad237a9ed41323fd
soyoung125 <sypark041754@gmail.com>
2023-02-08 02:31:13 +0900

    feat: 정기 입출금액 설정 페이지 소제목 추가

--- src/containers/assetManagement/pages/RegularDepositWithdrawal.js
+++ src/containers/assetManagement/pages/RegularDepositWithdrawal.js
@@ -1,6 +1,53 @@
+import {
+  Box, Button, IconButton, Stack,
+} from '@mui/material';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+
 function RegularDepositWithdrawal() {
   return (
-    <>정기 입출금 설정</>
+    <>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Stack direction="row">
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              borderRadius: 5, minWidth: 0, width: '20px', height: '20px', marginY: 'auto', mr: 1,
+            }}
+          >
+            +
+          </Button>
+          <Box sx={{ typography: 'h6', fontWeight: 'bold' }}>정기 입금 내역</Box>
+        </Stack>
+        <Stack direction="row" alignItems="center" sx={{ color: 'primary.main' }}>
+          <Box>총 n건</Box>
+          <IconButton color="primary" onClick={() => console.log('정기 입금')}>
+            <BorderColorIcon fontSize="small" />
+          </IconButton>
+        </Stack>
+      </Stack>
+
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Stack direction="row">
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              borderRadius: 5, minWidth: 0, width: '20px', height: '20px', marginY: 'auto', mr: 1,
+            }}
+          >
+            -
+          </Button>
+          <Box sx={{ typography: 'h6', fontWeight: 'bold' }}>정기 출금 내역</Box>
+        </Stack>
+        <Stack direction="row" alignItems="center" sx={{ color: 'primary.main' }}>
+          <Box>총 n건</Box>
+          <IconButton color="primary" onClick={() => console.log('정기 출금')}>
+            <BorderColorIcon fontSize="small" />
+          </IconButton>
+        </Stack>
+      </Stack>
+    </>
   );
 }
 
