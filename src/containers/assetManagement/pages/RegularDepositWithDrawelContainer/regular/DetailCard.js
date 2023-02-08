6d040c0895ea36da5ab136deff7dcbd307f3bd40
soyoung125 <sypark041754@gmail.com>
2023-02-08 22:57:21 +0900

    refactor: 컴포넌트 스와이프로 수정.삭제할 수 있는 방법 모색중

--- src/containers/assetManagement/pages/RegularDepositWithDrawelContainer/regular/DetailCard.js
+++ src/containers/assetManagement/pages/RegularDepositWithDrawelContainer/regular/DetailCard.js
@@ -1,20 +1,28 @@
 import { Box, Stack } from '@mui/material';
+import { useState } from 'react';
 import RoundedBorderBox from '../../../../../components/common/RoundedBorderBox';
 
 function DetailCard() {
+  const [positionX, setPositionX] = useState(0);
   return (
-    <RoundedBorderBox>
-      <Stack direction="row" justifyContent="space-between">
-        <Box>
-          <Box sx={{ mb: 1 }}>매달 1일</Box>
-          <Box>OO은행 월급</Box>
-        </Box>
-        <Box sx={{ textAlign: 'end' }}>
-          <Box sx={{ mb: 1 }}>월급날</Box>
-          <Box sx={{ color: 'primary.main' }}>xxxxxxx원</Box>
-        </Box>
-      </Stack>
-    </RoundedBorderBox>
+    <Box
+      onMouseDown={(e) => setPositionX(e.clientX)}
+      onMouseMoveCapture={(e) => console.log(positionX - e.clientX)}
+      onMouseOver={(e) => console.log(positionX - e.clientX)}
+    >
+      <RoundedBorderBox>
+        <Stack direction="row" justifyContent="space-between">
+          <Box>
+            <Box sx={{ mb: 1 }}>매달 1일</Box>
+            <Box>OO은행 월급</Box>
+          </Box>
+          <Box sx={{ textAlign: 'end' }}>
+            <Box sx={{ mb: 1 }}>월급날</Box>
+            <Box sx={{ color: 'primary.main' }}>xxxxxxx원</Box>
+          </Box>
+        </Stack>
+      </RoundedBorderBox>
+    </Box>
   );
 }
 
