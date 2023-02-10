0908f5bc7b721c2b020ac6ffaa376e8d05813bd3
soyoung125 <sypark041754@gmail.com>
2023-02-10 10:33:00 +0900

    feat: swiper 라이브러리 추가

--- src/containers/assetManagement/pages/RegularDepositWithDrawelContainer/regular/DetailCard.js
+++ src/containers/assetManagement/pages/RegularDepositWithDrawelContainer/regular/DetailCard.js
@@ -1,25 +1,40 @@
-/* eslint-disable no-unused-vars */
 import { Box, Stack } from '@mui/material';
-import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import RoundedBorderBox from '../../../../../components/common/RoundedBorderBox';
 
 function DetailCard() {
-  const [positionX, setPositionX] = useState(0);
   return (
-    <Box
-      onTouchStart={(e) => setPositionX(e.changedTouches[0].clientX)}
-      onTouchMove={(e) => console.log(e.changedTouches[0].clientX - positionX)}
-    >
+    <Box>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+        // modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
       <RoundedBorderBox>
-        <Stack direction="row" justifyContent="space-between">
-          <Box>
-            <Box sx={{ mb: 1 }}>매달 1일</Box>
-            <Box>OO은행 월급</Box>
-          </Box>
-          <Box sx={{ textAlign: 'end' }}>
-            <Box sx={{ mb: 1 }}>월급날</Box>
-            <Box sx={{ color: 'primary.main' }}>xxxxxxx원</Box>
-          </Box>
+        <Stack direction="row" sx={{ width: '100%' }}>
+          <Box sx={{ width: 0 }}>delete</Box>
+          <Stack direction="row" justifyContent="space-between">
+            <Box>
+              <Box sx={{ mb: 1 }}>매달 1일</Box>
+              <Box>OO은행 월급</Box>
+            </Box>
+            <Box sx={{ textAlign: 'end' }}>
+              <Box sx={{ mb: 1 }}>월급날</Box>
+              <Box sx={{ color: 'primary.main' }}>xxxxxxx원</Box>
+            </Box>
+          </Stack>
         </Stack>
       </RoundedBorderBox>
     </Box>
