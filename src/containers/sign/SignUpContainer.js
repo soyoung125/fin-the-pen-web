812d8eb040cc1412491c5ea8cee8658a90cd66dd
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-10 17:17:41 +0900

    refactor: 회원가입 페이지의 컴포넌트가 완전히 중앙에 오도록 개선

--- src/containers/sign/SignUpContainer.js
+++ src/containers/sign/SignUpContainer.js
@@ -61,85 +61,111 @@ function SignUpContainer() {
   };
   return (
     <CenterBox>
-      <LogoCircle />
-      <Typography component="h1" variant="h5">
-        계정 가입
-      </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="name"
-          label="성명"
-          name="name"
-          autoFocus
-        />
-
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="이메일 주소"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="비밀번호"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <Button variant="contained" size="small">사용가능</Button>
-              </InputAdornment>
-            ),
-          }}
-        />
-
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="phoneNumber"
-          label="전화번호"
-          name="phoneNumber"
-          autoFocus
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <Button variant="contained" size="small">인증완료</Button>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Stack direction="row" spacing={1} my={2}>
-          <Button
-            fullWidth
-            variant="contained"
-            color="error"
-            onClick={() => navigate(PATH.signIn)}
-          >
-            뒤로가기
-          </Button>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-          >
-            회원가입
-          </Button>
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        px={1}
+      >
+        <LogoCircle />
+        <Stack my={2}>
+          <Typography component="h1" variant="h5">
+            계정 만들기
+          </Typography>
         </Stack>
-      </Box>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{ maxWidth: '400px' }}
+        >
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="성명"
+            name="name"
+            autoFocus
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="이메일 주소"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="비밀번호"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="success"
+                  >
+                    사용가능
+
+                  </Button>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="phoneNumber"
+            label="전화번호"
+            name="phoneNumber"
+            autoFocus
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="success"
+                  >
+                    인증완료
+                  </Button>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Stack direction="row" spacing={1} my={2}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="error"
+              onClick={() => navigate(PATH.signIn)}
+            >
+              뒤로가기
+            </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+            >
+              회원가입
+            </Button>
+          </Stack>
+        </Box>
+      </Stack>
     </CenterBox>
   );
 }
