b860e038c9ad8fe9affa52c92c0224d7602743cb
soyoung125 <sypark041754@gmail.com>
2023-02-13 18:12:28 +0900

    refactor: 정기 입출금 설정 메인 페이지에서 내역 swipe 제거

--- src/containers/assetManagement/pages/RegularDepositWithDrawelContainer/regular/DetailCard.js
+++ src/containers/assetManagement/pages/RegularDepositWithDrawelContainer/regular/DetailCard.js
@@ -1,9 +1,6 @@
 import {
-  Box, Button, Stack,
+  Box, Stack,
 } from '@mui/material';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import SettingsIcon from '@mui/icons-material/Settings';
 import 'swiper/css';
 import { useState } from 'react';
 import RoundedBorderBox from '../../../../../components/common/RoundedBorderBox';
@@ -15,40 +12,22 @@ function DetailCard() {
   return (
     <Box>
       <RoundedBorderBox>
-        <Swiper
-          slidesPerView="auto"
-          className="mySwiper"
-          initialSlide={1}
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          sx={{
+            p: 2,
+          }}
         >
-          <SwiperSlide style={{ display: 'flex', width: 'auto', height: 'auto' }}>
-            <Button variant="contained">
-              <DeleteForeverIcon fontSize="large" />
-            </Button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              sx={{
-                p: 2,
-              }}
-            >
-              <Box>
-                <Box sx={{ mb: 1 }}>매달 1일</Box>
-                <Box>OO은행 월급</Box>
-              </Box>
-              <Box sx={{ textAlign: 'end' }}>
-                <Box sx={{ mb: 1 }}>월급날</Box>
-                <Box sx={{ color: 'primary.main' }}>xxxxxxx원</Box>
-              </Box>
-            </Stack>
-          </SwiperSlide>
-          <SwiperSlide style={{ display: 'flex', width: 'auto', height: 'auto' }}>
-            <Button variant="contained" onClick={() => setSettingModalOpen(true)}>
-              <SettingsIcon fontSize="large" />
-            </Button>
-          </SwiperSlide>
-        </Swiper>
+          <Box>
+            <Box sx={{ mb: 1 }}>매달 1일</Box>
+            <Box>OO은행 월급</Box>
+          </Box>
+          <Box sx={{ textAlign: 'end' }}>
+            <Box sx={{ mb: 1 }}>월급날</Box>
+            <Box sx={{ color: 'primary.main' }}>xxxxxxx원</Box>
+          </Box>
+        </Stack>
       </RoundedBorderBox>
 
       <ModifyModal settingModalOpen={settingModalOpen} setSettingModalOpen={setSettingModalOpen} />
