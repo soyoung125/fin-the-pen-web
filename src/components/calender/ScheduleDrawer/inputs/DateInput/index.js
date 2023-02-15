bceeea6c3050d8b576b8715ec3e507ec561476d0
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-15 22:27:51 +0900

    refactor: 일정 등록 시간 선택기 복구

--- src/components/calender/ScheduleDrawer/inputs/DateInput/index.js
+++ src/components/calender/ScheduleDrawer/inputs/DateInput/index.js
@@ -1,40 +1,18 @@
 import {
-  Alert, Stack, TextField,
+  Stack, TextField,
 } from '@mui/material';
-import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { SCHEDULE_DRAWER, WRONG_TIME_ORDER } from '../../../../../utils/constants/schedule';
+import { SCHEDULE_DRAWER } from '../../../../../utils/constants/schedule';
 import { selectSchedule } from '../../../../../utils/redux/schedule/scheduleSlice';
-import { isTimeOrderCorrect } from '../../../../../utils/tools';
-import ModalStaticBackdrop from '../../../../layouts/ModalStaticBackdrop';
 import { updateSchedule } from '../../utils/schedule';
-import TimeSelector from './TimeSelector';
 
 function DateInput() {
   const dispatch = useDispatch();
   const schedule = useSelector(selectSchedule);
-  const [timeId, setTimeId] = useState(null);
-  const [currentTime, setCurrentTime] = useState('09:00');
-  const [modalOpen, setModalOpen] = useState(false);
-  const [error, setError] = useState(null);
 
   const changeSchedule = (state) => {
     updateSchedule(dispatch, schedule, state);
   };
-
-  const openModal = (id, time) => {
-    setTimeId(id);
-    setCurrentTime(time);
-    setModalOpen(!modalOpen);
-  };
-
-  useEffect(() => {
-    if (isTimeOrderCorrect(schedule.start_time, schedule.end_time)) {
-      setError(false);
-    } else {
-      setError(true);
-    }
-  }, [schedule]);
 
   return (
     <>
@@ -50,7 +28,7 @@ function DateInput() {
         onChange={changeSchedule}
         size="small"
       />
-      {/* <Stack
+      <Stack
         direction="row"
         justifyContent="space-between"
         alignItems="center"
@@ -87,53 +65,7 @@ function DateInput() {
           onChange={changeSchedule}
           size="small"
         />
-      </Stack> */}
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        spacing={2}
-        sx={{ width: '100%' }}
-      >
-        <TextField
-          id="start_time"
-          label={`${SCHEDULE_DRAWER.start_time} (beta)`}
-          fullWidth
-          value={schedule.start_time}
-          onClick={() => openModal('start_time', schedule.start_time)}
-          size="small"
-        />
-        <TextField
-          id="end_time"
-          label={`${SCHEDULE_DRAWER.end_time} (beta)`}
-          fullWidth
-          value={schedule.end_time}
-          onClick={() => openModal('end_time', schedule.end_time)}
-          size="small"
-        />
       </Stack>
-      {
-        error && (
-          <Stack justifyContent="center">
-            <Alert color="error">
-              {WRONG_TIME_ORDER}
-            </Alert>
-          </Stack>
-        )
-      }
-      <ModalStaticBackdrop
-        keepMounted
-        width="xs"
-        open={modalOpen}
-        component={(
-          <TimeSelector
-            timeId={timeId}
-            currentTime={currentTime}
-            setModalOpen={setModalOpen}
-            changeSchedule={changeSchedule}
-          />
-        )}
-      />
     </>
 
   );
