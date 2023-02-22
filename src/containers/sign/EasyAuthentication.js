7e1e8fb084e1c3542bd7ef67b3475b7da3024d0a
soyoung125 <sypark041754@gmail.com>
2023-02-22 22:58:00 +0900

    feat: 비밀번호 입력칸 추가

--- src/containers/sign/EasyAuthentication.js
+++ src/containers/sign/EasyAuthentication.js
@@ -1,4 +1,6 @@
-import { Box, Button, Stack } from '@mui/material';
+import {
+  Box, Button, Grid, Stack, TextField,
+} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import LogoCircle from '../../components/common/LogoCircle';
 import CenterBox from '../../components/layouts/CenterBox';
@@ -18,7 +20,83 @@ function EasyAuthentication() {
 
         <Box sx={{ fontWeight: 'bold', fontSize: '17px', color: 'primary.main' }}>설정하신 PIN 6자리를 입력해주세요.</Box>
 
-        <Button fullWidth variant="contained" onClick={() => navigate(-1)}>인증</Button>
+        <Box
+          component="form"
+          // onSubmit={handleSubmit}
+          noValidate
+          sx={{ maxWidth: '400px', mx: 2 }}
+        >
+          <Grid container spacing={1}>
+            <Grid item xs={2}>
+              <TextField
+                margin="normal"
+                required
+                name="pwd1"
+                type="pwd1"
+                id="pwd1"
+                autoComplete="current-password"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <TextField
+                margin="normal"
+                hiddenLabel
+                required
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <TextField
+                margin="normal"
+                hiddenLabel
+                required
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <TextField
+                margin="normal"
+                hiddenLabel
+                required
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <TextField
+                margin="normal"
+                hiddenLabel
+                required
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <TextField
+                margin="normal"
+                hiddenLabel
+                required
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+
+          <Button fullWidth variant="contained" onClick={() => navigate(-1)}>인증</Button>
+        </Box>
       </Stack>
     </CenterBox>
   );
