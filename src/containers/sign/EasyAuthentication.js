95be41ed83a11b61e6386d78e795471f17b14070
soyoung125 <sypark041754@gmail.com>
2023-02-22 23:52:18 +0900

    fix: 모달 배경이 불투명하게 뜨는 문제 해결

--- src/containers/sign/EasyAuthentication.js
+++ src/containers/sign/EasyAuthentication.js
@@ -1,5 +1,5 @@
 import {
-  Box, Button, Grid, Modal, Stack, TextField,
+  Box, Button, Dialog, DialogContent, Grid, Stack, TextField,
 } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import LogoCircle from '../../components/common/LogoCircle';
@@ -10,102 +10,104 @@ function EasyAuthentication() {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(selectIsAuthenticated);
   return (
-    <Modal
+    <Dialog
       fullScreen
       open={!isAuthenticated}
     >
-      <CenterBox>
-        <Stack
-          justifyContent="center"
-          alignItems="center"
-          px={1}
-        >
-          <LogoCircle />
-
-          <Box my={2} sx={{ typography: 'h5', fontWeight: 'bold' }}>비밀번호 입력</Box>
-
-          <Box sx={{ fontWeight: 'bold', fontSize: '17px', color: 'primary.main' }}>설정하신 PIN 6자리를 입력해주세요.</Box>
-
-          <Box
-            component="form"
-          // onSubmit={handleSubmit}
-            noValidate
-            sx={{ maxWidth: '400px', mx: 2 }}
+      <DialogContent>
+        <CenterBox>
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            px={1}
           >
-            <Grid container spacing={1}>
-              <Grid item xs={2}>
-                <TextField
-                  margin="normal"
-                  required
-                  name="pwd1"
-                  type="pwd1"
-                  id="pwd1"
-                  autoComplete="current-password"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={2}>
-                <TextField
-                  margin="normal"
-                  hiddenLabel
-                  required
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-              <Grid item xs={2}>
-                <TextField
-                  margin="normal"
-                  hiddenLabel
-                  required
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-              <Grid item xs={2}>
-                <TextField
-                  margin="normal"
-                  hiddenLabel
-                  required
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-              <Grid item xs={2}>
-                <TextField
-                  margin="normal"
-                  hiddenLabel
-                  required
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-              <Grid item xs={2}>
-                <TextField
-                  margin="normal"
-                  hiddenLabel
-                  required
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-            </Grid>
+            <LogoCircle />
 
-            <Button fullWidth variant="contained" onClick={() => dispatch(setIsAuthenticatedTrue())}>인증</Button>
-          </Box>
-        </Stack>
-      </CenterBox>
-    </Modal>
+            <Box my={2} sx={{ typography: 'h5', fontWeight: 'bold' }}>비밀번호 입력</Box>
+
+            <Box sx={{ fontWeight: 'bold', fontSize: '17px', color: 'primary.main' }}>설정하신 PIN 6자리를 입력해주세요.</Box>
+
+            <Box
+              component="form"
+          // onSubmit={handleSubmit}
+              noValidate
+              sx={{ maxWidth: '400px', mx: 2 }}
+            >
+              <Grid container spacing={1}>
+                <Grid item xs={2}>
+                  <TextField
+                    margin="normal"
+                    required
+                    name="pwd1"
+                    type="pwd1"
+                    id="pwd1"
+                    autoComplete="current-password"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <TextField
+                    margin="normal"
+                    hiddenLabel
+                    required
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <TextField
+                    margin="normal"
+                    hiddenLabel
+                    required
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <TextField
+                    margin="normal"
+                    hiddenLabel
+                    required
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <TextField
+                    margin="normal"
+                    hiddenLabel
+                    required
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <TextField
+                    margin="normal"
+                    hiddenLabel
+                    required
+                    name="password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
+              </Grid>
+
+              <Button fullWidth variant="contained" onClick={() => dispatch(setIsAuthenticatedTrue())}>인증</Button>
+            </Box>
+          </Stack>
+        </CenterBox>
+      </DialogContent>
+    </Dialog>
   );
 }
 
