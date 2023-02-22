fa1c1297c3f52ef05f06e1b312aff277d9d58ca1
soyoung125 <sypark041754@gmail.com>
2023-02-22 22:21:28 +0900

    design: 간편 인증 페이지 디자인 중 - 타이들과 서브타이틀 추가

--- src/containers/sign/EasyAuthentication.js
+++ src/containers/sign/EasyAuthentication.js
@@ -1,10 +1,26 @@
-import { Button } from '@mui/material';
+import { Box, Button, Stack } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import LogoCircle from '../../components/common/LogoCircle';
+import CenterBox from '../../components/layouts/CenterBox';
 
 function EasyAuthentication() {
   const navigate = useNavigate();
   return (
-    <Button onClick={() => navigate(-1)}>인증</Button>
+    <CenterBox>
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        px={1}
+      >
+        <LogoCircle />
+
+        <Box my={2} sx={{ typography: 'h5', fontWeight: 'bold' }}>비밀번호 입력</Box>
+
+        <Box sx={{ fontWeight: 'bold', fontSize: '17px', color: 'primary.main' }}>설정하신 PIN 6자리를 입력해주세요.</Box>
+
+        <Button fullWidth variant="contained" onClick={() => navigate(-1)}>인증</Button>
+      </Stack>
+    </CenterBox>
   );
 }
 
