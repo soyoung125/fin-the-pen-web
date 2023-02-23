b765f45a73760687440b71c2d5c72144896c2331
soyoung125 <sypark041754@gmail.com>
2023-02-23 23:41:07 +0900

    refactor: 간편 인증 페이지 불필요한 스크롤 제거

--- src/containers/sign/EasyAuthentication.js
+++ src/containers/sign/EasyAuthentication.js
@@ -1,5 +1,5 @@
 import {
-  Box, Button, Dialog, DialogContent, Grid, Stack, TextField,
+  Box, Button, Dialog, Grid, Stack, TextField,
 } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import LogoCircle from '../../components/common/LogoCircle';
@@ -14,99 +14,97 @@ function EasyAuthentication() {
       fullScreen
       open={!isAuthenticated}
     >
-      <DialogContent>
-        <CenterBox>
-          <Stack
-            justifyContent="center"
-            alignItems="center"
-            px={1}
-          >
-            <LogoCircle />
+      <CenterBox>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          px={1}
+        >
+          <LogoCircle />
 
-            <Box my={2} sx={{ typography: 'h5', fontWeight: 'bold' }}>비밀번호 입력</Box>
+          <Box my={2} sx={{ typography: 'h5', fontWeight: 'bold' }}>비밀번호 입력</Box>
 
-            <Box sx={{ fontWeight: 'bold', fontSize: '17px', color: 'primary.main' }}>설정하신 PIN 6자리를 입력해주세요.</Box>
+          <Box sx={{ fontWeight: 'bold', fontSize: '17px', color: 'primary.main' }}>설정하신 PIN 6자리를 입력해주세요.</Box>
 
-            <Box
-              component="form"
+          <Box
+            component="form"
           // onSubmit={handleSubmit}
-              noValidate
-              sx={{ maxWidth: '400px', mx: 2 }}
-            >
-              <Grid container spacing={1}>
-                <Grid item xs={2}>
-                  <TextField
-                    margin="normal"
-                    required
-                    name="pwd1"
-                    type="pwd1"
-                    id="pwd1"
-                    autoComplete="current-password"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={2}>
-                  <TextField
-                    margin="normal"
-                    hiddenLabel
-                    required
-                    name="password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                </Grid>
-                <Grid item xs={2}>
-                  <TextField
-                    margin="normal"
-                    hiddenLabel
-                    required
-                    name="password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                </Grid>
-                <Grid item xs={2}>
-                  <TextField
-                    margin="normal"
-                    hiddenLabel
-                    required
-                    name="password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                </Grid>
-                <Grid item xs={2}>
-                  <TextField
-                    margin="normal"
-                    hiddenLabel
-                    required
-                    name="password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                </Grid>
-                <Grid item xs={2}>
-                  <TextField
-                    margin="normal"
-                    hiddenLabel
-                    required
-                    name="password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                </Grid>
+            noValidate
+            sx={{ maxWidth: '400px', mx: 2 }}
+          >
+            <Grid container spacing={1}>
+              <Grid item xs={2}>
+                <TextField
+                  margin="normal"
+                  required
+                  name="pwd1"
+                  type="pwd1"
+                  id="pwd1"
+                  autoComplete="current-password"
+                  autoFocus
+                />
               </Grid>
+              <Grid item xs={2}>
+                <TextField
+                  margin="normal"
+                  hiddenLabel
+                  required
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <TextField
+                  margin="normal"
+                  hiddenLabel
+                  required
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <TextField
+                  margin="normal"
+                  hiddenLabel
+                  required
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <TextField
+                  margin="normal"
+                  hiddenLabel
+                  required
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <TextField
+                  margin="normal"
+                  hiddenLabel
+                  required
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+            </Grid>
 
-              <Button fullWidth variant="contained" onClick={() => dispatch(setIsAuthenticatedTrue())}>인증</Button>
-            </Box>
-          </Stack>
-        </CenterBox>
-      </DialogContent>
+            <Button fullWidth variant="contained" onClick={() => dispatch(setIsAuthenticatedTrue())}>인증</Button>
+          </Box>
+        </Stack>
+      </CenterBox>
     </Dialog>
   );
 }
