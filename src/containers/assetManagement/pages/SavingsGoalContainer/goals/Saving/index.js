b50f545b16994ddcaba05873dc6e4ba1033fc74c
soyoung125 <sypark041754@gmail.com>
2023-03-04 23:02:41 +0900

    refactor: 저축 목표 금액 설정 모달 뜨기 전 알림창이 먼저 뜨도록 수정

--- src/containers/assetManagement/pages/SavingsGoalContainer/goals/Saving/index.js
+++ src/containers/assetManagement/pages/SavingsGoalContainer/goals/Saving/index.js
@@ -12,16 +12,24 @@ import ModalStaticBackdrop from '../../../../../../components/layouts/ModalStati
 import RoundedBorderBox from '../../../../../../components/common/RoundedBorderBox';
 import { selectSavingGoal, setSavingGoal } from '../../../../../../utils/redux/asset/assetSlice';
 import InputModal from './InputModal';
+import AlertModal from '../../../../../../components/common/AlertModal';
 
 function Saving() {
   const [savingGoalModalOpen, setSavingGoalModalOpen] = useState(false);
+  const [openAlertModal, setOpenAlertModal] = useState(false);
   const saving = useSelector(selectSavingGoal);
+
+  const openSavingGoalModal = () => {
+    setOpenAlertModal(false);
+    setSavingGoalModalOpen(true);
+  };
+
   return (
     <>
       <RoundedPaper sx={{ p: 2, mt: 1, mb: 3 }}>
         <Stack direction="row" justifyContent="space-between" alignItems="center">
           <Box sx={{ typography: 'h6', fontWeight: 'bold' }}>1 Year Goal</Box>
-          <IconButton color="primary" onClick={() => setSavingGoalModalOpen(true)}>
+          <IconButton color="primary" onClick={() => setOpenAlertModal(true)}>
             <BorderColorIcon fontSize="small" />
           </IconButton>
         </Stack>
@@ -36,7 +44,7 @@ function Saving() {
         </RoundedBorderBox>
         <Stack direction="row" justifyContent="space-between" alignItems="center">
           <Box sx={{ typography: 'h6', fontWeight: 'bold' }}>1 Month Goal</Box>
-          <IconButton color="primary" onClick={() => setSavingGoalModalOpen(true)}>
+          <IconButton color="primary" onClick={() => setOpenAlertModal(true)}>
             <BorderColorIcon fontSize="small" />
           </IconButton>
         </Stack>
@@ -58,6 +66,13 @@ function Saving() {
         component={(
           <InputModal setSavingGoalModalOpen={setSavingGoalModalOpen} />
         )}
+      />
+
+      <AlertModal
+        open={openAlertModal}
+        handleClose={() => setOpenAlertModal(false)}
+        handleClickYes={() => openSavingGoalModal()}
+        mode="modify"
       />
     </>
 
