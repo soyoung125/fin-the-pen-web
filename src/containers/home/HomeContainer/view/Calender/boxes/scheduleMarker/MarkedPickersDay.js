39b70501751407be2a4ae76762c90062aee48359
soyoung125 <sypark041754@gmail.com>
2023-03-30 22:23:01 +0900

    refactor: 정기입출금액 배경색 채우기

--- src/containers/home/HomeContainer/view/Calender/boxes/scheduleMarker/MarkedPickersDay.js
+++ src/containers/home/HomeContainer/view/Calender/boxes/scheduleMarker/MarkedPickersDay.js
@@ -1,13 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { PickersDay } from '@mui/x-date-pickers';
+import { useSelector } from 'react-redux';
+import { selectIsDarkMode } from '../../../../../../../utils/redux/setting/settingSlice';
 
 function MarkedPickersDay({ color, DayComponentProps }) {
+  const isDarkMode = useSelector(selectIsDarkMode);
   return (
     <PickersDay
       sx={{
         border: 1,
         borderRadius: 2,
         borderColor: color,
+        backgroundColor: isDarkMode && color,
       }}
       {...DayComponentProps}
     />
