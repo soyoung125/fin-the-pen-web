38b12b46c83940a0e0b148ca9a4d552940304f29
soyoung125 <sypark041754@gmail.com>
2023-04-10 22:54:45 +0900

    refactor: 엔터키로 다음 카테고리 자산 설정으로 넘어갈 수 있도록 수정

--- src/containers/assetManagement/pages/AssetsByCategory/CategoryList.js
+++ src/containers/assetManagement/pages/AssetsByCategory/CategoryList.js
@@ -10,11 +10,11 @@ function CategoryList({
   handleClick, open, modifyAsset,
 }) {
   const assets = useSelector(selectAssetsByCategory);
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const [asset, setAsset] = useState(0);
 
-  const modify = (type, title, preValue) => {
-    setSelectedCategory('');
+  const modify = (type, index, title, preValue) => {
+    setSelectedCategory(index);
     modifyAsset(type, title, preValue === '-' ? 0 : preValue, parseInt(asset, 10));
   };
 
@@ -33,26 +33,33 @@ function CategoryList({
           </ListItemButton>
           <Collapse in={open === category.type} timeout="auto" unmountOnExit>
             <List component="div" disablePadding>
-              {category.categories.map((c) => (
+              {category.categories.map((c, index) => (
                 <ListItem sx={{ py: 0 }} key={c.title}>
                   <Stack direction="row" justifyContent="space-between" sx={{ width: '100%', color: c.asset === '-' ? '#979797' : 'primary.main', fontSize: '14px' }}>
                     <Box sx={{ ml: 2 }}>
                       {c.title}
                     </Box>
-                    {selectedCategory === c.title
+                    {selectedCategory === index
                       ? (
                         <Box>
                           <InputBase
                             onChange={(e) => setAsset(e.target.value)}
+                            autoFocus
                             sx={{
                               border: '1px solid', borderRadius: 1, fontSize: '14px', height: '21px', width: '100px',
+                            }}
+                            onKeyDown={(ev) => {
+                              if (ev.key === 'Enter') {
+                                // setSelectedCategory(index + 1);
+                                modify(category.type, index + 1, c.title, c.asset);
+                              }
                             }}
                           />
                           <Button sx={{ fontSize: '14px', height: '21px' }} onClick={() => modify(category.type, c.title, c.asset)}>save</Button>
                         </Box>
                       )
                       : (
-                        <Box onClick={() => setSelectedCategory(c.title)}>
+                        <Box onClick={() => setSelectedCategory(index)}>
                           {c.asset === '-' ? `${c.asset}원` : `${c.asset.toLocaleString('kr-KO')}원`}
                         </Box>
                       )}
