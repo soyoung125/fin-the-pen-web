2b35b3e63328c87d90927b3b15ca9e40c407c00d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-03 23:39:48 +0900

    refactor: 불필요한 코드 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,17 @@
-/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
-
+import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import { store } from './store';
-import { createTheme, responsiveFontSizes } from '@mui/material';
+import reportWebVitals from './reportWebVitals';
 import CustomThemeProvider from './components/providers/CustomThemeProvider';
 import router from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-let persistor = persistStore(store);
+const persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
@@ -24,9 +19,6 @@ root.render(
       <PersistGate loading={null} persistor={persistor}>
         <CustomThemeProvider>
           <RouterProvider router={router} />
-          {/* <BrowserRouter basename='fin-the-pen-web'>
-            <App />
-          </BrowserRouter> */}
         </CustomThemeProvider>
       </PersistGate>
     </Provider>
