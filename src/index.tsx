d0fd6eb887c009d93912a638317c87d64451469e
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-07 02:18:34 +0900

    fix: 과도한 지연시간으로 인해 프로바이더 분리 원상 복구

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import { store } from './store';
 import reportWebVitals from './reportWebVitals';
+import CustomThemeProvider from './components/providers/CustomThemeProvider';
 import router from './router';
-import Providers from './providers/Providers';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const persistor = persistStore(store);
 
 root.render(
-  <StrictMode>
-    <Providers>
-      <RouterProvider router={router} />
-    </Providers>
-  </StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <CustomThemeProvider>
+          <RouterProvider router={router} />
+        </CustomThemeProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
