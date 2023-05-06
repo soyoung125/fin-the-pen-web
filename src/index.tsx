efdeed4ef46037c79b86605fa18ca2582a57e81d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-07 02:11:58 +0900

    refactor: 라우터를 제외한 각종 Providers 분리

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-import { store } from './store';
+import { StrictMode } from 'react';
 import reportWebVitals from './reportWebVitals';
-import CustomThemeProvider from './components/providers/CustomThemeProvider';
 import router from './router';
+import Providers from './providers/Providers';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const persistor = persistStore(store);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <CustomThemeProvider>
-          <RouterProvider router={router} />
-        </CustomThemeProvider>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
+  <StrictMode>
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>
+  </StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
