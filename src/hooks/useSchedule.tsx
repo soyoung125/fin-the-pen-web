3b0bffc458d94412f5e9bddfe8a7364784e5549d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-07 12:18:05 +0900

    feat: 스케쥴 관련 커스텀 훅 추가

---
+++ src/hooks/useSchedule.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useDispatch } from 'react-redux';
+
+const useSchedule = () => {
+  const dispatch = useDispatch();
+  return {
+
+  };
+};
+
+export default useSchedule;
