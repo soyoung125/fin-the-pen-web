ed2b74ff5866ea315f43b33ce86fadf06cfa7f1c
soyoung125 <sypark041754@gmail.com>
2023-05-18 23:11:59 +0900

    refactor: 누락된 설정 카드 추가

--- src/containers/assetManagement/pages/SavingsGoalContainer/DetailSetting/index.tsx
+++ src/containers/assetManagement/pages/SavingsGoalContainer/DetailSetting/index.tsx
@@ -4,6 +4,7 @@ import {
 import PrioritySetting from './PrioritySetting';
 import RemittanceSetting from './RemittanceSetting';
 import PopupSetting from './PopupSetting';
+import NotificationSetting from './NotificationSetting';
 
 function DetailSetting() {
   return (
@@ -14,7 +15,7 @@ function DetailSetting() {
 
       <RemittanceSetting />
 
-      <RemittanceSetting />
+      <NotificationSetting />
 
       <PopupSetting />
     </Box>
