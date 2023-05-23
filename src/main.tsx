9ac430bda9f189964a97328ca9c2131da8dba8bd
soyoung125 <sypark041754@gmail.com>
2023-05-23 23:04:58 +0900

    chore: cra -> vite 수정

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import { store } from './app/redux/store';
-import reportWebVitals from './reportWebVitals';
 import CustomThemeProvider from './components/providers/CustomThemeProvider';
 import router from './app/router';
 
@@ -24,8 +23,3 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
