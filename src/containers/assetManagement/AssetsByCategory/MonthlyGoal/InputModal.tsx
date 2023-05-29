009be41ba7f0e7b659051a4cd9fac206808f24d1
soyoung125 <sypark041754@gmail.com>
2023-05-29 23:45:29 +0900

    refactor: 지출 목표 금액 설정 초기화 확인 모달 추가

--- src/containers/assetManagement/AssetsByCategory/MonthlyGoal/InputModal.tsx
+++ src/containers/assetManagement/AssetsByCategory/MonthlyGoal/InputModal.tsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import moment from 'moment';
 import { selectMonthlyConsumptionGoal, setMonthlyConsumptionGoal } from '../../../../app/redux/slices/assetSlice';
+import AlertModal from '../../../../components/common/AlertModal';
 
 interface InputModalProps {
   setMonthlyGoalModalOpen: React.Dispatch<React.SetStateAction<boolean>>
@@ -15,6 +16,7 @@ interface InputModalProps {
 function InputModal({
   setMonthlyGoalModalOpen,
 }: InputModalProps) {
+  const [openAlertModal, setOpenAlertModal] = useState(false);
   const month = moment().format('M월');
   const [goal, setGoal] = useState(0);
 
@@ -38,45 +40,55 @@ function InputModal({
   };
 
   return (
-    <Stack p={2} spacing={1}>
-      <Stack direction="row" alignItems="center" justifyContent="space-between">
-        <IconButton
-          onClick={() => setGoal(0)}
-          color="error"
-        >
-          <DeleteForeverIcon />
-        </IconButton>
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>{`${month} 지출 목표 설정`}</Typography>
-        <IconButton onClick={() => setMonthlyGoalModalOpen(false)}>
-          <ClearIcon />
-        </IconButton>
-      </Stack>
-      <Box my={1}>
-        <Divider />
-      </Box>
-      <Stack spacing={1}>
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>{`${month} 지출 목표 금액`}</Typography>
-        <TextField
+    <>
+      <Stack p={2} spacing={1}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <IconButton
+            onClick={() => setOpenAlertModal(true)}
+            color="error"
+          >
+            <DeleteForeverIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>{`${month} 지출 목표 설정`}</Typography>
+          <IconButton onClick={() => setMonthlyGoalModalOpen(false)}>
+            <ClearIcon />
+          </IconButton>
+        </Stack>
+        <Box my={1}>
+          <Divider />
+        </Box>
+        <Stack spacing={1}>
+          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>{`${month} 지출 목표 금액`}</Typography>
+          <TextField
+            fullWidth
+            placeholder="이달의 소비 목표액을 입력하세요"
+            type="text"
+            value={goal.toLocaleString('ko-KR')}
+            onFocus={(e) => e.target.select()}
+            onChange={handleChange}
+            id="monthlyGoal" />
+        </Stack>
+        <Button
           fullWidth
-          placeholder="이달의 소비 목표액을 입력하세요"
-          type="text"
-          value={goal.toLocaleString('ko-KR')}
-          onFocus={(e) => e.target.select()}
-          onChange={handleChange}
-          id="monthlyGoal"
-        />
+          variant="contained"
+          onClick={() => {
+            dispatch(setMonthlyConsumptionGoal(goal));
+            setMonthlyGoalModalOpen(false);
+          } }
+        >
+          한해 저축 목표 설정하기
+        </Button>
       </Stack>
-      <Button
-        fullWidth
-        variant="contained"
-        onClick={() => {
-          dispatch(setMonthlyConsumptionGoal(goal));
-          setMonthlyGoalModalOpen(false);
-        }}
-      >
-        한해 저축 목표 설정하기
-      </Button>
-    </Stack>
+      <AlertModal
+        open={openAlertModal}
+        handleClose={() => setOpenAlertModal(false)}
+        handleClickYes={() => {
+          setOpenAlertModal(false);
+          setGoal(0)
+        } }
+        mode="reset"
+      />
+    </>
   );
 }
 
