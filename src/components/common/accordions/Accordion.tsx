cf7aeec61db9cdd765366ad90babe50449940a70
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-13 22:26:20 +0900

    fix: Accordion type 정의

--- src/components/common/accordions/Accordion.tsx
+++ src/components/common/accordions/Accordion.tsx
@@ -1,15 +1,22 @@
-import styled from '@emotion/styled';
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import { Theme } from "@mui/system";
 
-const Accordion = styled((props: AccordionProps) => (
+import styled from "@emotion/styled";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+
+interface StyledAccordionProps extends AccordionProps {
+  theme: Theme;
+}
+
+const Accordion = styled((props: StyledAccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }: any) => ({
+))(({ theme }) => ({
   border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
+  "&:not(:last-child)": {
     borderBottom: 0,
   },
-  '&:before': {
-    display: 'none',
+  "&:before": {
+    display: "none",
   },
 }));
+
 export default Accordion;
