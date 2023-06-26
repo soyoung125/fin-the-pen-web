0b473cd30ce97ea36c46cf7ef92751f1e97ce287
soyoung125 <sypark041754@gmail.com>
2023-06-26 20:25:12 +0900

    feat: 팝업 버튼 위아래 드래그 구현

--- src/containers/home/common/TopBar/buttons/PopupButton.tsx
+++ src/containers/home/common/TopBar/buttons/PopupButton.tsx
@@ -1,21 +1,65 @@
 import { Fab } from "@mui/material";
 import SavingsOutlinedIcon from '@mui/icons-material/SavingsOutlined';
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function PopupButton() {
-  const [height, setHeight] = useState(80);
-  
+  const position = 120;
+  const [offset, setOffset] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const fabRef = useRef<HTMLDivElement>(null);
+  const startPositionRef = useRef<number>(120);
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    startPositionRef.current = event.clientY - offset;
+    setIsDragging(true);
+  };
+
+  const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    startPositionRef.current = event.touches[0].clientY - offset;
+    setIsDragging(true);
+  };
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDragging) return;
+
+    const offsetY = event.clientY - startPositionRef.current;
+
+    setOffset(offsetY);
+  };
+
+  const handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (!isDragging) return;
+
+    const offsetY = event.touches[0].clientY - startPositionRef.current;
+
+    setOffset(offsetY);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+
   return (
+    <div
+      ref={fabRef}
+      style={{ position: 'fixed', top: `calc(100vh - ${position - offset}px)`, right: 10 }}
+      onMouseDown={handleMouseDown}
+      onTouchStart={handleTouchStart}
+      onMouseUp={handleMouseUp}
+      onTouchEnd={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      onTouchMove={handleTouchMove}
+    >
       <Fab
         color="secondary"
         size="small"
         aria-label="popup"
-        sx={{ position: 'fixed', bottom: height, right: 10 }}
-        //onMouseDown={(e) => console.log(e)}
-        onMouseMove={(e) => setHeight(height + e.movementY)}
       >
         <SavingsOutlinedIcon />
       </Fab>
+    </div>
   );
 }
 
