16ff62fcaf2c2e13555146c2abb1599323f3e24a
soyoung125 <sypark041754@gmail.com>
2023-07-23 23:54:20 +0900

    feat: popover 컴포넌트 추가

--- src/containers/home/common/TopBar/buttons/SearchButton.tsx
+++ src/containers/home/common/TopBar/buttons/SearchButton.tsx
@@ -1,12 +1,47 @@
+import { Box, Popover, Typography } from "@mui/material";
 import RoundedButton from "../../../../../components/common/RoundedButton";
 import SearchIcon from '@mui/icons-material/Search';
+import { useRef, useState } from "react";
 
 function SearchButton() {
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+    const searchBtn = useRef(null);
+
+    const handleClick = () => {
+        setAnchorEl(searchBtn.current);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
     return (
         <>
-        <RoundedButton value="user" onClick={() => alert('준비 중인 메뉴')}>
-          <SearchIcon />
-        </RoundedButton>
+            <Box ref={searchBtn}>
+                <RoundedButton value="user" onClick={handleClick}>
+                    <SearchIcon />
+                </RoundedButton>
+            </Box>
+
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+            </Popover>
         </>
     );
 }
