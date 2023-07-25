45eded96023e1ba5f6fc9a93dce71b7c32e8774c
soyoung125 <sypark041754@gmail.com>
2023-07-25 16:33:41 +0900

    reat: popover 일정 검색 컴포넌트 구현

--- src/containers/home/common/TopBar/buttons/SearchButton.tsx
+++ src/containers/home/common/TopBar/buttons/SearchButton.tsx
@@ -1,12 +1,13 @@
-import { Box, Popover, Typography, Stack, ButtonBase, List, ListItem, IconButton, ListItemText, ListItemButton } from "@mui/material";
+import { Box, Popover, Typography, Stack, ButtonBase, List, ListItem, IconButton, ListItemText, ListItemButton, FormControl, OutlinedInput, InputAdornment } from "@mui/material";
 import RoundedButton from "../../../../../components/common/RoundedButton";
 import SearchIcon from '@mui/icons-material/Search';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function SearchButton() {
     const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+    const [isSearchMode, setIsSearchMode] = useState<Boolean>(false);
     const searchBtn = useRef(null);
 
     const handleClick = () => {
@@ -15,6 +16,7 @@ function SearchButton() {
 
     const handleClose = () => {
         setAnchorEl(null);
+        setTimeout(() => setIsSearchMode(false), 300);
     };
 
     const open = Boolean(anchorEl);
@@ -42,39 +44,57 @@ function SearchButton() {
                     horizontal: 'left',
                 }}
             >
-                <List dense>
-                    <ListItem
-                        secondaryAction={
-                            <IconButton edge="end">
-                                <KeyboardArrowRightIcon />
-                            </IconButton>
-                        }
-                        disablePadding
-                        onClick={() => alert('일정 검색')}
-                    >
-                        <ListItemButton>
-                            <ListItemText
-                                primary="My 일정 검색하기"
+                {isSearchMode ?
+                    <Box px={1.5} py={1}>
+                        <Typography variant="button">My 일정 검색하기</Typography>
+                        <FormControl fullWidth>
+                            <OutlinedInput
+                                id="time"
+                                type="text"
+                                autoFocus
+                                endAdornment={<InputAdornment position="end"><SearchIcon color="primary" /></InputAdornment>}
+                                // onChange={}
+                                size="small"
+                                inputProps={{
+                                    style: { textAlign: 'right' },
+                                }}
                             />
-                        </ListItemButton>
-                    </ListItem>
+                        </FormControl>
+                    </Box>
+                    : <List dense>
+                        <ListItem
+                            secondaryAction={
+                                <IconButton edge="end">
+                                    <KeyboardArrowRightIcon />
+                                </IconButton>
+                            }
+                            disablePadding
+                            onClick={() => setIsSearchMode(true)}
+                        >
+                            <ListItemButton>
+                                <ListItemText
+                                    primary="My 일정 검색하기"
+                                />
+                            </ListItemButton>
+                        </ListItem>
 
-                    <ListItem
-                        secondaryAction={
-                            <IconButton edge="end">
-                                <KeyboardArrowRightIcon />
-                            </IconButton>
-                        }
-                        disablePadding
-                        onClick={() => alert('결제 내역 불러오기')}
-                    >
-                        <ListItemButton>
-                            <ListItemText
-                                primary="My 결제 내역 불러오기"
-                            />
-                        </ListItemButton>
-                    </ListItem>
-                </List>
+                        <ListItem
+                            secondaryAction={
+                                <IconButton edge="end">
+                                    <KeyboardArrowRightIcon />
+                                </IconButton>
+                            }
+                            disablePadding
+                            onClick={() => alert('결제 내역 불러오기')}
+                        >
+                            <ListItemButton>
+                                <ListItemText
+                                    primary="My 결제 내역 불러오기"
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                }
             </Popover>
         </>
     );
