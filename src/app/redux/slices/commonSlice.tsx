a27889db463b300f2d01355c13ee4d03661bf419
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-16 02:12:42 +0900

    refactor: 기존 redux 코드를 headerRepository으로 이전

--- src/app/redux/slices/commonSlice.tsx
+++ src/app/redux/slices/commonSlice.tsx
@@ -1,17 +1,11 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { HEADER_MODE } from "../../../domain/constants/common";
-import { HeaderModeValue } from "../../../types/common";
-import { RootState } from "../store";
+import {createSlice} from "@reduxjs/toolkit";
+import {RootState} from "../store";
 
 interface CommonState {
-  headerOpen: boolean;
-  headerMode: HeaderModeValue;
   guestMode: boolean;
 }
 
 const initialState: CommonState = {
-  headerOpen: true,
-  headerMode: HEADER_MODE.home,
   guestMode: false,
 };
 
@@ -19,19 +13,6 @@ export const commonSlice = createSlice({
   name: "common",
   initialState,
   reducers: {
-    setHeaderOpenTrue: (
-      state,
-      action: PayloadAction<HeaderModeValue | undefined>
-    ) => {
-      const mode = action.payload;
-      if (mode !== undefined) {
-        state.headerMode = mode;
-      }
-      state.headerOpen = true;
-    },
-    setHeaderOpenFalse: (state) => {
-      state.headerOpen = false;
-    },
     setGuestModeTrue: (state) => {
       state.guestMode = true;
     },
@@ -42,15 +23,9 @@ export const commonSlice = createSlice({
   },
 });
 export const {
-  setHeaderOpenTrue,
-  setHeaderOpenFalse,
   setGuestModeTrue,
   setGuestModeFalse,
 } = commonSlice.actions;
-
-export const selectHeaderOpen = (state: RootState) => state.common.headerOpen;
-export const selectHeaderMode = (state: RootState) => state.common.headerMode;
 export const selectGuestMode = (state: RootState) => state.common.guestMode;
-
 
 export default commonSlice.reducer;
--- src/hooks/useHeader.tsx
+++ src/hooks/useHeader.tsx
@@ -1,29 +1,28 @@
 /* eslint-disable consistent-return */
-import { useEffect } from "react";
-import {
-  setHeaderOpenFalse,
-  setHeaderOpenTrue,
-} from "@redux/slices/commonSlice.tsx";
-import { useAppDispatch } from "@redux/hooks.ts";
-import { HeaderModeValue } from "@type/common.tsx";
+import {useEffect} from "react";
+import {useRecoilValue} from "recoil";
+import {HeaderModeType, headerRepository} from "@recoil/header.ts";
 
 /**
  * 헤더를 관리해주는 customHook.
- * 어차피 redux에게 상태 관리 맡기고 있어 상태값을 따로 반환은 해주지 않는다.
+ * recoil에 의존한다.
  * @param {boolean} open 헤더 오픈 여부
- * @param {HeaderModeValue} mode 헤더의 모드 변경
+ * @param {HeaderModeType} mode 헤더의 모드 변경
  */
-const useHeader = (open: boolean, mode?: HeaderModeValue) => {
-  const dispatch = useAppDispatch();
+const useHeader = (open: boolean, mode?: HeaderModeType) => {
+  const {closeHeader, openHeader} = useRecoilValue(headerRepository)
+
   useEffect(() => {
     if (open) {
-      dispatch(setHeaderOpenTrue(mode));
+      openHeader(mode);
     } else {
-      dispatch(setHeaderOpenFalse()); // 페이지 진입 시 헤더 감추기
+      closeHeader(); // 페이지 진입 시 헤더 감추기
       return () => {
-        dispatch(setHeaderOpenTrue(undefined)); // 페이지 탈출 시 헤더 복구
+        openHeader(undefined); // 페이지 탈출 시 헤더 복구
       };
     }
   }, []);
+
 };
+
 export default useHeader;
