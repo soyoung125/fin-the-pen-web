97f06e1e3d31428184a226850ece46405ef1e19c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-25 02:17:29 +0900

    refactor: 로그인 시 랜덤 토큰이 발급되도록 개선

--- src/app/tanstack-query/useAuth.ts
+++ src/app/tanstack-query/useAuth.ts
@@ -5,12 +5,22 @@ import { useSetRecoilState } from "recoil";
 import { userState } from "@recoil/user.ts";
 import { useNavigate } from "react-router-dom";
 import PATH from "../../constants/path.tsx";
+import { setSessionStorage } from "@utils/storage.ts";
+import { SESSION_STORAGE_KEY_TOKEN } from "@api/keys.ts";
 
 const fetchSignIn = async (credentials: SignIn) => {
   return fetch(`${DOMAIN}/fin-the-pen-web/sign-in`, {
     method: "POST",
     body: JSON.stringify(credentials),
   });
+};
+
+// TODO: 추후에 토큰 방식으로 수정 예정입니다. 지금은 기존 구조로 진행합니다.
+const generateRandomToken = () => {
+  const randomEightDigit = Math.floor(
+    10000000 + Math.random() * 90000000
+  ).toString();
+  setSessionStorage(SESSION_STORAGE_KEY_TOKEN, randomEightDigit);
 };
 
 export const useAuth = () => {
@@ -24,6 +34,7 @@ export const useAuth = () => {
       const user: User | "" = await data.json();
       if (user !== "") {
         setUser(user);
+        generateRandomToken();
         navigate(PATH.home);
       } else {
         alert("로그인에 실패했습니다.");
@@ -41,6 +52,7 @@ export const useAuth = () => {
 
   const mockSignIn = () => {
     alert("게스트 모드로 로그인 합니다.");
+    generateRandomToken();
     setUser({
       id: 0,
       user_id: "guest@finthepen.com",
