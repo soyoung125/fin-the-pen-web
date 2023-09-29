35bbb02f387cb89b0bfc42a06d5f1324a06ed5ab
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-29 18:00:11 +0900

    refactor: 미사용 코드 제거

--- src/temp/api.ts
+++
@@ -1,9 +0,0 @@
-export const fetchCreateTodo = async (text: string) =>
-  fetch("/temp/todo/create", {
-    method: "POST",
-    body: JSON.stringify({
-      text: text,
-    }),
-  });
-
-export const fetchTodo = () => fetch("/temp/todo").then((res) => res.json());
--- src/temp/useTodo.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchTodo } from "./api.ts";
-
-export const useTodo = () => {
-  return useQuery({
-    queryKey: ["todo"],
-    queryFn: fetchTodo,
-  });
-};
