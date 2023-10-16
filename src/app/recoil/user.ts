9bea5718d9070ba262ea2a660edc7d6609732ad0
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-10-17 01:49:37 +0900

    refactor: recoil에서 새로고침 하더라도 세션을 유지하는 기능 구현

--- src/app/recoil/user.ts
+++ src/app/recoil/user.ts
@@ -1,6 +1,7 @@
 import { atom } from "recoil";
 
 import { User } from "@type/auth.tsx";
+import { sessionStorageEffect } from "@utils/storageEffect.ts";
 
 /**
  *  회원 정보
@@ -8,4 +9,5 @@ import { User } from "@type/auth.tsx";
 export const userState = atom<User | undefined>({
   key: "userState",
   default: undefined,
+  effects: [sessionStorageEffect<User | undefined>("userState")],
 });
