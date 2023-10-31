75ac002dc388edffb00cc216fd7a4212410944ed
soyoung125 <sypark041754@gmail.com>
2023-10-31 20:39:10 +0900

    refactor: 로그인 헤더 문구 수정, 문자 중앙 정렬

--- src/pages/SignIn/Header.tsx
+++ src/pages/SignIn/Header.tsx
@@ -3,14 +3,19 @@ import LogoCircle from "@components/common/LogoCircle.tsx";
 
 function Header() {
   return (
-    <>
+    <Stack py={3} alignItems="center" spacing={2}>
       <LogoCircle />
       <Stack my={2}>
-        <Typography component="h1" variant="h5">
-          핀더펜과 함께 자산설계를 시작하세요!
+        <Typography
+          fontSize={22}
+          fontWeight={700}
+          textAlign="center"
+          whiteSpace="pre-line"
+        >
+          {`핀더펜과 함께\n자산설계를 시작하세요!`}
         </Typography>
       </Stack>
-    </>
+    </Stack>
   );
 }
 export default Header;
