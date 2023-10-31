0c4f6025d05f2206d04b61df08d641406190b9a4
soyoung125 <sypark041754@gmail.com>
2023-10-31 17:27:56 +0900

    feat 로그인/회원가입 페이지의 로고 변경

--- src/components/common/LogoCircle.tsx
+++ src/components/common/LogoCircle.tsx
@@ -1,11 +1,7 @@
-import { Avatar } from '@mui/material';
-import logo from '../../assets/logos/logo_white.png';
+import { Avatar } from "@mui/material";
+import logo from "../../assets/logos/logo_main.svg";
 
 function LogoCircle() {
-  return (
-    <Avatar sx={{ bgcolor: 'primary.main', width: 100, height: 100 }}>
-      <img src={logo} alt="" width="50px" height="50px" />
-    </Avatar>
-  );
+  return <img src={logo} alt="" width="64px" height="64px" />;
 }
 export default LogoCircle;
