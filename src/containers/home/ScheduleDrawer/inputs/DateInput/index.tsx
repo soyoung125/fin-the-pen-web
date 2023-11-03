3131212d8c81fac236ee638c729ea17be14761e8
soyoung125 <sypark041754@gmail.com>
2023-11-03 21:33:53 +0900

    feat: 시작 날짜 선택 달력 추가

--- src/containers/home/ScheduleDrawer/inputs/DateInput/index.tsx
+++ src/containers/home/ScheduleDrawer/inputs/DateInput/index.tsx
@@ -1,49 +1,86 @@
 import { Box, Input, InputAdornment, InputBase, Stack } from "@mui/material";
+import Collapse from "@mui/material/Collapse";
 import { useSelector } from "react-redux";
 import { SCHEDULE_DRAWER } from "../../../../../constants/schedule";
 import { selectSchedule } from "../../../../../app/redux/slices/scheduleSlice";
 import { updateSchedule } from "../../domain/schedule";
 import { useAppDispatch } from "../../../../../app/redux/hooks";
 import SwitchButton from "@components/common/SwitchButton";
+import { useState } from "react";
+import CalenderBox from "@containers/home/HomeContainer/view/Calender/boxes/CalenderBox";
+import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
+import moment from "moment";
+import { UpdateStateInterface } from "@type/common";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 
 function DateInput() {
   const dispatch = useAppDispatch();
   const schedule = useSelector(selectSchedule);
+  const [showStart, setShowStart] = useState(false);
+  const [showEnd, setShowEnd] = useState(false);
 
   const changeSchedule = (
-    state: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+    state:
+      | React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+      | UpdateStateInterface,
   ) => {
     updateSchedule(dispatch, schedule, state);
   };
 
   return (
     <>
-      <Input
-        type="date"
-        id="date"
-        startAdornment={
-          <InputAdornment position="start">
-            <Box sx={{ color: "primary.main", fontWeight: 500 }}>
-              {SCHEDULE_DRAWER.start_time}
-            </Box>
-          </InputAdornment>
-        }
-        endAdornment={
-          <InputAdornment position="start">
-            <InputBase
-              id="start_time"
-              type="time"
-              value={schedule?.start_time}
-              onChange={changeSchedule}
-              inputProps={{
-                style: { textAlign: "right" },
-              }}
-            />
-          </InputAdornment>
-        }
-        value={schedule?.date}
-        onChange={changeSchedule}
-      />
+      <Box>
+        <Input
+          // type="date"
+          onClick={() => setShowStart(!showStart)}
+          id="date"
+          startAdornment={
+            <InputAdornment position="start">
+              <Box sx={{ color: "primary.main", fontWeight: 500 }}>
+                {SCHEDULE_DRAWER.start_time}
+              </Box>
+            </InputAdornment>
+          }
+          endAdornment={
+            <InputAdornment position="start">
+              <InputBase
+                id="start_time"
+                type="time"
+                value={schedule?.start_time}
+                onChange={changeSchedule}
+                onClick={(e) => e.stopPropagation()}
+                inputProps={{
+                  style: { textAlign: "right" },
+                }}
+              />
+            </InputAdornment>
+          }
+          value={schedule?.date}
+          onChange={changeSchedule}
+        />
+        <Collapse in={showStart}>
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <CalenderBox dateHeight={50} dateSize={32} week={6}>
+              <DateCalendar
+                views={["year", "month", "day"]}
+                disableHighlightToday
+                dayOfWeekFormatter={(day) => day.substring(0, 3)}
+                value={moment(schedule?.date)}
+                onChange={(newValue) => {
+                  newValue &&
+                    changeSchedule({
+                      target: {
+                        id: "date",
+                        value: newValue.format("YYYY-MM-DD"),
+                      },
+                    });
+                }}
+                reduceAnimations
+              />
+            </CalenderBox>
+          </LocalizationProvider>
+        </Collapse>
+      </Box>
 
       <Input
         type="date"
