d96fe9871770d3fd4ffd64c33d7562b2ee0989ba
soyoung125 <sypark041754@gmail.com>
2023-11-14 17:58:25 +0900

    feat: 반복 단위 선택

--- src/containers/home/ScheduleDrawer/inputs/RepeatInput.tsx
+++ src/containers/home/ScheduleDrawer/inputs/RepeatInput.tsx
@@ -1,14 +1,20 @@
 import {
   // eslint-disable-next-line max-len
   Box,
+  Collapse,
   Stack,
+  RadioGroup,
+  Radio,
 } from "@mui/material";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectSchedule } from "../../../../app/redux/slices/scheduleSlice";
 import { updateRepeat } from "../domain/schedule";
 import { useAppDispatch } from "../../../../app/redux/hooks";
 import SwitchButton from "@components/common/SwitchButton";
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
+import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
 
 function RepeatInput() {
   const dispatch = useAppDispatch();
@@ -17,6 +23,7 @@ function RepeatInput() {
   const [openDatePickerModal, setOpenDatePickerModal] = useState(false);
 
   const changeRepeat = (state: { target: { value: string; name: string } }) => {
+    console.log(state);
     updateRepeat(dispatch, schedule, setOpenDatePickerModal, state);
   };
 
@@ -36,6 +43,50 @@ function RepeatInput() {
           }
         />
       </Stack>
+      <Collapse in={schedule?.repeat !== "None"}>
+        <RadioGroup
+          value={schedule?.repeat}
+          onChange={(e) =>
+            changeRepeat({
+              target: {
+                value: e.target.value,
+                name: "repeat",
+              },
+            })
+          }
+        >
+          <FormControlLabel
+            value="AllDay"
+            control={
+              <Radio
+                icon={<CheckCircleOutlineRoundedIcon />}
+                checkedIcon={<CheckCircleRoundedIcon />}
+              />
+            }
+            label="매일"
+          />
+          <FormControlLabel
+            value="Week"
+            control={
+              <Radio
+                icon={<CheckCircleOutlineRoundedIcon />}
+                checkedIcon={<CheckCircleRoundedIcon />}
+              />
+            }
+            label="매주"
+          />
+          <FormControlLabel
+            value="Month"
+            control={
+              <Radio
+                icon={<CheckCircleOutlineRoundedIcon />}
+                checkedIcon={<CheckCircleRoundedIcon />}
+              />
+            }
+            label="매달"
+          />
+        </RadioGroup>
+      </Collapse>
     </>
   );
 }
