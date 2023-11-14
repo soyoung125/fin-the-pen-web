b25be8da5f9041301ad2b1ac46ad56f95dd016df
soyoung125 <sypark041754@gmail.com>
2023-11-14 20:45:18 +0900

    feat: 반복 기간 설정

--- src/containers/home/ScheduleDrawer/inputs/RepeatInput.tsx
+++ src/containers/home/ScheduleDrawer/inputs/RepeatInput.tsx
@@ -36,7 +36,7 @@ function RepeatInput() {
           handleChange={() =>
             changeRepeat({
               target: {
-                value: schedule?.repeat === "None" ? "" : "None",
+                value: schedule?.repeat === "None" ? "AllDay" : "None",
                 name: "repeat",
               },
             })
@@ -84,6 +84,50 @@ function RepeatInput() {
               />
             }
             label="매달"
+          />
+        </RadioGroup>
+
+        <Box sx={{ color: "primary.main" }}>기간</Box>
+        <RadioGroup
+          value={schedule?.period}
+          onChange={(e) =>
+            changeRepeat({
+              target: {
+                value: e.target.value,
+                name: "period",
+              },
+            })
+          }
+        >
+          <FormControlLabel
+            value="All"
+            control={
+              <Radio
+                icon={<CheckCircleOutlineRoundedIcon />}
+                checkedIcon={<CheckCircleRoundedIcon />}
+              />
+            }
+            label="계속 반복"
+          />
+          <FormControlLabel
+            value="numberOf"
+            control={
+              <Radio
+                icon={<CheckCircleOutlineRoundedIcon />}
+                checkedIcon={<CheckCircleRoundedIcon />}
+              />
+            }
+            label="일정 반복 횟수"
+          />
+          <FormControlLabel
+            value="end"
+            control={
+              <Radio
+                icon={<CheckCircleOutlineRoundedIcon />}
+                checkedIcon={<CheckCircleRoundedIcon />}
+              />
+            }
+            label="종료 날짜"
           />
         </RadioGroup>
       </Collapse>
