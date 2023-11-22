58f58fa31f52e6c022e0d36bb6f13f80916440e5
soyoung125 <sypark041754@gmail.com>
2023-11-22 15:43:31 +0900

    refactor: 디자인 테마 수정

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -1,53 +1,126 @@
 /* eslint-disable */
-import { ThemeOptions } from '@mui/material/styles';
+import { ThemeOptions } from "@mui/material/styles";
 
 export const lightThemeOptions: ThemeOptions = {
   palette: {
-    mode: 'light',
+    mode: "light",
     primary: {
-      main: '#735bf2',
-      light: 'rgba(143,123,244,0.57)',
+      main: "#735BF2",
     },
     secondary: {
-      main: '#bfbaf3',
+      main: "#C8CBD0",
+      light: "#C8CBD0",
+      dark: "#C8CBD0",
+      contrastText: "#FFFFFF",
     },
     error: {
-      main: '#ffea00',
+      main: "#E82A2A",
     },
     warning: {
-      main: '#ff5252',
+      main: "#FFBE16",
     },
     success: {
-      main: '#b9f6ca',
+      main: "#0E9B35",
     },
+    info: {
+      main: "#0075FF",
+    },
+    divider: "#F7F7F8",
     text: {
-      primary: '#000000',
+      primary: "#131416",
+      secondary: "#A9ACB2",
     },
   },
+  // props: {
+  //   MuiTooltip: {
+  //     arrow: true,
+  //   },
+  // },
   typography: {
-    fontFamily: 'Noto Sans KR',
+    fontFamily: "Noto Sans KR",
+    fontSize: 14,
+    button: {
+      fontSize: 16,
+      fontWeight: 500,
+    },
+    h6: {
+      fontSize: 15,
+      fontWeight: 700,
+    },
+    h5: {
+      fontSize: 16,
+      fontWeight: 400,
+    },
+    h4: {
+      fontSize: 16,
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: 17,
+      fontWeight: 500,
+    },
+    h2: {
+      fontSize: 18,
+      fontWeight: 500,
+    },
+    h1: {
+      fontSize: 20,
+      fontWeight: 700,
+    },
+    caption: {
+      fontSize: 14,
+    },
+    subtitle1: {
+      fontSize: 15,
+      fontWeight: 500,
+    },
+    subtitle2: {
+      fontSize: 12,
+      fontWeight: 400,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 4,
+        },
+        outlined: {
+          border: "1.4px solid",
+        },
+        sizeSmall: {
+          paddingY: 7,
+        },
+        sizeMedium: {
+          paddingY: 8,
+        },
+        sizeLarge: {
+          paddingY: 10,
+        },
+      },
+    },
   },
 };
 
 export const darkThemeOptions: ThemeOptions = {
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#44b9d6',
+      main: "#44b9d6",
     },
     secondary: {
-      main: '#7497a4',
+      main: "#7497a4",
     },
     background: {
-      default: '#2a2e33',
-      paper: '#12293b',
+      default: "#2a2e33",
+      paper: "#12293b",
     },
     text: {
-      primary: '#eaeded',
-      secondary: '#44b9d6',
+      primary: "#eaeded",
+      secondary: "#44b9d6",
     },
   },
   typography: {
-    fontFamily: 'Noto Sans KR',
+    fontFamily: "Noto Sans KR",
   },
 };
