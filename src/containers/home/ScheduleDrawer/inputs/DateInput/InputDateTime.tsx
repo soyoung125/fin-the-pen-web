ef76b3411d8796086af919e64ee243ed28d2bbab
soyoung125 <sypark041754@gmail.com>
2023-11-30 20:50:29 +0900

    fix: 시작/종료일 빈칸입력시 생기는 오류 해결

--- src/containers/home/ScheduleDrawer/inputs/DateInput/InputDateTime.tsx
+++ src/containers/home/ScheduleDrawer/inputs/DateInput/InputDateTime.tsx
@@ -1,6 +1,6 @@
 import { useAppDispatch } from "@app/redux/hooks";
 import { selectSchedule } from "@app/redux/slices/scheduleSlice";
-import { Box, Collapse, InputAdornment, TextField } from "@mui/material";
+import { Box, Collapse, InputAdornment } from "@mui/material";
 import { UpdateStateInterface } from "@type/common";
 import { SCHEDULE_DRAWER } from "constants/schedule";
 import { useSelector } from "react-redux";
@@ -9,6 +9,8 @@ import SelectTime from "./select/SelectTime";
 import SelectDate from "./select/SelectDate";
 import moment from "moment";
 import "moment/dist/locale/ko";
+import { DateField, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 
 interface InputDateTimeProps {
   date: string | undefined;
@@ -31,53 +33,78 @@ function InputDateTime({
   const schedule = useSelector(selectSchedule);
   const title = type === "start" ? SCHEDULE_DRAWER.start : SCHEDULE_DRAWER.end;
 
-  const changeSchedule = (
-    state:
-      | React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-      | UpdateStateInterface,
-  ) => {
+  const changeSchedule = (state: UpdateStateInterface) => {
     updateSchedule(dispatch, schedule, state);
   };
 
   return (
     <Box sx={{ px: 2.5 }}>
-      <TextField
-        fullWidth
-        onClick={() => handleClick(type, "date")}
-        id={type + "_date"}
-        variant="standard"
-        error={showError && date === ""}
-        helperText={showError && date === "" ? "필수 입력 값입니다!" : ""}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Box sx={{ color: "primary.main", fontWeight: 500 }}>{title}</Box>
-            </InputAdornment>
-          ),
-          endAdornment: !schedule?.all_day && (
-            <InputAdornment position="start">
-              <Box
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleClick(type, "time");
-                }}
-              >
-                {moment(time, "hh:mm").locale("ko").format("LT")}
-              </Box>
-            </InputAdornment>
-          ),
-        }}
-        value={date}
-        onChange={changeSchedule}
-      />
-      <Collapse in={showCalendar !== ""}>
-        {showCalendar === "date" && (
-          <SelectDate date={date} changeSchedule={changeSchedule} type={type} />
-        )}
-        {showCalendar === "time" && (
-          <SelectTime time={time} changeSchedule={changeSchedule} type={type} />
-        )}
-      </Collapse>
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <DateField
+          onClick={() => handleClick(type, "date")}
+          fullWidth
+          variant="standard"
+          format="YYYY-MM-DD"
+          maxDate={moment().add(18, "M")}
+          minDate={moment().subtract(18, "M")}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Box sx={{ color: "primary.main", fontWeight: 500 }}>
+                  {title}
+                </Box>
+              </InputAdornment>
+            ),
+            endAdornment: !schedule?.all_day && (
+              <InputAdornment position="start">
+                <Box
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleClick(type, "time");
+                  }}
+                />
+                <Box
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleClick(type, "time");
+                  }}
+                >
+                  {moment(time, "hh:mm").format("LT")}
+                </Box>
+              </InputAdornment>
+            ),
+          }}
+          value={moment(date)}
+          onChange={(value, context) =>
+            context.validationError === null &&
+            value &&
+            changeSchedule({
+              target: {
+                id: type + "_date",
+                value: value.format("YYYY-MM-DD"),
+              },
+            })
+          }
+          onSelectedSectionsChange={(newValue) => console.log(1, newValue)}
+        />
+
+        <Collapse in={showCalendar !== ""}>
+          {showCalendar === "date" && (
+            <SelectDate
+              date={date}
+              changeSchedule={changeSchedule}
+              type={type}
+            />
+          )}
+          {showCalendar === "time" && (
+            <SelectTime
+              time={time}
+              changeSchedule={changeSchedule}
+              type={type}
+            />
+          )}
+        </Collapse>
+      </LocalizationProvider>
     </Box>
   );
 }
