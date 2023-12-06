c64eebd872de24e6925c145fe60bcaf2a7eb1591
soyoung125 <sypark041754@gmail.com>
2023-12-06 16:15:33 +0900

    feat: 매주 반복 요일 선택 구현

--- src/containers/home/ScheduleDrawer/inputs/RepeatInput/repeat/Week/index.tsx
+++ src/containers/home/ScheduleDrawer/inputs/RepeatInput/repeat/Week/index.tsx
@@ -1,11 +1,22 @@
 import { selectSchedule } from "@app/redux/slices/scheduleSlice";
 import RadioButton from "@components/common/RadioButton";
 import { FormControlLabel, Input, Box, Button, Grid } from "@mui/material";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 function Week() {
   const schedule = useSelector(selectSchedule);
   const week = ["월", "화", "수", "목", "금", "토", "일"];
+
+  const [selected, setSelected] = useState<string[]>([]);
+
+  const handleChange = (w: string) => {
+    if (selected.includes(w)) {
+      setSelected(selected.filter((s) => s !== w));
+      return;
+    }
+    setSelected(selected.concat(w));
+  };
 
   return (
     <Box>
@@ -37,8 +48,9 @@ function Week() {
           week.map((w) => (
             <Grid item xs={2}>
               <Button
-                variant="outlined"
-                color="secondary"
+                onClick={() => handleChange(w)}
+                variant={selected.includes(w) ? "contained" : "outlined"}
+                color={selected.includes(w) ? "primary" : "secondary"}
                 sx={{
                   minWidth: "36px",
                   height: "36px",
