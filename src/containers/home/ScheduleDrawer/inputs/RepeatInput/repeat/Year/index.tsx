325c9983c42c1e61ec98eb857bc486852e02fea6
soyoung125 <sypark041754@gmail.com>
2023-12-07 00:48:37 +0900

    feat: 달반복 옵션 선택 기능 구현

--- src/containers/home/ScheduleDrawer/inputs/RepeatInput/repeat/Year/index.tsx
+++ src/containers/home/ScheduleDrawer/inputs/RepeatInput/repeat/Year/index.tsx
@@ -3,9 +3,23 @@ import { Box, Button, Stack } from "@mui/material";
 import { useSelector } from "react-redux";
 import RadioLabel from "../../radio/RadioLabel";
 import InputLabel from "../../radio/RadioLabel/InputLabel";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
 function Year() {
   const schedule = useSelector(selectSchedule);
+  const start_date = moment(schedule?.start_date);
+  const week = ["첫", "두", "세", "네", "여섯"];
+
+  const [selected, setSelected] = useState("date");
+  const date = {
+    month: start_date.month(),
+    date: start_date.date(),
+    day: start_date.format("dddd"),
+    week: start_date.week() - start_date.startOf("month").week(),
+    lastWeek: start_date.daysInMonth(),
+    lastDate: start_date.endOf("month"),
+  };
 
   return (
     <Box>
@@ -23,28 +37,33 @@ function Year() {
       />
 
       {schedule?.repeat === "Year" && (
-        <Stack px={2.5} spacing={1} alignItems="center">
+        <Stack px={2.5} my={1.5} spacing={1} alignItems="center">
           <Button
-            variant="outlined"
-            color="secondary"
+            variant={selected === "date" ? "contained" : "outlined"}
+            color={selected === "date" ? "primary" : "secondary"}
             sx={{ borderRadius: "20px", width: "200px" }}
+            onClick={() => setSelected("date")}
           >
-            MM월 DD일
+            {`${date.month}월 ${date.date}일`}
           </Button>
           <Button
-            variant="outlined"
-            color="secondary"
+            variant={selected === "weekNum" ? "contained" : "outlined"}
+            color={selected === "weekNum" ? "primary" : "secondary"}
             sx={{ borderRadius: "20px", width: "200px" }}
+            onClick={() => setSelected("weekNum")}
           >
-            MM월 N 번째 D요일
+            {`${date.month}월 ${week[date.week - 1]}번째 ${date.day}`}
           </Button>
-          <Button
-            variant="outlined"
-            color="secondary"
-            sx={{ borderRadius: "20px", width: "200px" }}
-          >
-            MM월 마지막 D요일
-          </Button>
+          {date.lastDate.diff(schedule?.start_date, "day") < 7 && (
+            <Button
+              variant={selected === "lastWeek" ? "contained" : "outlined"}
+              color={selected === "lastWeek" ? "primary" : "secondary"}
+              sx={{ borderRadius: "20px", width: "200px" }}
+              onClick={() => setSelected("lastWeek")}
+            >
+              {`${date.month}월 마지막 ${date.day}`}
+            </Button>
+          )}
         </Stack>
       )}
     </Box>
