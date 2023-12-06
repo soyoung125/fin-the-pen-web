da604624e972505234d51663d8d2cfcb2b86d8f0
soyoung125 <sypark041754@gmail.com>
2023-12-07 00:04:15 +0900

    feat: 월 반복 옵션 선택 구현

--- src/containers/home/ScheduleDrawer/inputs/RepeatInput/repeat/Month/index.tsx
+++ src/containers/home/ScheduleDrawer/inputs/RepeatInput/repeat/Month/index.tsx
@@ -1,16 +1,30 @@
 import { selectSchedule } from "@app/redux/slices/scheduleSlice";
 import { Box, Button, Grid } from "@mui/material";
+import { useState } from "react";
 import moment from "moment";
 import { useSelector } from "react-redux";
 import RadioLabel from "../../radio/RadioLabel";
 import InputLabel from "../../radio/RadioLabel/InputLabel";
+import DateButton from "@components/common/DateButton";
 
 function Month() {
   const schedule = useSelector(selectSchedule);
   const months = Array.from({ length: 31 }, (_, i) => i + 1);
+  const date = moment(schedule?.start_date).format("D");
+
+  const [selectedOption, setSelectedOption] = useState("date");
+  const [selectedDate, setSelectedDate] = useState<number[]>([Number(date)]);
+
+  const handleClick = (d: number) => {
+    if (selectedDate.includes(d)) {
+      setSelectedDate(selectedDate.filter((s) => s !== d));
+      return;
+    }
+    setSelectedDate(selectedDate.concat(d));
+  };
 
   return (
-    <Box>
+    <>
       <RadioLabel
         value="Month"
         label={
@@ -25,46 +39,42 @@ function Month() {
       />
 
       {schedule?.repeat === "Month" && (
-        <Grid px={2.5} container columns={{ xs: 14 }} spacing={1}>
+        <Grid container px={2.5} py={1.5} columns={14} spacing={1.5}>
           <Grid item xs={7}>
             <Button
               fullWidth
-              variant="outlined"
-              color="secondary"
+              variant={selectedOption === "date" ? "contained" : "outlined"}
+              color={selectedOption === "date" ? "primary" : "secondary"}
               sx={{ borderRadius: "20px" }}
-            >{`${moment(schedule.start_date).format("D")}일마다 반복`}</Button>
+              onClick={() => setSelectedOption("date")}
+            >{`${date}일마다 반복`}</Button>
           </Grid>
 
           <Grid item xs={7}>
             <Button
               fullWidth
-              variant="outlined"
-              color="secondary"
+              variant={selectedOption === "select" ? "contained" : "outlined"}
+              color={selectedOption === "select" ? "primary" : "secondary"}
               sx={{ borderRadius: "20px" }}
+              onClick={() => setSelectedOption("select")}
             >
               반복할 날짜 선택
             </Button>
           </Grid>
 
-          {months.map((d) => (
-            <Grid item xs={2}>
-              <Button
-                variant="outlined"
-                color="secondary"
-                sx={{
-                  minWidth: "36px",
-                  height: "36px",
-                  p: 0,
-                  borderRadius: "12px",
-                }}
-              >
-                {d}
-              </Button>
-            </Grid>
-          ))}
+          {selectedOption === "select" &&
+            months.map((d) => (
+              <Grid item xs={2} display="flex" mt={2.5} justifyContent="center">
+                <DateButton
+                  value={d}
+                  handleClick={() => handleClick(d)}
+                  isSelected={selectedDate.includes(d)}
+                />
+              </Grid>
+            ))}
         </Grid>
       )}
-    </Box>
+    </>
   );
 }
 
