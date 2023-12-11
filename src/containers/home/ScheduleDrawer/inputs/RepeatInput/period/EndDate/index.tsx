b020fd79cacf323c66e71e8c694580b5508bd2d7
soyoung125 <sypark041754@gmail.com>
2023-12-11 14:03:47 +0900

    feat: 종료 날짜 저장

--- src/containers/home/ScheduleDrawer/inputs/RepeatInput/period/EndDate/index.tsx
+++ src/containers/home/ScheduleDrawer/inputs/RepeatInput/period/EndDate/index.tsx
@@ -1,10 +1,72 @@
-import { selectSchedule } from "@app/redux/slices/scheduleSlice";
-import { Box, Stack, InputBase } from "@mui/material";
+import {
+  selectSchedule,
+  selectStartDate,
+} from "@app/redux/slices/scheduleSlice";
+import { Box, Stack, InputBase, FormControl } from "@mui/material";
 import { useSelector } from "react-redux";
 import RadioLabel from "../../radio/RadioLabel";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
 function EndDate() {
   const schedule = useSelector(selectSchedule);
+  const startDate = useSelector(selectStartDate);
+
+  const [date, setDate] = useState({
+    year: 2023,
+    month: 1,
+    date: 1,
+    day: "월요일",
+  });
+
+  useEffect(() => {
+    const date = moment(startDate);
+
+    setDate({
+      year: date.year(),
+      month: date.month(),
+      date: date.date(),
+      day: date.format("dddd"),
+    });
+  }, [startDate]);
+
+  const handleChange = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { id, value, min, max } = e.target;
+    if (value < min) e.target.value = min;
+    if (value > max) e.target.value = max;
+
+    switch (id) {
+      case "year": {
+        const d = `${e.target.value}-${date.month}-${date.date}`;
+        setDate({
+          ...date,
+          year: Number(e.target.value),
+          day: moment(d, "YYYY-M-D").format("dddd"),
+        });
+        break;
+      }
+      case "month": {
+        const d = `${date.year}-${e.target.value}-${date.date}`;
+        setDate({
+          ...date,
+          month: Number(e.target.value),
+          day: moment(d, "YYYY-M-D").format("dddd"),
+        });
+        break;
+      }
+      case "date": {
+        const d = `${date.year}-${date.month}-${e.target.value}`;
+        setDate({
+          ...date,
+          date: Number(e.target.value),
+          day: moment(d, "YYYY-M-D").format("dddd"),
+        });
+        break;
+      }
+      default:
+        return;
+    }
+  };
 
   return (
     <Box>
@@ -16,47 +78,77 @@ function EndDate() {
           spacing={1}
           justifyContent="center"
           alignItems="center"
+          mt={0.5}
         >
-          <InputBase
-            sx={{
-              py: 1,
-              px: "10px",
-              width: "60px",
-              backgroundColor: "rgba(115, 91, 242, 0.10)",
-            }}
-            placeholder="YYYY"
-            inputProps={{ "aria-label": "search google maps" }}
-          />
+          <FormControl onSubmit={() => console.log(111)}>
+            <InputBase
+              id="year"
+              sx={{
+                py: 1,
+                px: "10px",
+                width: "60px",
+                backgroundColor: "rgba(115, 91, 242, 0.10)",
+              }}
+              type="number"
+              placeholder="YYYY"
+              onBlur={handleChange}
+              defaultValue={date.year}
+              inputProps={{
+                style: { textAlign: "center" },
+                min: date.year,
+                max: date.year + 50,
+              }}
+            />
+          </FormControl>
 
           <Box>년</Box>
 
-          <InputBase
-            sx={{
-              py: 1,
-              px: "10px",
-              width: "50px",
-              backgroundColor: "rgba(115, 91, 242, 0.10)",
-            }}
-            placeholder="MM"
-            inputProps={{ "aria-label": "search google maps" }}
-          />
+          <FormControl>
+            <InputBase
+              id="month"
+              sx={{
+                py: 1,
+                px: "10px",
+                width: "50px",
+                backgroundColor: "rgba(115, 91, 242, 0.10)",
+              }}
+              type="number"
+              placeholder="MM"
+              onBlur={handleChange}
+              defaultValue={date.month}
+              inputProps={{ style: { textAlign: "center" }, min: 1, max: 12 }}
+            />
+          </FormControl>
 
           <Box>월</Box>
 
-          <InputBase
-            sx={{
-              py: 1,
-              px: "10px",
-              width: "50px",
-              backgroundColor: "rgba(115, 91, 242, 0.10)",
-            }}
-            placeholder="DD"
-            inputProps={{ "aria-label": "search google maps" }}
-          />
+          <FormControl>
+            <InputBase
+              id="date"
+              sx={{
+                py: 1,
+                px: "10px",
+                width: "50px",
+                backgroundColor: "rgba(115, 91, 242, 0.10)",
+              }}
+              type="number"
+              placeholder="DD"
+              onBlur={handleChange}
+              defaultValue={date.date}
+              inputProps={{
+                style: { textAlign: "center" },
+                min: 1,
+                max: moment(
+                  `${date.year}-${date.month}`,
+                  "YYYY-M",
+                ).daysInMonth(),
+              }}
+            />
+          </FormControl>
 
           <Box>일</Box>
 
-          <Box>N요일</Box>
+          <Box>{date.day}</Box>
         </Stack>
       )}
     </Box>
