029df149298e70532a187f28b74a3e2468efbc0a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-13 04:07:54 +0900

    feat: select time 에서 시간을 입력하는 기능 구현 - 오전/오후 버튼 추가 및 유효성 검증 추가 필요

--- src/components/layouts/date-picker/components/TimePicker.tsx
+++ src/components/layouts/date-picker/components/TimePicker.tsx
@@ -35,9 +35,7 @@ function TimePicker({ onClickApprove, onClickReject }: TimePickerProps) {
         </Stack>
         <Divider sx={{ backgroundColor: "black", height: "1px" }} />
         <div>
-          <SelectTime
-          // value={value} setValue={setValue}
-          />
+          <SelectTime value={value} setValue={setValue} />
         </div>
         <Stack direction="row" gap="10px">
           <Button
