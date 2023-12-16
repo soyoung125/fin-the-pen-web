f434e2aa3af9efa24584cfe36c2b3ecfce7737b9
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-16 17:26:19 +0900

    feat: CategoryButton 구현

--- src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategoryButton.stories.tsx
+++ src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategoryButton.stories.tsx
@@ -1,15 +1,20 @@
 import type { Meta } from "@storybook/react";
-import CategoryButton from "./CategoryButton";
+import CategoryButton, { CategoryButtonProps } from "./CategoryButton";
 
 const meta = {
   title: "ui/ScheduleDrawer/CategoryButton",
   component: CategoryButton,
   tags: ["autodocs"],
+  args: { category: "카테고리", selected: true },
   argTypes: {},
 } satisfies Meta<typeof CategoryButton>;
 
 export default meta;
 
-export const Default = () => {
-  return <CategoryButton />;
+export const Default = (args: CategoryButtonProps) => {
+  return (
+    <div style={{ width: "500px" }}>
+      <CategoryButton {...args} />
+    </div>
+  );
 };
--- src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategoryButton.tsx
+++ src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategoryButton.tsx
@@ -1,5 +1,38 @@
-function CategoryButton() {
-  return <>button</>;
+import { Button, Stack, Typography } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+
+export interface CategoryButtonProps {
+  selected: boolean;
+  category: string;
+}
+
+function CategoryButton({ category, selected }: CategoryButtonProps) {
+  return (
+    <Button
+      sx={{
+        borderTop: "1px solid #E0E0E0",
+        borderBottom: "1px solid #E0E0E0",
+        borderRadius: 0,
+      }}
+      fullWidth
+    >
+      <Stack
+        p="16px"
+        color={selected ? "black" : "#8C919C"}
+        justifyContent="space-between"
+        direction="row"
+        sx={{ width: "100%" }}
+      >
+        <Typography>{category}</Typography>
+        {selected ? (
+          <CheckCircleIcon sx={{ color: "#735BF2" }} />
+        ) : (
+          <CheckCircleOutlineIcon />
+        )}
+      </Stack>
+    </Button>
+  );
 }
 
 export default CategoryButton;
