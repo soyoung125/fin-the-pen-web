bf4570731203ca0c5f0473017832e0689e465b8d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-16 16:24:06 +0900

    test: CategoryPicker storybook 추가

---
+++ src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategoryPicker.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CategoryPicker from "./CategoryPicker";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "ScheduleDrawer/CategoryPicker",
+  component: CategoryPicker,
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof CategoryPicker>;
+
+export default meta;
+
+export const Default = () => {
+  return <CategoryPicker />;
+};
