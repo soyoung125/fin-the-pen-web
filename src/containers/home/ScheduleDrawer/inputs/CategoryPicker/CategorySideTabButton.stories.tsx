c9216aa8dbc165931f24fa4033bca217fbe5d077
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-17 17:40:59 +0900

    design: CategorySideTabButton 디자인

--- src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategorySideTabButton.stories.tsx
+++ src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategorySideTabButton.stories.tsx
@@ -1,22 +1,70 @@
 import type { Meta } from "@storybook/react";
-import CategoryButton, { CategoryButtonProps } from "./CategoryButton";
+import CategorySideTabButton, {
+  CategorySideTabProps,
+} from "./CategorySideTabButton.tsx";
 import { useState } from "react";
-import CategorySideTabButton from "./CategorySideTabButton.tsx";
 
 const meta = {
   title: "ui/ScheduleDrawer/CategorySideTabButton",
   component: CategorySideTabButton,
   tags: ["autodocs"],
-  args: { onClick: () => {} },
+  args: {
+    tab: "탭",
+    isSelected: true,
+    selectedCategoryCount: 3,
+    onClick: () => alert("clicked"),
+  },
   argTypes: {},
 } satisfies Meta<typeof CategorySideTabButton>;
 
 export default meta;
 
-export const Default = (args: CategoryButtonProps) => {
+export const Default = (args: CategorySideTabProps) => {
   return (
     <div style={{ width: "500px" }}>
       <CategorySideTabButton {...args} />
+    </div>
+  );
+};
+
+export const Dynamic = () => {
+  const [isSelected, setIsSelected] = useState(false);
+  return (
+    <div style={{ width: "500px" }}>
+      <CategorySideTabButton
+        tab="hi"
+        selectedCategoryCount={5}
+        isSelected={isSelected}
+        onClick={() => setIsSelected(!isSelected)}
+      />
+    </div>
+  );
+};
+
+export const Dynamics = () => {
+  const [tabList, setTabList] = useState([
+    { id: 0, isActive: true, count: 3, tab: "식비" },
+    { id: 1, isActive: false, count: 5, tab: "교통" },
+    { id: 2, isActive: false, count: 2, tab: "구독" },
+    { id: 3, isActive: false, count: 1, tab: "기타" },
+  ]);
+
+  return (
+    <div style={{ width: "200px" }}>
+      {tabList.map((tab) => (
+        <CategorySideTabButton
+          isSelected={tab.isActive}
+          tab={tab.tab}
+          selectedCategoryCount={tab.count}
+          onClick={() =>
+            setTabList((prev) => {
+              const list = [...prev].map((t) => ({ ...t, isActive: false }));
+              list[tab.id].isActive = true;
+              return list;
+            })
+          }
+        />
+      ))}
     </div>
   );
 };
--- src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategorySideTabButton.tsx
+++ src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategorySideTabButton.tsx
@@ -1,12 +1,43 @@
 import { MouseEventHandler } from "react";
-import { Button } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 
 export interface CategorySideTabProps {
+  isSelected: boolean;
+  tab: string;
+  selectedCategoryCount: number;
   onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-function CategorySideTabButton({ onClick }: CategorySideTabProps) {
-  return <Button onClick={onClick}>ㅇㅇ</Button>;
+function CategorySideTabButton({
+  isSelected,
+  tab,
+  selectedCategoryCount,
+  onClick,
+}: CategorySideTabProps) {
+  return (
+    <Button
+      sx={{
+        borderTop: "1px solid #E0E0E0",
+        borderBottom: "1px solid #E0E0E0",
+        borderRadius: 0,
+        backgroundColor: isSelected ? "rgba(115, 91, 242, 0.15)" : "#F7F7F8",
+      }}
+      fullWidth
+      onClick={onClick}
+    >
+      <Stack
+        p="16px"
+        color={isSelected ? "black" : "#8C919C"}
+        justifyContent="space-between"
+        direction="row"
+        sx={{ width: "100%" }}
+      >
+        <Typography>
+          {tab}({selectedCategoryCount})
+        </Typography>
+      </Stack>
+    </Button>
+  );
 }
 
 export default CategorySideTabButton;
