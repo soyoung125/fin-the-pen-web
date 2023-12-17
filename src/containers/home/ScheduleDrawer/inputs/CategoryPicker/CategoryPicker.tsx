7f49d9d3727507ec850bc4da9dded486bc9bd8bd
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-17 21:43:20 +0900

    feat: 탭 추가

--- src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategoryPicker.tsx
+++ src/containers/home/ScheduleDrawer/inputs/CategoryPicker/CategoryPicker.tsx
@@ -1,5 +1,26 @@
+import { SyntheticEvent, useState } from "react";
+import { Tab, Tabs } from "@mui/material";
+
 function CategoryPicker() {
-  return <>CategoryPicker</>;
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
+    setActiveTabIndex(newValue);
+  };
+
+  return (
+    <>
+      <Tabs
+        value={activeTabIndex}
+        onChange={handleChange}
+        textColor="primary"
+        indicatorColor="primary"
+        variant="fullWidth"
+      >
+        <Tab label="지출" />
+        <Tab label="수입" />
+      </Tabs>
+    </>
+  );
 }
 
 export default CategoryPicker;
