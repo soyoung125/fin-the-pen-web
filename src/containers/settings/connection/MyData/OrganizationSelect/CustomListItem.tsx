89778540897c3e1cbbf88a42571466e74fae30fa
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-26 00:23:04 +0900

    refactor: styled-components 를 emotion/styled로 변경

--- src/containers/settings/connection/MyData/OrganizationSelect/CustomListItem.tsx
+++ src/containers/settings/connection/MyData/OrganizationSelect/CustomListItem.tsx
@@ -1,33 +1,43 @@
-import { Grid, Stack, Box } from "@mui/material";
+import { Box, Grid, Stack } from "@mui/material";
 import { OrganizationInterface } from "@type/common";
-import styled from "styled-components";
+import styled from "@emotion/styled";
 
 interface CustomListItemProps {
-    item: OrganizationInterface,
-    isChecked: boolean,
-    opacity: number,
-    handleSelectOrganization: (org: OrganizationInterface) => void,
+  item: OrganizationInterface;
+  isChecked: boolean;
+  opacity: number;
+  handleSelectOrganization: (org: OrganizationInterface) => void;
 }
 
-const Img = styled('img')({
-    display: 'block',
-    width: '28px',
-    height: '28px',
+const Img = styled("img")({
+  display: "block",
+  width: "28px",
+  height: "28px",
 });
 
-function CustomListItem({ item, isChecked, opacity, handleSelectOrganization }: CustomListItemProps) {
-    return (
-        <Grid
-            item xs={6} key={Math.random()}
-            onClick={() => handleSelectOrganization(item)}
-            sx={{ color: isChecked ? '#735BF2' : '#43464C' , opacity: isChecked ? 1 : opacity }}
-        >
-            <Stack direction="row" spacing={2}>
-                <Img alt={item.name} src={item.icon} />
-                <Box>{item.name}</Box>
-            </Stack>
-        </Grid>
-    );
+function CustomListItem({
+  item,
+  isChecked,
+  opacity,
+  handleSelectOrganization,
+}: CustomListItemProps) {
+  return (
+    <Grid
+      item
+      xs={6}
+      key={Math.random()}
+      onClick={() => handleSelectOrganization(item)}
+      sx={{
+        color: isChecked ? "#735BF2" : "#43464C",
+        opacity: isChecked ? 1 : opacity,
+      }}
+    >
+      <Stack direction="row" spacing={2}>
+        <Img alt={item.name} src={item.icon} />
+        <Box>{item.name}</Box>
+      </Stack>
+    </Grid>
+  );
 }
 
 export default CustomListItem;
