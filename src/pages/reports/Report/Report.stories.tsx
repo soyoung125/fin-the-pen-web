528421f3984e5218f1f1bbcfafea2c8f1dc7c752
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-25 23:43:33 +0900

    test: 스토리북을 활용하여 모킹 예제 페이지 추가

--- src/pages/reports/Report/Report.stories.tsx
+++ src/pages/reports/Report/Report.stories.tsx
@@ -6,6 +6,7 @@ import PredictBox from "@pages/reports/Report/components/PredictBox";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import SettingsIcon from "@mui/icons-material/Settings";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import TopNavigationBar from "@components/layouts/common/TopNavigationBar";
 
 const meta = {
   title: "reports/Report",
@@ -17,33 +18,39 @@ const meta = {
 
 export default meta;
 
-export const Example = () => {
+export const PageExample = () => {
   return (
-    <Stack bgcolor="#F7F7F8" px="20px" py="24px" gap="24px">
-      <ReportTitle year={2023} month={5} amount={333000000} />
-      <Stack direction="row" gap="10px">
-        <PredictBox
-          title="이번 달 목표 지출"
-          titleIcon={<AccountBalanceWalletIcon />}
-          amount={1200000}
-          navigateIcon={<SettingsIcon />}
+    <>
+      <TopNavigationBar
+        onClick={() => alert("hey")}
+        title="월간 소트비 리포트"
+      />
+      <Stack bgcolor="#F7F7F8" px="20px" py="24px" gap="24px">
+        <ReportTitle year={2023} month={5} amount={333000000} />
+        <Stack direction="row" gap="10px">
+          <PredictBox
+            title="이번 달 목표 지출"
+            titleIcon={<AccountBalanceWalletIcon />}
+            amount={1200000}
+            navigateIcon={<SettingsIcon />}
+          />
+          <PredictBox
+            title="사용 가능 금액"
+            titleIcon={<InfoOutlinedIcon />}
+            amount={579000}
+          />
+        </Stack>
+        <ReportBox
+          title="월간 소비 리포트"
+          navigateTo="/somewhere"
+          content={<div>원형 그래프 미구현</div>}
         />
-        <PredictBox
-          title="사용 가능 금액"
-          titleIcon={<InfoOutlinedIcon />}
-          amount={579000}
+        <ReportBox
+          title="소비 예측 리포트"
+          navigateTo="/somewhere"
+          content={<div>막대 그래프 미구현</div>}
         />
       </Stack>
-      <ReportBox
-        title="월간 소비 리포트"
-        navigateTo="/somewhere"
-        content={<div>원형 그래프 미구현</div>}
-      />
-      <ReportBox
-        title="소비 예측 리포트"
-        navigateTo="/somewhere"
-        content={<div>막대 그래프 미구현</div>}
-      />
-    </Stack>
+    </>
   );
 };
