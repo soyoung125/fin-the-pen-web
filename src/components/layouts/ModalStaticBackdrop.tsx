74a6ea35ba767ae73a94db68ac8422724bc0b60a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-26 00:42:50 +0900

    fix: 타입 오류를 any로 임시 해결

--- src/components/layouts/ModalStaticBackdrop.tsx
+++ src/components/layouts/ModalStaticBackdrop.tsx
@@ -1,21 +1,17 @@
-import {
-  Dialog, DialogProps,
-} from '@mui/material';
+import { Dialog, DialogProps } from "@mui/material";
 
 interface ModalStaticBackdropProps extends DialogProps {
-  width: 	'xs' | 'sm' | 'md' | 'lg' | 'xl' | false,
-  component: JSX.Element,
-  open: boolean,
+  width: "xs" | "sm" | "md" | "lg" | "xl" | false;
+  component: any; // DialogProps
+  open: boolean;
 }
 
 export default function ModalStaticBackdrop(props: ModalStaticBackdropProps) {
-  const {
-    width, component, open, ...other
-  } = props;
+  const { width, component, open, ...other } = props;
 
   return (
     <Dialog
-      sx={{ '& .MuiDialog-paper': { borderRadius: '1rem', width: '100%' } }}
+      sx={{ "& .MuiDialog-paper": { borderRadius: "1rem", width: "100%" } }}
       maxWidth={width}
       open={open}
       scroll="body"
