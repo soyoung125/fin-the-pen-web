a4a6c2887e5ec1eb48e201f869bf05cd406f6521
soyoung125 <sypark041754@gmail.com>
2023-12-27 21:11:50 +0900

    refactor: scheduleList2에 scheduleList 이식
    - scheduleDrawer는 bottombar의 drawer 공유할 예쩡
    - 자동 스크롤 버튼은 useSchedules 적용 완료 후 추가 구현 예정

--- src/containers/home/HomeContainer/view/ScheduleList/ScheduleList2.tsx
+++ src/containers/home/HomeContainer/view/ScheduleList/ScheduleList2.tsx
@@ -1,9 +1,24 @@
 import { useSchedules } from "@app/tanstack-query/schedules/useSchedules";
 import { useUser } from "@app/tanstack-query/useUser";
 import useSchedule from "@hooks/useSchedule";
+import {
+  Box,
+  CircularProgress,
+  Drawer,
+  Stack,
+  Typography,
+} from "@mui/material";
 import moment from "moment";
+import ScheduleCard from "./ScheduleCard";
+import { CATEGORIES, Category } from "@constants/categories";
+import { useState } from "react";
+import { Schedule } from "@type/schedule";
+import EasyAuthentication from "@containers/sign/EasyAuthentication";
+import { useAppDispatch } from "@app/redux/hooks";
+import { changeHideBudgetMode } from "@app/redux/slices/settingSlice";
 
 function ScheduleList2() {
+  const dispatch = useAppDispatch();
   const { data: user } = useUser();
   const { date } = useSchedule(); // redux가 직접 하도록 개선 예정
   const query = {
@@ -11,17 +26,54 @@ function ScheduleList2() {
     date: moment(date).format("YYYY-MM"),
   };
   const { data: schedules, isPending } = useSchedules(query);
+  const [authenticationPageOpen, setAuthenticationPageOpen] = useState(false);
+
   if (isPending) {
-    return <>loading...</>;
+    return (
+      <Stack direction="row" justifyContent="center" alignItems="center">
+        <Box my={5}>
+          <CircularProgress />
+        </Box>
+      </Stack>
+    );
   }
   if (!schedules) {
-    return <>schedules is null</>;
+    return (
+      <Stack justifyContent="center" alignItems="center">
+        <Box my={5}>
+          <Typography>{date}에 등록된 일정이 없습니다!</Typography>
+        </Box>
+      </Stack>
+    );
   }
+
+  if (authenticationPageOpen) {
+    return (
+      <EasyAuthentication
+        handleAuthenticate={() => dispatch(changeHideBudgetMode(false))}
+      />
+    );
+  }
+
+  const handleModal = (schedule: Schedule) => {
+    alert(JSON.stringify(schedule));
+    // setBottomDrawerOpen(true); // 수정 drawer는 bottombar의 drawer를 공유할 수 있도록 수정 예정
+  };
 
   return (
     <>
-      {schedules.map((s) => (
-        <div key={s.id}>{s.event_name}</div>
+      {schedules.map((schedule) => (
+        <ScheduleCard
+          schedule={schedule}
+          category={
+            // 추후 제거 예정
+            CATEGORIES.find((c) => c.title === schedule.category) ||
+            ({ color: "#C8A2C8" } as Category)
+          }
+          key={Math.random()}
+          handleModal={handleModal}
+          openAuthenticationPage={() => setAuthenticationPageOpen(true)}
+        />
       ))}
     </>
   );
