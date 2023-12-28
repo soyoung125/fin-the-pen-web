f11ae73e973c6f86f91ee1b3061418eecceace7c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-12-29 02:14:40 +0900

    test: SelectMenus 설명 추가

--- src/components/common/SelectMenus/SelectMenus.stories.tsx
+++ src/components/common/SelectMenus/SelectMenus.stories.tsx
@@ -14,7 +14,18 @@ const meta = {
     selectedOption: "최신순",
     setSelectedOption: () => alert("아래에서 테스트해주세요"),
   },
-  argTypes: {},
+  argTypes: {
+    options: {
+      description: "선택할 수 있는 옵션들. 최소 1개 이상의 옵션이 필요합니다.",
+    },
+    selectedOption: {
+      description:
+        "현재 선택된 옵션 state. 기본 값은 첫 번째 옵션으로 줘야 합니다.",
+    },
+    setSelectedOption: {
+      description: "옵션을 선택했을 때 실행되는 setState입니다.",
+    },
+  },
 } satisfies Meta<typeof SelectMenus>;
 
 export default meta;
@@ -27,10 +38,14 @@ export const Example = () => {
   const options = ["최신순", "과거순", "높은 금액순", "낮은 금액순"];
   const [selectedOption, setSelectedOption] = useState(options[0]);
   return (
-    <SelectMenus
-      options={options}
-      selectedOption={selectedOption}
-      setSelectedOption={setSelectedOption}
-    />
+    <>
+      <div>현재 상태 : {selectedOption}</div>
+      <SelectMenus
+        options={options}
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
+      />
+      <div style={{ height: "300px" }}></div>
+    </>
   );
 };
