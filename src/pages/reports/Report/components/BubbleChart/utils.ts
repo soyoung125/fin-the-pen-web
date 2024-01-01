87298f2ddc90486367193a033e33cb1bd1be67c2
Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>
2024-01-02 03:23:16 +0900

    design: 버블차트 컴포넌트를 구현한다 (#175)
    * feat: BubbleChart 컴포넌트 추가
    
    * docs: yarn stable 설명 추가
    
    * feat: BubbleChart 내부 컴포넌트 추가
    
    * design: BubbleComponent 구현
    
    * test: BubbleChart 스토리북 설명 추가
    
    * test: BubbleChart 위치 랜덤 생성기 구현
    
    * fix: 버블이 제대로 생성되도록 개선
    
    * design: 버블 차트에 텍스트 색상도 줄 수 있도록 개선
    
    * refactor: 텍스트 가독성 개선
    
    * docs: 함수 설명 추가

--- src/pages/reports/Report/components/BubbleChart/utils.ts
+++ src/pages/reports/Report/components/BubbleChart/utils.ts
@@ -1,5 +1,10 @@
 import { Bubble } from "./BubbleChart.tsx";
 
+/**
+ * 서로 겹치지 않게 랜덤으로 버블을 생성합니다.
+ * 버블의 사이즈가 너무 커서 배치가 불가능하면 앱이 중단되니, 조심히 사용 바랍니다. (추후 개선 예정)
+ * @param bubbleTemplates
+ */
 export const generateRandomBubbles = (
   bubbleTemplates: Omit<Bubble, "x" | "y">[]
 ) => {
