dbd4f88b5919e38c5188ac46eea6c5381437072f
gabrielyoon7 <gabrielyoon7@gmail.com>
2024-01-03 19:46:03 +0900

    feat: MonthlyExpenditureTarget 설명 추가

--- src/pages/Home/next-components/HomeHeader/MonthlyExpenditureTarget/MonthlyExpenditureTarget.tsx
+++ src/pages/Home/next-components/HomeHeader/MonthlyExpenditureTarget/MonthlyExpenditureTarget.tsx
@@ -1,8 +1,10 @@
+import MonthlyExpenditureTargetCard from "@pages/Home/next-components/HomeHeader/MonthlyExpenditureTarget/MonthlyExpenditureTargetCard.tsx";
+
 function MonthlyExpenditureTarget() {
   /**
-   * TODO: TanStack Query
+   * TODO: TanStack Query을 활용해서 사용자의 이름과 이번달 목표 지출 금액을 가져옵니다.
    */
-  return <></>;
+  return <MonthlyExpenditureTargetCard amount={0} userName={""} />;
 }
 
 export default MonthlyExpenditureTarget;
