f4ece8b420538ca45369d7b45068d348c099307c
soyoung125 <sypark041754@gmail.com>
2024-01-05 02:56:37 +0900

    feat: report api tanstack query 연결

---
+++ src/app/types/report.ts
@@ -0,0 +1,5 @@
+export interface Report {
+    category: string;
+    amount: number;
+    rate: string;
+}
--- src/pages/reports/Report/Report.tsx
+++ src/pages/reports/Report/Report.tsx
@@ -1,5 +1,20 @@
+import {Button} from "@mui/material";
+import {useReport} from "@app/tanstack-query/useReport.ts";
+import {useSelector} from "react-redux";
+import {selectMonth} from "@redux/slices/scheduleSlice.tsx";
+import {useUser} from "@app/tanstack-query/useUser.ts";
+
 function Report() {
-  return <>report</>;
+    const month = useSelector(selectMonth);
+    const {data: user} = useUser();
+    const {data} = useReport({
+        user_id: user?.user_id ?? "",
+        date: month,
+    });
+
+    console.log(data);
+
+    return <>{JSON.stringify(data)}</>;
 }
 
 export default Report;
