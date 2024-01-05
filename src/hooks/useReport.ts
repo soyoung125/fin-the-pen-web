1d0b22c98a0fc37c63af0f91392703f75a5cf163
soyoung125 <sypark041754@gmail.com>
2024-01-06 02:40:38 +0900

    refactor: useReport 달 이동 메서드 로직 수정
    - moment js를 이용해 달 계산

--- src/hooks/useReport.ts
+++ src/hooks/useReport.ts
@@ -2,45 +2,48 @@ import {useState} from "react";
 import {useDatePicker} from "@hooks/date-picker/hooks/useDatePicker.tsx";
 import {useUser} from "@app/tanstack-query/useUser.ts";
 import {useReports} from "@app/tanstack-query/useReports.ts";
+import moment from "moment";
 
 const useReport = () => {
-    const [yearMonth, setYearMonth] = useState("2024-01");
-    const [year, month] = yearMonth.split("-").map((s) => Number(s));
-    const {data: user} = useUser();
-    const {data: reportList, isPending, isError} = useReports({
-        user_id: user?.user_id ?? "",
-        date: yearMonth,
-    });
-    const {openMonthPicker} = useDatePicker();
+  const [yearMonth, setYearMonth] = useState("2024-01");
+  const [year, month] = yearMonth.split("-").map((s) => Number(s));
+  const {data: user} = useUser();
+  const {data: reportList, isPending, isError} = useReports({
+    user_id: user?.user_id ?? "",
+    date: yearMonth,
+  });
+  const {openMonthPicker} = useDatePicker();
 
-    const maxPercent = Math.max(...reportList?.map((l) => Number(l.rate)) ?? []);
+  const maxPercent = Math.max(...reportList?.map((l) => Number(l.rate)) ?? []);
 
-    const addMonth = () => {
-        setYearMonth(month >= 12 ? `${year + 1}-${1}` : `${year}-${month + 1}`);
-    };
+  const addMonth = () => {
+    const date = moment(yearMonth, "YYYY-MM")
+    setYearMonth(date.add(1, "month").format("YYYY-MM"));
+  };
 
-    const subtractMonth = () => {
-        setYearMonth(month <= 1 ? `${year - 1}-${12}` : `${year}-${month - 1}`);
-    };
+  const subtractMonth = () => {
+    const date = moment(yearMonth, "YYYY-MM")
+    setYearMonth(date.subtract(1, "month").format("YYYY-MM"));
+  };
 
-    const pickMonth = async () => {
-        const newMonth = await openMonthPicker(yearMonth);
-        setYearMonth(newMonth.format("YYYY-MM"));
-    };
+  const pickMonth = async () => {
+    const newMonth = await openMonthPicker(yearMonth);
+    setYearMonth(newMonth.format("YYYY-MM"));
+  };
 
-    return {
-        yearMonth,
-        year,
-        month,
-        reportList,
-        isPending,
-        isError,
-        openMonthPicker,
-        maxPercent,
-        addMonth,
-        subtractMonth,
-        pickMonth
-    }
+  return {
+    yearMonth,
+    year,
+    month,
+    reportList,
+    isPending,
+    isError,
+    openMonthPicker,
+    maxPercent,
+    addMonth,
+    subtractMonth,
+    pickMonth
+  }
 }
 
 export default useReport;
