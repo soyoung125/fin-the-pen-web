aac050aed6b0813c88d11af30ed50ae78abab859
soyoung125 <sypark041754@gmail.com>
2024-01-06 00:49:54 +0900

    refactor: report 페이지 컴포넌트 조립

--- src/pages/reports/Report/Report.tsx
+++ src/pages/reports/Report/Report.tsx
@@ -1,20 +1,52 @@
-import {Button} from "@mui/material";
 import {useReport} from "@app/tanstack-query/useReport.ts";
 import {useSelector} from "react-redux";
 import {selectMonth} from "@redux/slices/scheduleSlice.tsx";
 import {useUser} from "@app/tanstack-query/useUser.ts";
+import ReportTitle from "@pages/reports/Report/components/ReportTitle";
+import {Stack} from "@mui/material";
+import PredictBox from "@pages/reports/Report/components/PredictBox";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import SettingsIcon from "@mui/icons-material/Settings";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import ReportBox from "@pages/reports/Report/components/ReportBox";
+import {PATH} from "@constants/path.ts";
 
 function Report() {
-    const month = useSelector(selectMonth);
+    const selectedMonth = useSelector(selectMonth);
+    const [year, month] = selectedMonth.split("-");
     const {data: user} = useUser();
     const {data} = useReport({
         user_id: user?.user_id ?? "",
-        date: month,
+        date: selectedMonth,
     });
 
-    console.log(data);
-
-    return <>{JSON.stringify(data)}</>;
+    return <Stack bgcolor="#F7F7F8" gap="24px">
+        <ReportTitle year={year} month={month} amount={333000000}/>
+        <Stack direction="row" gap="10px">
+            <PredictBox
+                title="이번 달 목표 지출"
+                titleIcon={<AccountBalanceWalletIcon/>}
+                amount={1200000}
+                navigateIcon={<SettingsIcon/>}
+            />
+            <PredictBox
+                title="사용 가능 금액"
+                titleIcon={<InfoOutlinedIcon/>}
+                amount={579000}
+                navigateIcon={<InfoOutlinedIcon/>}
+            />
+        </Stack>
+        <ReportBox
+            title="월간 소비 리포트"
+            navigateTo={PATH.reportMonthDetail}
+            content={<div>{JSON.stringify(data)}</div>}
+        />
+        <ReportBox
+            title="소비 예측 리포트"
+            navigateTo="/somewhere"
+            content={<div>막대 그래프 미구현</div>}
+        />
+    </Stack>;
 }
 
 export default Report;
