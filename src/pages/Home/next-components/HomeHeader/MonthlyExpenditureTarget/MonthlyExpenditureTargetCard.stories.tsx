74873910c6fd8764b8e5a66b12221926767d83d8
gabrielyoon7 <gabrielyoon7@gmail.com>
2024-01-06 14:23:08 +0900

    design: 스켈레톤 구현

--- src/pages/Home/next-components/HomeHeader/MonthlyExpenditureTarget/MonthlyExpenditureTargetCard.stories.tsx
+++ src/pages/Home/next-components/HomeHeader/MonthlyExpenditureTarget/MonthlyExpenditureTargetCard.stories.tsx
@@ -2,10 +2,12 @@ import { Meta } from "@storybook/react";
 import MonthlyExpenditureTargetCard, {
   MonthlyExpenditureTargetCardProps,
 } from "./MonthlyExpenditureTargetCard.tsx";
+import MonthlyExpenditureTargetCardSkeleton from "@pages/Home/next-components/HomeHeader/MonthlyExpenditureTarget/MonthlyExpenditureTargetCardSkeleton.tsx";
 
 const meta = {
   title: "Home/HomeHeader/MonthlyExpenditureTargetCard",
   component: MonthlyExpenditureTargetCard,
+  tags: ["autodocs"],
   args: {
     yyyyMM: "2021-10",
     amount: 1200000,
@@ -19,3 +21,5 @@ export default meta;
 export const Default = (args: MonthlyExpenditureTargetCardProps) => (
   <MonthlyExpenditureTargetCard {...args} />
 );
+
+export const Skeleton = () => <MonthlyExpenditureTargetCardSkeleton />;
