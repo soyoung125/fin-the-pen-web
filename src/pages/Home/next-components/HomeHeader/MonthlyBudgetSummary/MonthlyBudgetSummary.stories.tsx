71bf56f2e08a8092794e9e21d3daec9e954cf0bb
gabrielyoon7 <gabrielyoon7@gmail.com>
2024-01-06 14:43:47 +0900

    feat: MonthlyBudgetSummary 구현

---
+++ src/pages/Home/next-components/HomeHeader/MonthlyBudgetSummary/MonthlyBudgetSummary.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta } from "@storybook/react";
+import MonthlyBudgetSummary, {
+  MonthlyBudgetSummaryProps,
+} from "./MonthlyBudgetSummary.tsx";
+
+const meta = {
+  title: "Home/HomeHeader/MonthlyBudgetSummary",
+  component: MonthlyBudgetSummary,
+  tags: ["autodocs"],
+  args: {
+    income: 1200000,
+    expenditure: 1000000,
+    availableAmount: 200000,
+  },
+  argTypes: {
+    income: {
+      control: {
+        type: "number",
+      },
+      description: "수입",
+    },
+    expenditure: {
+      control: {
+        type: "number",
+      },
+      description: "지출",
+    },
+    availableAmount: {
+      control: {
+        type: "number",
+      },
+      description: "사용 가능 금액",
+    },
+  },
+} satisfies Meta<typeof MonthlyBudgetSummary>;
+
+export default meta;
+
+export const Default = (args: MonthlyBudgetSummaryProps) => (
+  <MonthlyBudgetSummary {...args} />
+);
