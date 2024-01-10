e809ecbff656f98f76a3322d39ebd573d0f30e92
soyoung125 <sypark041754@gmail.com>
2024-01-10 21:23:40 +0900

    fix: Report.stories.tsx 누락된 props 설정

--- src/pages/reports/Report/Report.stories.tsx
+++ src/pages/reports/Report/Report.stories.tsx
@@ -1,5 +1,5 @@
-import {Meta} from "@storybook/react";
-import {Stack} from "@mui/material";
+import { Meta } from "@storybook/react";
+import { Stack } from "@mui/material";
 import ReportTitle from "@pages/reports/Report/components/ReportTitle";
 import ReportBox from "@pages/reports/Report/components/ReportBox";
 import PredictBox from "@pages/reports/Report/components/PredictBox";
@@ -9,48 +9,53 @@ import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import TopNavigationBar from "@components/layouts/common/TopNavigationBar";
 
 const meta = {
-    title: "reports/Report",
-    component: undefined,
-    tags: ["autodocs"],
-    args: {},
-    argTypes: {},
+  title: "reports/Report",
+  component: undefined,
+  tags: ["autodocs"],
+  args: {},
+  argTypes: {},
 } satisfies Meta;
 
 export default meta;
 
 export const PageExample = () => {
-    return (
-        <>
-            <TopNavigationBar
-                onClick={() => alert("hey")}
-                title="월간 소비 리포트"
-            />
-            <Stack bgcolor="#F7F7F8" px="20px" py="24px" gap="24px">
-                <ReportTitle year={2023} month={5} amount={333000000}/>
-                <Stack direction="row" gap="10px">
-                    <PredictBox
-                        title="이번 달 목표 지출"
-                        titleIcon={<AccountBalanceWalletIcon/>}
-                        amount={1200000}
-                        navigateIcon={<SettingsIcon/>}
-                    />
-                    <PredictBox
-                        title="사용 가능 금액"
-                        titleIcon={<InfoOutlinedIcon/>}
-                        amount={579000}
-                    />
-                </Stack>
-                <ReportBox
-                    title="월간 소비 리포트"
-                    navigateTo="/somewhere"
-                    content={<div>원형 그래프 미구현</div>}
-                />
-                <ReportBox
-                    title="소비 예측 리포트"
-                    navigateTo="/somewhere"
-                    content={<div>막대 그래프 미구현</div>}
-                />
-            </Stack>
-        </>
-    );
+  return (
+    <>
+      <TopNavigationBar onClick={() => alert("hey")} title="월간 소비 리포트" />
+      <Stack bgcolor="#F7F7F8" px="20px" py="24px" gap="24px">
+        <ReportTitle
+          year={2023}
+          month={5}
+          amount={333000000}
+          pickMonth={() => alert("pick month")}
+        />
+        <Stack direction="row" gap="10px">
+          <PredictBox
+            title="이번 달 목표 지출"
+            titleIcon={<AccountBalanceWalletIcon />}
+            amount={1200000}
+            navigateIcon={<SettingsIcon />}
+            handleClick={() => alert("goal")}
+          />
+          <PredictBox
+            title="사용 가능 금액"
+            titleIcon={<InfoOutlinedIcon />}
+            amount={579000}
+            navigateIcon={<InfoOutlinedIcon />}
+            handleClick={() => alert("useable amount")}
+          />
+        </Stack>
+        <ReportBox
+          title="월간 소비 리포트"
+          navigateTo="/somewhere"
+          content={<div>원형 그래프 미구현</div>}
+        />
+        <ReportBox
+          title="소비 예측 리포트"
+          navigateTo="/somewhere"
+          content={<div>막대 그래프 미구현</div>}
+        />
+      </Stack>
+    </>
+  );
 };
