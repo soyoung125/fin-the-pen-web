993ae26034bd9714156e902e8f54ce0702de25b9
soyoung125 <sypark041754@gmail.com>
2024-01-27 16:21:03 +0900

    refactor: 스토리북 Report 예시 페이지 구현

--- src/pages/reports/Report/Report.stories.tsx
+++ src/pages/reports/Report/Report.stories.tsx
@@ -8,6 +8,15 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import TopNavigationBar from "@components/layouts/common/TopNavigationBar";
 import ReportLayout from "@pages/reports/Report/components/layout/ReportLayout";
+import { Report } from "@app/types/report.ts";
+import { generateRandomBubbles2 } from "@pages/reports/Report/components/BubbleChart/utils.ts";
+import BubbleChart from "@pages/reports/Report/components/BubbleChart";
+import { useState } from "react";
+import { useDatePicker } from "@hooks/date-picker/hooks/useDatePicker.tsx";
+import moment from "moment";
+import PredictReport from "@pages/reports/Report/components/PredictReport";
+import FixedTransaction from "@pages/reports/Report/components/FixedTransaction";
+import MonthlyReport from "@pages/reports/Report/components/MonthlyReport";
 
 const meta = {
   title: "reports/Report",
@@ -19,29 +28,111 @@ const meta = {
 
 export default meta;
 
+const useReport = () => {
+  const [selected, setSelected] = useState("used");
+  const goal = 1009000;
+  const used = 117000;
+  const predict = 13000;
+  const useable = 1009000 - 117000 - 13000;
+
+  const [yearMonth, setYearMonth] = useState("2023-5");
+  const [year, month] = yearMonth.split("-").map((s) => Number(s));
+  const { openMonthPicker } = useDatePicker();
+
+  const pickMonth = async () => {
+    const newMonth = await openMonthPicker(yearMonth);
+    setYearMonth(newMonth.format("YYYY-MM"));
+  };
+
+  const addMonth = () => {
+    const date = moment(yearMonth, "YYYY-MM");
+    setYearMonth(date.add(1, "month").format("YYYY-MM"));
+  };
+
+  const subtractMonth = () => {
+    const date = moment(yearMonth, "YYYY-MM");
+    setYearMonth(date.subtract(1, "month").format("YYYY-MM"));
+  };
+
+  return {
+    selected,
+    setSelected,
+    goal,
+    used,
+    predict,
+    useable,
+    month,
+    year,
+    pickMonth,
+    addMonth,
+    subtractMonth,
+  };
+};
+
 export const PageExample = () => {
+  const {
+    selected,
+    setSelected,
+    year,
+    month,
+    goal,
+    used,
+    predict,
+    useable,
+    pickMonth,
+    addMonth,
+    subtractMonth,
+  } = useReport();
+  const list: Report[] = [
+    {
+      amount: 71000,
+      rate: "20",
+      category: "식비",
+    },
+    {
+      amount: 71000,
+      rate: "12",
+      category: "미용",
+    },
+    {
+      amount: 71000,
+      rate: "8",
+      category: "자동차",
+    },
+    {
+      amount: 71000,
+      rate: "7",
+      category: "패션/쇼핑",
+    },
+    {
+      amount: 71000,
+      rate: "6",
+      category: "카페",
+    },
+  ];
+
   return (
     <>
       <TopNavigationBar onClick={() => alert("hey")} title="월간 소비 리포트" />
       <Stack bgcolor="#F7F7F8" px="20px" py="24px" gap="24px">
         <ReportTitle
-          year={2023}
-          month={5}
-          amount={333000000}
-          pickMonth={() => alert("pick month")}
+          year={year}
+          month={month}
+          amount={used}
+          pickMonth={pickMonth}
         />
         <Stack direction="row" gap="10px">
           <PredictBox
             title="이번 달 목표 지출"
             titleIcon={<AccountBalanceWalletIcon />}
-            amount={1200000}
+            amount={goal}
             navigateIcon={<SettingsIcon />}
             handleClick={() => alert("goal")}
           />
           <PredictBox
             title="사용 가능 금액"
             titleIcon={<InfoOutlinedIcon />}
-            amount={579000}
+            amount={useable}
             navigateIcon={<InfoOutlinedIcon />}
             handleClick={() => alert("useable amount")}
           />
@@ -51,7 +142,7 @@ export const PageExample = () => {
             <ReportLayout
               title="월간 소비 리포트"
               navigateTo="/somewhere"
-              content={<div>원형 그래프 미구현</div>}
+              content={<BubbleChart bubbles={generateRandomBubbles2(list)} />}
             />
           }
         />
@@ -60,15 +151,47 @@ export const PageExample = () => {
             <Stack spacing={5}>
               <ReportLayout
                 title="소비 예측 리포트"
-                content={<div>막대 그래프 미구현</div>}
+                content={
+                  <PredictReport
+                    selected={selected}
+                    setSelected={setSelected}
+                    month={month}
+                    goal={goal}
+                    predict={predict}
+                    used={used}
+                    useable={useable}
+                  />
+                }
               />
               <ReportLayout
                 title="N월 고정 입출금"
-                content={<div>카드 미구현</div>}
+                content={
+                  <Stack spacing={2}>
+                    <FixedTransaction
+                      title={"고정 수입"}
+                      amount={12000000}
+                      month={month}
+                      difference={-200000}
+                    />
+                    <FixedTransaction
+                      title={"고정 지출"}
+                      amount={579000}
+                      month={month}
+                      difference={200000}
+                    />
+                  </Stack>
+                }
               />
               <ReportLayout
                 title="월별 소비 리포트"
-                content={<div>선 그래프 미구현</div>}
+                content={
+                  <MonthlyReport
+                    month={month}
+                    previousSpending={400000}
+                    spending={600000}
+                    twoMonthsAgoSpending={500000}
+                  />
+                }
               />
             </Stack>
           }
