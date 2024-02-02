fd4bfe6e10125cda3414d243087d6f77e9a80211
soyoung125 <sypark041754@gmail.com>
2024-02-02 16:28:59 +0900

    fix: 배포시 연결되는 서버 url 수정

--- src/app/api/url.ts
+++ src/app/api/url.ts
@@ -4,5 +4,5 @@
 
 export const DOMAIN =
   process.env.NODE_ENV === "production"
-    ? "http://ec2-43-202-60-242.ap-northeast-2.compute.amazonaws.com:8080" // 배포 시 실제로 연결할 서버
+    ? "https://api.foo.com" // 배포 시 실제로 연결할 서버로 변경하기
     : "/local"; // 로컬 서버와 연결할 때 (단, MSW 동작 시 로컬 서버로 요청이 나가지 않고 mocking 당할 수 있음)
