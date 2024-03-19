83b0421734c5dbc21f68551f0aef52da6f3e9dba
soyoung125 <sypark041754@gmail.com>
2024-03-19 17:30:23 +0900

    refactor: 불필요한 코드 제거

--- src/pages/Settings/utils.ts
+++ src/pages/Settings/utils.ts
@@ -1,8 +1,3 @@
-const DISPLAY = ["다크모드켜기", "어플비밀번호설정", "금액숨기기"];
-const SECURE = ["비밀번호설정", "비밀번호변경"];
-const ALARM = ["알림데이터"];
-const CONNECT = ["My데이터", "캘린더연동"];
-const INFO = ["공지사항", "인증서", "약관및정책", "버전정보"];
 const MENUS: { [key: string]: string[] } = {
   display: ["다크모드켜기", "어플비밀번호설정", "금액숨기기"],
   secure: ["비밀번호설정", "비밀번호변경"],
