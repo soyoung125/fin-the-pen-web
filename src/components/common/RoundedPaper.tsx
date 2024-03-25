4ccd2f6abe4bebf6dba9a6a072c20d8e0b0035e8
soyoung125 <sypark041754@gmail.com>
2024-03-26 01:03:52 +0900

    refactor: 저축 목표 금액 설정 페이지 디자인 수정

--- src/components/common/RoundedPaper.tsx
+++ src/components/common/RoundedPaper.tsx
@@ -1,15 +1,19 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Box } from "@mui/material";
+import React from "react";
 
 interface RoundedPaperProps {
   children: React.ReactNode;
   my: number;
 }
+
 function RoundedPaper({ children, my }: RoundedPaperProps) {
   return (
     <Box
       sx={{
-        marginY: my, padding: 2, borderRadius: '8px', boxShadow: '0px 0px 12px 0px rgba(0, 0, 0, 0.15)',
+        marginY: my,
+        padding: "20px",
+        borderRadius: "20px",
+        boxShadow: "0px 0px 12px 0px rgba(0, 0, 0, 0.15)",
       }}
     >
       {children}
--- src/legacies/assetManagement/SavingsGoalContainer/goals/Personal/Personal.tsx
+++ src/legacies/assetManagement/SavingsGoalContainer/goals/Personal/Personal.tsx
@@ -1,11 +1,12 @@
-import { Box, Grid, IconButton, Stack } from "@mui/material";
-import BorderColorIcon from "@mui/icons-material/BorderColor";
-import RoundedBorderBox from "../../../../../components/common/RoundedBorderBox";
+import { Box, IconButton, Stack } from "@mui/material";
 import InputModal from "./InputModal";
 import { PersonalGoal, SetPersonalGoalQuery } from "@app/types/asset.ts";
 import { getAmount } from "@legacies/assetManagement/SavingsGoalContainer/utils.ts";
 import { useDialog } from "@hooks/dialog/useDialog.tsx";
 import { useModal } from "@hooks/modal/useModal.tsx";
+import RoundedPaper from "@components/common/RoundedPaper.tsx";
+import filter_main from "@assets/icons/header/filter_main.svg";
+import GoalCard from "@legacies/assetManagement/SavingsGoalContainer/goals/Personal/components/GoalCard";
 
 interface PersonalProps {
   personal?: PersonalGoal;
@@ -38,52 +39,35 @@ function Personal({ personal, handleSetPersonalGoal }: PersonalProps) {
   };
 
   return (
-    <>
-      <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Box sx={{ fontWeight: "bold" }}>당신의 또 다른 목표는 무엇인가요?</Box>
-        <IconButton color="primary" onClick={handleModify}>
-          <BorderColorIcon fontSize="small" />
-        </IconButton>
-      </Stack>
-      <Grid container spacing={1} textAlign="center" mt={0}>
-        <Grid item xs={6}>
-          <Stack
-            justifyContent="space-around"
-            sx={{
-              borderRadius: 2,
-              backgroundColor: "primary.main",
-              color: "white",
-              p: 2,
-              height: "100%",
-            }}
-          >
-            <Box mb={2}>나의 목표</Box>
-            <Box>{personal?.goal_name}</Box>
-            <Box>{getAmount(personal?.goal_amount)}원</Box>
-          </Stack>
-        </Grid>
+    <Box mt="50px">
+      <RoundedPaper my={2}>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box sx={{ fontSize: "18px", fontWeight: "700" }}>
+            나만의 저축 목표
+          </Box>
+          <IconButton color="primary" onClick={handleModify}>
+            <img src={filter_main} alt="filter" />
+          </IconButton>
+        </Stack>
 
-        <Grid item xs={6}>
-          <RoundedBorderBox>
-            <Stack direction="row" justifyContent="space-between" p={2}>
-              <Box>기간</Box>
-              <Box sx={{ color: "primary.main" }}>{personal?.period}</Box>
-            </Stack>
-          </RoundedBorderBox>
-
-          <Box my={1} />
-
-          <RoundedBorderBox>
-            <Box p={2}>
-              <Box mb={1}>핀더펜 MONEY</Box>
-              <Box sx={{ color: "primary.main" }}>
-                {getAmount(personal?.goal_amount)}원
-              </Box>
-            </Box>
-          </RoundedBorderBox>
-        </Grid>
-      </Grid>
-    </>
+        <Stack direction="row" spacing={4} mt={3.5}>
+          <GoalCard
+            title={personal?.goal_name ?? ""}
+            subTitle={`${personal?.period}까지`}
+            amount={getAmount(personal?.goal_amount) ?? 0}
+          />
+          <GoalCard
+            title="저축 목표액"
+            subTitle="한달 기준"
+            amount={getAmount(personal?.required_amount) ?? 0}
+          />
+        </Stack>
+      </RoundedPaper>
+    </Box>
   );
 }
 
